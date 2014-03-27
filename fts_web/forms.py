764a145284b0143f75bfdcea885e2691ef43e6c3
Carlos Ilcobich <cilcobich@puntoangular.com>
2014-03-27 13:38:04 -0300

    US#1-c-Arega forms GrupoAtencionForm, AgenteGrupoAtencionForm y formset AgentesGrupoAtencionFormSet.

--- fts_web/forms.py
+++ fts_web/forms.py
@@ -1,26 +1,55 @@
-# -*- coding: utf-8 -*-
-# from django.db.models import get_model
-# from django import forms
+#-*- coding: utf-8 -*-
+from django.db.models import get_model
+from django import forms
+from django.forms.models import inlineformset_factory
 
-# from crispy_forms.helper import FormHelper
-# from crispy_forms.layout import (
-#     Layout, Fieldset, ButtonHolder, Submit,
-#     Div,
-# )
-
-# GrupoAtencion = get_model('fts_web', 'GrupoAtencion')
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field
 
 
-# class GrupoAtencionForm(forms.ModelForm):
-#     def __init__(self, *args, **kwargs):
-#         super(GrupoAtencionForm, self).__init__(*args, **kwargs)
+GrupoAtencion = get_model('fts_web', 'GrupoAtencion')
+AgenteGrupoAtencion = get_model('fts_web', 'AgenteGrupoAtencion')
 
-#         # If you pass FormHelper constructor a form instance
-#         # It builds a default layout with all its fields
-#         self.helper = FormHelper(self)
 
-#         # You can dynamically adjust your layout
-#         self.helper.layout.append(Submit('save', 'save'))
+class AgenteGrupoAtencionForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+            Field('numero_interno'),
+        )
+        super(AgenteGrupoAtencionForm, self).__init__(*args, **kwargs)
 
-#     class Meta:
-#         model = GrupoAtencion
+    class Meta:
+        model = GrupoAtencion
+        exclude = ('grupo_atencion',)
+        labels = {
+            'numero_interno': '',
+        }
+
+AgentesGrupoAtencionFormSet = inlineformset_factory(
+    GrupoAtencion, AgenteGrupoAtencion,
+    form=AgenteGrupoAtencionForm,
+    extra=1,
+)
+
+
+class GrupoAtencionForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+            Field('nombre'),
+            Field('timeout'),
+            Field('ring_strategy'),
+        )
+        super(GrupoAtencionForm, self).__init__(*args, **kwargs)
+
+    class Meta:
+        model = GrupoAtencion
+
+    def clean_nombre(self):
+        data = self.cleaned_data['nombre']
+        if not data:
+            raise forms.ValidationError("Este campo es requerido.")
+        return data
