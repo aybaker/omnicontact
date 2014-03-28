4dbc1901c48d3cd32d53f18fe4ee81b33da26d59
Horacio G. de Oro <hgdeoro@gmail.com>
2014-03-28 15:51:08 -0300

    Daemon: busca y procesa campanas

--- fts_daemon/poll_daemon.py
+++ fts_daemon/poll_daemon.py
@@ -7,13 +7,42 @@ import logging
 import time
 
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("FTSenderDaemon")
+
+
+def setup():
+    from django.conf import settings
+    from fts_web.models import Campana
+    logging.getLogger().setLevel(logging.INFO)
+
+
+def procesar_campana(campana):
+    """Procesa una campana.
+
+    Returns:
+        cant_contactos_procesados
+    """
+    from fts_web.models import Campana
+    assert isinstance(campana, Campana)
+    logger.info("Iniciando procesado de campana %s", campana.id)
+    contador_contactos = 0
+    for contacto in campana.bd_contacto.contactos.all():
+        logger.info(" - Procesando contacto %s", contacto.id)
+        contador_contactos += 1
+
+    logger.info("Marcando cmapana %s como FINALIZADA", campana.id)
+    campana.finalizar()
+    return contador_contactos
+
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    from django.conf import settings
-    from fts_web.models import GrupoAtencion
+    logging.info("Inicianodo FTSenderDaemon...")
+    setup()
+    logging.info("Setup de Django OK")
+    from fts_web.models import Campana
     while True:
-        for ga in GrupoAtencion.objects.all():
-            print " - {}".format(ga.nombre)
-        time.sleep(20)
+        logging.info("Obteniendo campanas activas...")
+        campanas = Campana.objects.obtener_activas()
+        for campana in campanas:
+            resultado = procesar_campana(campana)
+        time.sleep(2)
