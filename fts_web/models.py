2a103394b339b6c2c800bb4bbd788654751fc104
Horacio G. de Oro <hgdeoro@gmail.com>
2014-03-28 14:45:06 -0300

    Implementa CampanaManager y finalizacion de campanas

--- fts_web/models.py
+++ fts_web/models.py
@@ -110,11 +110,51 @@ class Contacto(models.Model):
         return '(ELiminado) {0}'.format(self.telefono)
 
 
+#===============================================================================
+# Campaña
+#===============================================================================
+
+class CampanaManager(models.Manager):
+    """
+    Manager para Campanas
+    """
+    #    def get_queryset(self):
+    #        # F-I-X-M-E: esto no romperá los modelforms? Ej: form.campana. Si la campana
+    #        # que referencia el modelform esta ESTADO_EN_DEFINICION, ¿aparece en el html?
+    #        # Especificamente, en el caso de Campana.estado no habria problema, pero
+    #        # esta medotodologia de filtrado automatico ¿podria ser un peligro
+    #        # en el "borrado logico"?
+    #        return super(CampanaManager, self).exclude(estado=Campana.ESTADO_EN_DEFINICION)
+
+    def obtener_activas(self):
+        """Devuelve campañas en estado activas.
+        Puede suceder que este metodo devuelva campañas en estado activas,
+        pero que en realidad deberian estar en estado FINALIZADAS porque,
+        por ejemplo, la fecha actual es posterior a "fecha de fin", todavía
+        no se actualizó el estado de dichas campañas en la BD.
+        """
+        return self.filter(estado=Campana.ESTADO_ACTIVA)
+
+
 class Campana(models.Model):
     """Una campaña del call center"""
+    objects = CampanaManager()
+
+    """La campaña esta siendo definida en el wizard"""
     ESTADO_EN_DEFINICION = 1
+
+    """La campaña esta activa, o sea, EN_CURSO o PROGRAMADA
+    A nivel de modelos, solo queremos registrar si está ACTIVA, y no nos
+    importa si esta EN_CURSO (o sea, si en este momento el daemon está
+    generando llamadas asociadas a la campaña) o PROGRAMADA (si todavia no
+    estamos en el rango de dias y horas en los que se deben generar las llamadas)
+    """
     ESTADO_ACTIVA = 2
+
+    """La capaña fue pausada"""
     ESTADO_PAUSADA = 3
+
+    """La campaña fue finalizada, automatica o manualmente"""
     ESTADO_FINALIZADA = 4
 
     ESTADOS = (
@@ -136,6 +176,12 @@ class Campana(models.Model):
         'ListaContacto',
         related_name='campanas'
     )
+
+    def finalizar(self):
+        """Setea la campaña como finalizada"""
+        assert self.estado in (Campana.ESTADO_ACTIVA, Campana.ESTADO_PAUSADA)
+        self.estado = Campana.ESTADO_FINALIZADA
+        self.save()
 
     def __unicode__(self):
         return self.nombre
