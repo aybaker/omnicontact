4df0dd15320e12a6e36c4ba74e5b4be5292a65b8
Horacio G. de Oro <hgdeoro@gmail.com>
2014-03-28 01:01:04 -0300

    Agregue doc a GrupoAtencionManager

--- fts_web/managers.py
+++ fts_web/managers.py
@@ -4,5 +4,8 @@ from django.db import models
 
 
 class GrupoAtencionManager(models.Manager):
+    """Manager para el modelo GrupoAtencion"""
+
     def filtrar_activos(self):
+        """Devuelve queryset para devolver solo los G.A activos"""
         return self.filter(active=True)
