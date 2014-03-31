344c00a99bfd9695e912c165b07014ad20f8eca5
Horacio G. de Oro <hgdeoro@gmail.com>
2014-03-30 22:35:14 -0300

    Agrega sqlite a sample_settings_local.py

--- fts_web/sample_settings_local.py
+++ fts_web/sample_settings_local.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -19,6 +22,13 @@ DATABASES = {
         'PASSWORD': 'XXXXXXXXX',
         'HOST': 'localhost',
         'PORT': '',
+    }
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
