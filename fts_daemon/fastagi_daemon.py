4ca3a1bf7bee1ba4dd563740acb853207c2690b4
Horacio G. de Oro <hgdeoro@gmail.com>
2014-04-01 16:25:50 -0300

    Agrega daemon FastAGI

---
+++ fts_daemon/fastagi_daemon.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+'''
+Created on Mar 31, 2014
+
+@author: Horacio G. de Oro
+'''
+
+from __future__ import unicode_literals
+
+import logging as _logging
+from twisted.internet import reactor
+from twisted.internet.defer import Deferred
+from twisted.web.client import Agent
+from twisted.web.http_headers import Headers
+
+from starpy import fastagi
+from starpy.fastagi import FastAGIProtocol, FastAGIFactory
+
+
+logger = _logging.getLogger("FTSenderFastAgiDaemon")
+
+
+def setup():
+    from django.conf import settings  # @UnusedImport
+    from fts_web.models import Campana  # @UnusedImport
+    # _logging.getLogger().setLevel(_logging.INFO)
+
+
+SECRET_HEADER_NAME = 'FTSenderSecret'
+SECRET_HEADER_VALUE = 'NosisFej2gighKag4Ong9Mypphip0GhovAn3Ez0'
+
+
+def fastagi_handler(agi):
+    logger.info('Iniciando ejecucion de handler...')
+    assert isinstance(agi, FastAGIProtocol)
+    agi_network_script = agi.variables.get('agi_network_script', '')
+    splitted = agi_network_script.split('/')
+    if len(splitted) == 2:
+        if splitted[1] == 'ha-contestado':
+            id_intento = splitted[0]
+            logger.info("Ha contestado: %s", id_intento)
+
+            def informar_ha_atendido():
+                agent = Agent(reactor)
+                header = Headers({
+                    SECRET_HEADER_NAME: [SECRET_HEADER_VALUE]
+                })
+                url = 'http://localhost:8080/_/agi/contesto/{0}'.format(
+                    id_intento)
+                agent.request('GET', url, header, None)
+
+            d = Deferred()
+            d.addBoth(informar_ha_atendido)
+            reactor.callWhenRunning(d)  # @UndefinedVariable
+
+    agi.finish()
+
+
+if __name__ == '__main__':
+    _logging.basicConfig(level=_logging.INFO)
+    logger.info("Iniciando...")
+    setup()
+
+    logger.info("Setup de Django OK")
+
+    fast_agi_server = fastagi.FastAGIFactory(fastagi_handler)
+    assert isinstance(fast_agi_server, FastAGIFactory)
+    reactor.listenTCP(4573, fast_agi_server, 50,  # @UndefinedVariable
+        '172.19.1.104')
+    logger.info("Lanzando 'reactor.run()'")
+    reactor.run()  # @UndefinedVariable
