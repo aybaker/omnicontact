9780413bd7d69362c1b9b9c31508acd143e4d83b
Horacio G. de Oro <hgdeoro@gmail.com>
2014-04-04 13:04:55 -0300

    FIXes en importador xls

--- fts_web/parserxls.py
+++ fts_web/parserxls.py
@@ -1,31 +1,65 @@
 # -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import logging
+
 import xlrd
 
 
+logger = logging.getLogger('ParserXls')
+
+
 class ParserXls(object):
+    # FIXME: documentar
+
     def __init__(self):
         self.value_list = []
+        self.vacias = 0
+        self.erroneas = 0
 
     def read_file(self, file):
+        # FIXME: Assignment to reserved built-in symbol: file
+        # FIXME: documentar
         workbook = xlrd.open_workbook(file_contents=file.read())
         worksheet = workbook.sheet_by_index(0)
 
         num_rows = worksheet.nrows - 1
-        num_cells = worksheet.ncols - 1
         curr_row = -1
 
         while curr_row < num_rows:
             curr_row += 1
-            curr_cell = -1
+            cell = worksheet.cell(curr_row, 0)
+            
+            # Guardamos valor de nro telefonico en 'cell_value'
+            if type(cell.value) == float:
+                cell_value = str(int(cell.value))
 
-            while curr_cell < num_cells:
-                curr_cell += 1
-                cell_value = worksheet.cell_value(curr_row, curr_cell)
+            elif type(cell.value) == str:
+                cell_value = cell.value.strip()
+                if len(cell_value) == 0:
+                    logger.info("Ignorando celda vacia en fila %s", curr_row)
+                    self.vacias += 1
+                    continue
 
-                if curr_cell == 0:
-                    self.value_list.append(int(cell_value))
+            else:
+                try:
+                    # Intentamos convertir en string y ver que pasa...
+                    cell_value = str(cell.value).strip()
+                except:
+                    logger.info("Ignorando celda en fila %s con valor '%s' "
+                        "de tipo %s", curr_row, cell.value, type(cell.value))
+                    self.erroneas += 1
+                    continue
+
+            self.value_list.append(cell_value)
+
+        logger.info("%s contactos importados - %s celdas ignoradas"
+            " - %s celdas erroneas", len(self.value_list), self.vacias,
+            self.erroneas)
 
         return self.result()
 
     def result(self):
+        # FIXME: documentar
         return self.value_list
