fc2df6e9e9746ca1e9884cb34f61e57bab56d317
Horacio G. de Oro <hgdeoro@gmail.com>
2014-04-12 17:25:16 -0300

    FTS-93 Dialplan: genera opciones (erroneas por ahora)

--- fts_daemon/asterisk_config.py
+++ fts_daemon/asterisk_config.py
@@ -9,12 +9,15 @@ Created on Apr 11, 2014
 
 from __future__ import unicode_literals
 
+import datetime
+
 
 TEMPLATE_DIALPLAN_START = """
 
-;
+;----------------------------------------------------------------------
 ; TEMPLATE_DIALPLAN_START-{fts_campana_id}
-;
+;   Autogenerado {date}
+;----------------------------------------------------------------------
 
 [campania_{fts_campana_id}]
 
@@ -25,17 +28,29 @@ exten => _XXXXXX!.,1,Answer()
  same => _XXXXXX!.,n,WaitExten(5)
  same => _XXXXXX!.,n,Hangup()
 
-exten => 1,1,Agi(ivr_completada.php,${{id_num}},${{campania}})
+; exten => 1,1,Agi(ivr_completada.php,${{id_num}},{fts_campana_id})
+; exten => 1,n,Hangup()
+;
+; exten => 2,1,Agi(ivr_completada.php,${{id_num}},{fts_campana_id})
+; exten => 2,n,Hangup()
+;
+; exten => 3,1,Queue(queue1)
+; exten => 3,n,Hangup()
+;
+; exten => 0,1,Goto(${{id_num}},audio)
+; exten => 0,n,Hangup()
+
+"""
+
+
+TEMPLATE_OPCION = """
+
+:
+; TEMPLATE_OPCION-{fts_opcion_id}
+;
+
+exten => 1,1,Agi(ivr_opcion_seleccionada.php,{fts_opcion_id},{fts_campana_id})
 exten => 1,n,Hangup()
-
-exten => 2,1,Agi(ivr_completada.php,${{id_num}},${{campania}})
-exten => 2,n,Hangup()
-
-exten => 3,1,Queue(queue1)
-exten => 3,n,Hangup()
-
-exten => 0,1,Goto(${{id_num}},audio)
-exten => 0,n,Hangup()
 
 """
 
@@ -58,8 +73,15 @@ def generar_dialplan(campana):
     params = {
         'fts_campana_id': campana.id,
         'fts_audio_file': '/tmp/sample.wav',
+        'date': str(datetime.datetime.now())
     }
     partes.append(TEMPLATE_DIALPLAN_START.format(**params))
+    
+    # Genera opciones de campana
+    for opcion in campana.opciones.all():
+        partes.append(TEMPLATE_OPCION.format(fts_opcion_id=opcion.id,
+            **params))
+
     partes.append(TEMPLATE_DIALPLAN_END.format(**params))
     
     return ''.join(partes)
