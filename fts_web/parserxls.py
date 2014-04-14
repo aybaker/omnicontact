9d4f26c5db5eb6b85610423a41cb1c0a9d3b01a7
Carlos Ilcobich <cilcobich@puntoangular.com>
2014-04-14 14:28:12 -0300

    FTS-100 Valida read_line para tomar el sniffer de csv.

--- fts_web/parserxls.py
+++ fts_web/parserxls.py
@@ -150,24 +150,26 @@ class ParserCsv(object):
         self.erroneas = 0
         value_list = []
 
-        dialect = csv.Sniffer().sniff(file_obj.readline(), [',', ';', '|', ' '])
-        workbook = csv.reader(file_obj, dialect)
-        for i, curr_row in enumerate(workbook):
-            if not len(curr_row) == 0:
-                value = curr_row[columna_datos].strip()
-                if not len(value) == 0:
-                    value_list.append(value)
+        read_line = file_obj.readline()
+        if read_line:
+            dialect = csv.Sniffer().sniff(read_line, [',', ';', '|', ' '])
+            workbook = csv.reader(file_obj, dialect)
+            for i, curr_row in enumerate(workbook):
+                if not len(curr_row) == 0:
+                    value = curr_row[columna_datos].strip()
+                    if not len(value) == 0:
+                        value_list.append(value)
+                    else:
+                        logger.info("Ignorando valor vacio en fila %s", i)
+                        self.vacias += 1
+                        continue
                 else:
-                    logger.info("Ignorando valor vacio en fila %s", i)
-                    self.vacias += 1
-                    continue
-            else:
-                logger.info("Ignorando fila vacia %s", i)
-                self.erroneas += 1
+                    logger.info("Ignorando fila vacia %s", i)
+                    self.erroneas += 1
 
-        logger.info("%s contactos importados - %s valores ignoradas"
-            " - %s celdas erroneas", len(value_list), self.vacias,
-            self.erroneas)
+            logger.info("%s contactos importados - %s valores ignoradas"
+                " - %s celdas erroneas", len(value_list), self.vacias,
+                self.erroneas)
 
         return value_list
 
@@ -177,15 +179,17 @@ class ParserCsv(object):
         las primeras tres filas.
         """
 
-        dialect = csv.Sniffer().sniff(file_obj.readline(), [',', ';', '|', ' '])
-        workbook = csv.reader(file_obj, dialect)
-        structure_dic = {}
+        read_line = file_obj.readline()
+        if read_line:
+            dialect = csv.Sniffer().sniff(read_line, [',', ';', '|', ' '])
+            workbook = csv.reader(file_obj, dialect)
+            structure_dic = {}
 
-        for i in range(3):
-            try:
-                row = workbook.next()
-                structure_dic.update({i: row})
-            except:
-                pass
+            for i in range(3):
+                try:
+                    row = workbook.next()
+                    structure_dic.update({i: row})
+                except:
+                    pass
 
         return structure_dic
