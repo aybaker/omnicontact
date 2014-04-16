ab2bef97b3b4f8a94e265675ac9c51ce5f01bc69
Horacio G. de Oro <hgdeoro@gmail.com>
2014-04-15 23:22:22 -0300

    FTS-76 Conversor de audio desde modelo funcionando y tests

--- fts_daemon/asterisk_audio.py
+++ fts_daemon/asterisk_audio.py
@@ -7,30 +7,54 @@ Created on Apr 14, 2014
 '''
 
 from __future__ import unicode_literals
-import tempfile
-import subprocess
-import logging as _logging
-from fts_web.models import Campana
-from django.core.files.storage import DefaultStorage
+
 import os
+import subprocess
+import tempfile
+
 from django.conf import settings
+from django.core.files import File
+from django.core.files.storage import default_storage
 from fts_web.errors import FtsAudioConversionError
+from fts_web.models import Campana, upload_to_audios_asterisk
+import logging as _logging
+from fts_web.utiles import resolve_strftime
 
 
 logger = _logging.getLogger(__name__)
 
 
 def convertir_audio_de_campana(campana):
-    """Convierte archivo de audio de campaña"""
+    """Convierte archivo de audio de campaña,
+    y actualiza la instancia de campaña
+    """
     assert isinstance(campana, Campana)
-    storage = DefaultStorage()
-    storage.path(campana.reproduccion.name)
 
-    # FIXME: implementar
-    assert False, "IMPLEMENTAR"
+    wav_full_path = default_storage.path(campana.reproduccion.name)
+    assert os.path.exists(wav_full_path)
+
+    relative_filename = resolve_strftime(upload_to_audios_asterisk(campana,
+        'convertido-campana-{0}.gsm'.format(campana.id)))
+    output_directory_rel, prefix = os.path.split(relative_filename)
+    output_directory = os.path.join(settings.MEDIA_ROOT, output_directory_rel)
+
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    _, output_filename = tempfile.mkstemp(
+        dir=output_directory, prefix=prefix)
+
+    try:
+        convertir_audio(wav_full_path, output_filename, overwrite=True)
+        campana.audio_asterisk = relative_filename
+        campana.save()
+    except:
+        # En caso de error, borramos output
+        os.remove(output_filename)
+        raise
 
 
-def convertir_audio(input_file, output_file):
+def convertir_audio(input_file, output_file, overwrite=False):
     """Convierte archivo de audio de campaña
 
     Parametros:
@@ -41,6 +65,7 @@ def convertir_audio(input_file, output_file):
         FtsAudioConversionError: si se produjo algun tipo de error
     """
 
+    # chequeos...
     if not os.path.exists(input_file):
         logger.error("El archivo de entrada no existe: %s", input_file)
         raise FtsAudioConversionError("El archivo de entrada no existe")
@@ -51,7 +76,7 @@ def convertir_audio(input_file, output_file):
         raise FtsAudioConversionError("El archivo de entrada no es "
             "un path absoluto")
 
-    if os.path.exists(output_file):
+    if overwrite == False and os.path.exists(output_file):
         logger.error("El archivo de salida existe: %s", output_file)
         raise FtsAudioConversionError("El archivo de salida ya existe")
 
@@ -67,7 +92,12 @@ def convertir_audio(input_file, output_file):
         else:
             FTS_AUDIO_CONVERSOR.append(item)
 
+    assert input_file in FTS_AUDIO_CONVERSOR
+    assert output_file in FTS_AUDIO_CONVERSOR
+
+    # ejecutamos comando...
     try:
+        logger.info("Iniciando conversion de audio de %s", input_file)
         subprocess.check_call(FTS_AUDIO_CONVERSOR,
             stdout=stdout_file, stderr=stderr_file)
         logger.info("Conversion de audio finalizada exitosamente")
