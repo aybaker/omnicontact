8bac9e60eab8ba652cb96055ab76edea56a3d9cc
Horacio G. de Oro <hgdeoro@gmail.com>
2014-04-27 17:17:15 -0300

    FTS-92 Implementa get_class() y tests

--- fts_web/utiles.py
+++ fts_web/utiles.py
@@ -11,6 +11,13 @@ import re
 import time
 import uuid
 
+from fts_web.errors import FtsError
+
+from django.conf import settings
+
+import logging as _logging
+
+logger = _logging.getLogger('poll_daemon')
 
 SUBSITUTE_REGEX = re.compile(r'[^a-z\._-]')
 
@@ -33,4 +40,31 @@ def upload_to(prefix, max_length):
 
 def resolve_strftime(text):
     """Ejecuta strftime() en texto pasado por parametro"""
-    return time.strftime(text) # time.gmtime()
+    return time.strftime(text)  # time.gmtime()
+
+
+def get_class(full_class_name):
+    """Devuelve clase referenciada por `full_class_name`"""
+    splitted = full_class_name.split(r".")
+    if len(splitted) < 2:
+        raise FtsError("La clase sepecificada no es valida: '{0}'".format(
+            full_class_name))
+    module_name = ".".join(splitted[0:-1])
+    class_name = splitted[-1]
+    try:
+        module = __import__(module_name)
+        clazz = getattr(module, class_name)
+    except AttributeError as e:
+        msg = "No se pudo obtener la clase '{0}', o el modulo "\
+            "no posee dicha clase".format(full_class_name)
+        logger.warn(msg)
+        raise FtsError(msg, e)
+    except ImportError as e:
+        msg = "No se pudo importar el modulo '{0}'".format(module_name)
+        logger.warn(msg)
+        raise FtsError(msg, e)
+    except Exception as e:
+        msg = "No se pudo obtener la clase '{0}'".format(full_class_name)
+        logger.warn(msg)
+        raise FtsError(msg, e)
+    return clazz
