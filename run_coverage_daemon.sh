e1dd62fd42662d2dba264f0b5712a2ea910af52b
Horacio G. de Oro <hgdeoro@gmail.com>
2014-05-04 21:12:06 -0300

    Crea script para coverage de daemon

--- run_coverage_daemon.sh
+++ run_coverage_daemon.sh
@@ -7,6 +7,6 @@ fi
 
 cd $(dirname $0)
 
-coverage run --omit='fts_web/migrations/*,fts_web/tests/*,fts_web/sample_settings_local.py,fts_web/wsgi.py' --source='fts_web,fts_daemon' manage.py test fts_web fts_tests
+coverage run --omit='fts_web/migrations/*,fts_web/tests/*,fts_web/sample_settings_local.py,fts_web/wsgi.py' --source='fts_daemon' manage.py test fts_tests
 coverage html -d /tmp/fts-coverity
 which gnome-open > /dev/null 2> /dev/null && gnome-open /tmp/fts-coverity/index.html
