58b9cda35f95025dc3f3ab56ba0cb8d724ffd04e
Horacio G. de Oro <hgdeoro@gmail.com>
2014-05-07 15:46:04 -0300

    FTS-135 Mueve AmiStatusTracker a otro modulo

--- fts_daemon/asterisk_ami_http.py
+++ fts_daemon/asterisk_ami_http.py
@@ -4,9 +4,10 @@
 
 from __future__ import unicode_literals
 
-from datetime import datetime
+import collections
 import math
 import os
+import re
 import tempfile
 from xml.parsers.expat import ExpatError
 
@@ -439,6 +440,69 @@ class AsteriskHttpClient(object):
         parser = AsteriskXmlParserForOriginate()
         parser.parse(response_body)
         return parser
+
+
+#==============================================================================
+# AmiStatusTracker
+#==============================================================================
+
+class AmiStatusTracker(object):
+
+    REGEX = re.compile("^Local/([0-9]+)-([0-9]+)@FTS_local_campana_([0-9]+)")
+
+    def __init__(self):
+        pass
+
+    def _parse(self, calls_dicts):
+        """
+        Devuelve:
+        1. dict, cuyo key es un string, con 3 elementos, separados por
+           espacios: [contacto_id, numero, campana_id], y el
+           valor es la lista de registros asociados a este key
+        2. lista, con registros no parseados
+        """
+        parseados = collections.defaultdict(lambda: list())
+        no_parseados = []
+        for item in calls_dicts:
+            if "channel" in item:
+                # Local/28-620@FTS_local_campana
+                match_obj = AmiStatusTracker.REGEX.match(item["channel"])
+                if match_obj:
+                    contacto_id = match_obj.group(1)
+                    numero = match_obj.group(2)
+                    campana_id = match_obj.group(3)
+                    key = " ".join([contacto_id, numero, campana_id])
+                    parseados[key].append(item)
+                else:
+                    no_parseados.append(item)
+            else:
+                no_parseados.append(item)
+        return parseados, no_parseados
+
+    def get_status_por_campana(self):
+        """Devuelve diccionario, cuyos KEYs son los ID de campana,
+        y VALUEs son listas. Cada lista es una lista con:
+        [contacto_id, numero, campana_id]
+        """
+
+        # FIXME: crear cliente, loguear y reutilizar!
+        client = AsteriskHttpClient()
+        client.login()
+        client.ping()
+        calls_dicts = client.get_status().calls_dicts
+        parseados, no_parseados = self._parse(calls_dicts)
+        if no_parseados:
+            logger.warn("Algunos registros no fueron parseados: %s registros",
+                len(no_parseados))
+
+        campanas = collections.defaultdict(lambda: list())
+        for key in parseados:
+            contacto_id, numero, campana_id = key.split()
+            campanas[int(campana_id)].append([
+                int(contacto_id), numero, int(campana_id)
+            ])
+
+        return campanas
 
 
 #==============================================================================
