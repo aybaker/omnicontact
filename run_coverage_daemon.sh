98656c6023383e5eac1f4023aabdc24f6611a2cd
Horacio G. de Oro <hgdeoro@gmail.com>
2014-05-08 00:51:35 -0300

    Update script

--- run_coverage_daemon.sh
+++ run_coverage_daemon.sh
@@ -8,5 +8,5 @@ fi
 cd $(dirname $0)
 
 coverage run --omit='fts_web/migrations/*,fts_web/tests/*,fts_web/sample_settings_local.py,fts_web/wsgi.py' --source='fts_daemon' manage.py test fts_tests
-coverage html -d /tmp/fts-coverity
+coverage html -d /tmp/fts-coverity --title="Coverage para daemon de FTS"
 which gnome-open > /dev/null 2> /dev/null && gnome-open /tmp/fts-coverity/index.html
