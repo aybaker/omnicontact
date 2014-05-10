8f41f5e094ba9c320413934b8f32ca3401e4aff1
Horacio G. de Oro <hgdeoro@gmail.com>
2014-05-10 13:14:12 -0300

    FTS-135 Agrega tipo de baneo

--- fts_daemon/poll_daemon/ban_manager.py
+++ fts_daemon/poll_daemon/ban_manager.py
@@ -15,28 +15,50 @@ class Baneo(object):
     representar a un objeto baneado o no
     """
 
-    def __init__(self, baneada_hasta, contador=1):
-        self.baneada_hasta = baneada_hasta
-        self.contador = contador
-        self.objeto_baneado = True
+    def __init__(self, baneada_hasta, contador=1, reason=None):
+        assert type(baneada_hasta) == datetime
+        assert type(contador) == int
+        self._baneada_hasta = baneada_hasta
+        self._contador = contador
+        self._objeto_esta_baneado = True
+        self._reason = reason
+
+    @property
+    def contador(self):
+        return self._contador
+
+    @property
+    def objeto_esta_baneado(self):
+        return self._objeto_esta_baneado
+
+    @property
+    def reason(self):
+        return self._reason
+
+    @property
+    def baneada_hasta(self):
+        return self._baneada_hasta
 
     def des_banear(self):
         """Elimina el baneo en el objeto"""
-        self.objeto_baneado = False
+        self._objeto_esta_baneado = False
 
-    def re_banear(self, baneada_hasta):
+    def re_banear(self, baneada_hasta, reason=None):
         """Vuelve a banear el objeto, aumentando el contador"""
-        self.objeto_baneado = True
-        self.baneada_hasta = baneada_hasta
-        self.contador += 1
+        assert type(baneada_hasta) == datetime
+        self._objeto_esta_baneado = True
+        self._baneada_hasta = baneada_hasta
+        self._contador += 1
+        if reason is not None:
+            self._reason = reason
 
     def esta_baneado(self):
         """Devuelve True si el objeto esta baneado, sino False"""
-        if not self.objeto_baneado:
+        if not self._objeto_esta_baneado:
             return False
 
         # FIXME: usar TZ-aware datetimes para soportar cambios de TZ
-        if datetime.now() < self.baneada_hasta:
+        if datetime.now() < self._baneada_hasta:
             return True
         else:
             return False
@@ -47,33 +69,38 @@ class BanManager(object):
     objeto hasheable"""
 
     def __init__(self):
-        self.campanas_baneadas = {}
+        self._campanas_baneadas = {}
         """Diccionario que mapea campana (key) a instancia de ``Baneo``
         (que contiene el datetime indicando el momento hasta el cual la
         campaña esta baneada mas el contador de baneos"""
+
+    @property
+    def campanas_baneadas(self):
+        # Devolvemos copia
+        return dict(self._campanas_baneadas)
 
     def get_timedelta_baneo(self):
         """Devuelve tiempo por default de baneo"""
         # TODO: usar time.clock() u alternativa
         return timedelta(minutes=1)
 
-    def banear_campana(self, campana_u_objeto):
-        """Banea una campana"""
+    def banear_campana(self, campana_u_objeto, reason=None):
+        """Banea (o re-banea)  una campana"""
         # TODO: usar time.clock() u alternativa
         # FIXME: usar TZ-aware datetimes para soportar cambios de TZ
 
         baneada_hasta = datetime.now() + self.get_timedelta_baneo()
         try:
-            baneo = self.campanas_baneadas[campana_u_objeto]
-            baneo.re_banear(baneada_hasta)
+            baneo = self._campanas_baneadas[campana_u_objeto]
+            baneo.re_banear(baneada_hasta, reason)
         except KeyError:
-            baneo = Baneo(baneada_hasta)
-            self.campanas_baneadas[campana_u_objeto] = baneo
+            baneo = Baneo(baneada_hasta, reason=reason)
+            self._campanas_baneadas[campana_u_objeto] = baneo
 
     def des_banear(self, campana_u_objeto):
         """Des-banea una campana"""
         try:
-            baneo = self.campanas_baneadas[campana_u_objeto]
+            baneo = self._campanas_baneadas[campana_u_objeto]
             baneo.des_banear()
         except KeyError:
             pass
@@ -81,7 +108,7 @@ class BanManager(object):
     def get_baneo(self, campana_u_objeto):
         """Devuelve el objeto Baneo, o None si no existe"""
         try:
-            return self.campanas_baneadas[campana_u_objeto]
+            return self._campanas_baneadas[campana_u_objeto]
         except KeyError:
             return None
 
@@ -89,13 +116,16 @@ class BanManager(object):
         """Devuelve booleano indicando si la campana esta baneada."""
         # TODO: usar time.clock() u alternativa
         try:
-            baneo = self.campanas_baneadas[campana_u_objeto]
+            baneo = self._campanas_baneadas[campana_u_objeto]
         except KeyError:
             # Campaña no existe, asi q' no esta baneada...
             return False
 
         return baneo.esta_baneado()
 
-    def eliminar(self, campana_u_objeto):
-        """Elimina la informacion de una campana (si existe)."""
-        self.des_banear(campana_u_objeto)
+#    def eliminar(self, campana_u_objeto):
+#        """Elimina la informacion de una campana (si existe)."""
+#        try:
+#            del self._campanas_baneadas[campana_u_objeto]
+#        except KeyError:
+#            pass
