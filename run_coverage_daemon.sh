301d8e5a58bc66a1ce4c7408cdf8de5111d80bb2
Horacio G. de Oro <hgdeoro@gmail.com>
2014-05-12 18:38:17 -0300

    Agrega variables de entorno

--- run_coverage_daemon.sh
+++ run_coverage_daemon.sh
@@ -7,6 +7,10 @@ fi
 
 cd $(dirname $0)
 
-coverage run --omit='fts_web/migrations/*,fts_web/tests/*,fts_web/sample_settings_local.py,fts_web/wsgi.py' --source='fts_daemon' manage.py test fts_tests
+export SKIP_SELENIUM=1
+export FTS_DEBUG=1
+export USE_PG=1
+
+coverage run --omit='fts_web/migrations/*,fts_web/tests/*,fts_web/sample_settings_local.py,fts_web/wsgi.py' --source='fts_daemon' manage.py test fts_tests fts_web
 coverage html -d /tmp/fts-coverity --title="Coverage para daemon de FTS"
 which gnome-open > /dev/null 2> /dev/null && gnome-open /tmp/fts-coverity/index.html
