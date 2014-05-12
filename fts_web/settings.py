0430bb99eb57cd32a685a2814c9b8d0818f62bbe
Horacio G. de Oro <hgdeoro@gmail.com>
2014-05-12 15:05:26 -0300

    Update de settings

--- fts_web/settings.py
+++ fts_web/settings.py
@@ -22,6 +22,11 @@ import os
 DEBUG = False
 TEMPLATE_DEBUG = False
 
+
+#==============================================================================
+# Settings INTERNOS (NO son para customizar el deploy)
+#==============================================================================
+
 FTS_ENHANCED_URLS = False
 """Si FTS_ENHANCED_URLS value true, se sirve /media, /static, etc.
 En produccion deberia ser FALSE
@@ -35,89 +40,13 @@ poder ver el mensaje en el navegador.
 Ver `assertTrueSelenium()` para mas informacion
 """
 
-#FTS_JOIN_TIMEOUT_MARGIN = 5
-#"""Cuantos segundos esperar (mas alla del timeout para ORIGINATE)
-#para que el proceso hijo termine
-#"""
-
 FTS_TESTING_MODE = False
 """Esta variable vale `True` cuando se estan ejecutando los test."""
-
-FTS_DIALPLAN_FILENAME = None
-"""Path completo (absoluto) al archivo donde se debe generar el dialplan
-
-Ejemplos:
-
-.. code-block:: python
-
-    FTS_DIALPLAN_FILENAME = "/opt/asterisk-11/etc/extensions-ftsender.conf"
-    FTS_DIALPLAN_FILENAME = "/opt/asterisk-11/etc/ftsender/extensions.conf"
-"""
-
-FTS_QUEUE_FILENAME = None
-"""Path completo (absoluto) al archivo donde se debe generar queues
-
-Ejemplos:
-
-.. code-block:: python
-
-    FTS_QUEUE_FILENAME = "/opt/asterisk-11/etc/queues-ftsender.conf"
-    FTS_QUEUE_FILENAME = "/opt/asterisk-11/etc/ftsender/queues.conf"
-"""
-
-FTS_RELOAD_CMD = None
-"""Comando a ejecutar para hacer reload de Asterisk
-
-Ejemplo:
-
-.. code-block:: python
-
-    FTS_RELOAD_CMD = ["/usr/bin/asterisk", "-x", "reload"]
-"""
-
-TMPL_FTS_AUDIO_CONVERSOR = None
-"""Comando para convertir audios (wav a gsm)
-
-Ejemplos:
-
-.. code-block:: python
-
-    TMPL_FTS_AUDIO_CONVERSOR = ["sox", "<INPUT_FILE>", "<OUTPUT_FILE>"]
-
-Para transformar WAV (cualquier formato) -> WAV (compatible con Asterisk):
-
-.. code-block:: python
-
-    TMPL_FTS_AUDIO_CONVERSOR = ["sox", "-t", "wav", "<INPUT_FILE>",
-        "-r", "8k", "-c", "1", "-e", "signed-integer",
-        "-t", "wav", "<OUTPUT_FILE>"
-    ]
-
-"""
-
-TMPL_FTS_AUDIO_CONVERSOR_EXTENSION = None
-"""Extension a usar para archivo generado por `TMPL_FTS_AUDIO_CONVERSOR`
-
-Ejemplo: *.wav* (con el . incluido):  el archivo `<OUTPUT_FILE>`
-tendra la extension `.wav`
-"""
-
-FTS_ASTERISK_CONFIG_CHECK_AUDIO_FILE_EXISTS = True
 
 #FTS_ORIGINATE_SERVICE_CLASS = "fts_daemon.asterisk_originate."\
 #    "OriginateServiceProcess"
 
 FTS_ASTERISK_HTTP_CLIENT = "fts_daemon.asterisk_ami_http.AsteriskHttpClient"
-
-FTS_FAST_AGI_DAEMON_PROXY_URL = None
-"""URL donde `fastagi_daemon` realizará el request.
-Debe ser el URL para acceder a Django (SIN el / final)
-
-Ej: http://localhost:8080
-"""
-
-FTS_FAST_AGI_DAEMON_BIND = "0.0.0.0"
-"""Bind de fast agi daemon"""
 
 FTS_PROGRAMAR_CAMPANA_FUNC = "_programar_campana_sqlite"
 
@@ -129,41 +58,28 @@ FTS_EVENTOS_FINALIZADORES = [
 ]
 
 FTS_DUMP_HTTP_AMI_RESPONSES = False
+"""Si se hará un DUMP de cada respuesta, cada XML recibido,
+a un archivo en el directorio temporal."""
 
-FTS_DAEMON_SLEEP_SIN_TRABAJO = 2
-"""Cuantos segundos se esperará en el loop del daemon si no hay más
-trabajo por realizar
-"""
+FTS_SETTING_CUSTOMIZERS = []
 
-FTS_DAEMON_SLEEP_LIMITE_DE_CANALES = 2
-"""Cuantos segundos se esperará en el loop del daemon si todos las
-campañas llegaron al límite de uso de canales
-"""
+#Crispy Forms
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
-FTS_MARGEN_FINALIZACION_CAMPANA = 600
-"""Cuanto tiempo debe pasar (despues del ultimo evento) para
-finalizar una campana
-"""
+TEST_RUNNER = "fts_web.tests.utiles.FTSenderDiscoverRunner"
 
-ASTERISK = {}
-"""Configuracion para interactuar con Asterisk"""
+#==============================================================================
+# Settings de DEPLOY (para ser customizados en distintos deploys)
+#==============================================================================
 
-# ~~~ Ejemplo ~~~
-#ASTERISK = {
-#    'USERNAME': 'xx',
-#    'PASSWORD': 'xx',
-#    'HOST': 'xx.xx.xx.xx',
-#    'PORT': 5038,
-#    'CHANNEL_PREFIX': "IAX2/xx.xx.xx.xx/{numberToCall}",
-#    'LOCAL_CHANNEL':
-#        'Local/{contactoId}-{numberToCall}@FTS_local_campana_{campanaId}',
-#    'CONTEXT': "campania_{campanaId}",
-#    'EXTEN': "fts{contactoId}",
-#    'PRIORITY': 1,
-#    'TIMEOUT': 5,
-#    'HTTP_AMI_URL': 'http://xx.xx.xx.xx:7088',
-#}
+#FTS_JOIN_TIMEOUT_MARGIN = 5
+#"""Cuantos segundos esperar (mas alla del timeout para ORIGINATE)
+#para que el proceso hijo termine
+#"""
 
+#==============================================================================
+# DEPLOY -> Django
+#==============================================================================
 
 ALLOWED_HOSTS = []
 
@@ -227,12 +143,123 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "fts_web.context_processors.testing_mode"
 )
 
-#Crispy Forms
-CRISPY_TEMPLATE_PACK = 'bootstrap3'
+#==============================================================================
+# DEPLOY -> Asterisk
+#==============================================================================
 
-TEST_RUNNER = "fts_web.tests.utiles.FTSenderDiscoverRunner"
+FTS_DIALPLAN_FILENAME = None
+"""Path completo (absoluto) al archivo donde se debe generar el dialplan
 
-FTS_SETTING_CUSTOMIZERS = []
+Ejemplos:
+
+.. code-block:: python
+
+    FTS_DIALPLAN_FILENAME = "/opt/asterisk-11/etc/extensions-ftsender.conf"
+    FTS_DIALPLAN_FILENAME = "/opt/asterisk-11/etc/ftsender/extensions.conf"
+"""
+
+FTS_QUEUE_FILENAME = None
+"""Path completo (absoluto) al archivo donde se debe generar queues
+
+Ejemplos:
+
+.. code-block:: python
+
+    FTS_QUEUE_FILENAME = "/opt/asterisk-11/etc/queues-ftsender.conf"
+    FTS_QUEUE_FILENAME = "/opt/asterisk-11/etc/ftsender/queues.conf"
+"""
+
+FTS_RELOAD_CMD = None
+"""Comando a ejecutar para hacer reload de Asterisk
+
+Ejemplo:
+
+.. code-block:: python
+
+    FTS_RELOAD_CMD = ["/usr/bin/asterisk", "-x", "reload"]
+"""
+
+TMPL_FTS_AUDIO_CONVERSOR = None
+"""Comando para convertir audios (wav a gsm)
+
+Ejemplos:
+
+.. code-block:: python
+
+    TMPL_FTS_AUDIO_CONVERSOR = ["sox", "<INPUT_FILE>", "<OUTPUT_FILE>"]
+
+Para transformar WAV (cualquier formato) -> WAV (compatible con Asterisk):
+
+.. code-block:: python
+
+    TMPL_FTS_AUDIO_CONVERSOR = ["sox", "-t", "wav", "<INPUT_FILE>",
+        "-r", "8k", "-c", "1", "-e", "signed-integer",
+        "-t", "wav", "<OUTPUT_FILE>"
+    ]
+
+"""
+
+TMPL_FTS_AUDIO_CONVERSOR_EXTENSION = None
+"""Extension a usar para archivo generado por `TMPL_FTS_AUDIO_CONVERSOR`
+
+Ejemplo: *.wav* (con el . incluido):  el archivo `<OUTPUT_FILE>`
+tendra la extension `.wav`
+"""
+
+FTS_ASTERISK_CONFIG_CHECK_AUDIO_FILE_EXISTS = True
+
+#==============================================================================
+# DEPLOY -> Daemon
+#==============================================================================
+
+FTS_FAST_AGI_DAEMON_PROXY_URL = None
+"""URL donde `fastagi_daemon` realizará el request.
+Debe ser el URL para acceder a Django (SIN el / final)
+
+Ej: http://localhost:8080
+"""
+
+FTS_FAST_AGI_DAEMON_BIND = "0.0.0.0"
+"""Bind de fast agi daemon"""
+
+FTS_DAEMON_SLEEP_SIN_TRABAJO = 2
+"""Cuantos segundos se esperará en el loop del daemon si no hay más
+trabajo por realizar
+"""
+
+FTS_DAEMON_SLEEP_LIMITE_DE_CANALES = 2
+"""Cuantos segundos se esperará en el loop del daemon si todos las
+campañas llegaron al límite de uso de canales
+"""
+
+#FTS_MARGEN_FINALIZACION_CAMPANA = 600
+#"""Cuanto tiempo debe pasar (despues del ultimo evento) para
+#finalizar una campana
+#"""
+
+#==============================================================================
+# DEPLOY -> Varios
+#==============================================================================
+
+ASTERISK = {}
+"""Configuracion para interactuar con Asterisk"""
+
+# ~~~ Ejemplo ~~~
+#ASTERISK = {
+#    'USERNAME': 'xx',
+#    'PASSWORD': 'xx',
+#    'HOST': 'xx.xx.xx.xx',
+#    'PORT': 5038,
+#    'CHANNEL_PREFIX': "IAX2/xx.xx.xx.xx/{numberToCall}",
+#    'LOCAL_CHANNEL':
+#        'Local/{contactoId}-{numberToCall}@FTS_local_campana_{campanaId}',
+#    'CONTEXT': "campania_{campanaId}",
+#    'EXTEN': "fts{contactoId}",
+#    'PRIORITY': 1,
+#    'TIMEOUT': 5,
+#    'HTTP_AMI_URL': 'http://xx.xx.xx.xx:7088',
+#}
+
 
 #==============================================================================
 # Import de `fts_web_settings_local`
