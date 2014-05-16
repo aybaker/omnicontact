8e69356f7dd8a3532012f813c06906dca439b086
Horacio G. de Oro <hgdeoro@gmail.com>
2014-05-15 22:05:57 -0300

    Mueve migracion a fts_daemon

---
+++ fts_daemon/migrations/0003_fk_constraints_testing.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+#@PydevCodeAnalysisIgnore
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        if 'FTS_DONT_USE_FK_CONSTRAINTS' in os.environ:
+            print("Ignorando migracion: FTS_DONT_USE_FK_CONSTRAINTS")
+            return
+
+        if not settings.FTS_TESTING_MODE:
+            print("Ignorando migracion: no en FTS_TESTING_MODE")
+            return
+
+        pg = 'django.db.backends.postgresql_psycopg2'
+        if settings.DATABASES['default']['ENGINE'] != pg:
+            print("Ignorando migracion: BD no es postgresql")
+            return
+
+        sql = """
+        ALTER TABLE fts_web_eventodecontacto
+            ADD CONSTRAINT contacto_id_fk
+                FOREIGN KEY (contacto_id)
+                    REFERENCES fts_web_contacto (id) MATCH FULL;
+        """
+        db.execute(sql)
+
+        sql = """
+        ALTER TABLE fts_web_eventodecontacto
+            ADD CONSTRAINT campana_id_fk
+                FOREIGN KEY (campana_id)
+                    REFERENCES fts_web_campana (id) MATCH FULL;
+        """
+        db.execute(sql)
+
+    def backwards(self, orm):
+        pass
+
+    models = {
+        u'fts_daemon.eventodecontacto': {
+            'Meta': {'object_name': 'EventoDeContacto'},
+            'campana_id': ('django.db.models.fields.IntegerField', [], {'db_index': 'True'}),
+            'contacto_id': ('django.db.models.fields.IntegerField', [], {'db_index': 'True'}),
+            'dato': ('django.db.models.fields.SmallIntegerField', [], {'db_index': 'True'}),
+            'evento': ('django.db.models.fields.SmallIntegerField', [], {'db_index': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['fts_daemon']
