7aece8d5d1ceb42937916d03703eb1b6929d9696
Horacio G. de Oro <hgdeoro@gmail.com>
2014-05-18 19:03:11 -0300

    Deploy: configuracion de Nginx

--- deploy/roles/ftsender_server/templates/manage.sh
+++ deploy/roles/ftsender_server/templates/manage.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 /home/ftsender/deploy/virtualenv/bin/python \
+    --pythonpath=/home/ftsender/deploy/app \
+    --pythonpath=/home/ftsender/deploy/local \
     /home/ftsender/deploy/app/manage.py $*
