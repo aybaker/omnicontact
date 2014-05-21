54c279b5d0a19a3aad293fe3b1c25cf8f7567a0c
Horacio G. de Oro <hgdeoro@gmail.com>
2014-05-21 09:25:38 -0300

    Agrega management command para chequear sistema

---
+++ fts_daemon/management/commands/ftsdaemon_checks.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import logging
+import os
+import subprocess
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from fts_daemon.asterisk_config import create_queue_config_file, \
+    create_dialplan_config_file
+from fts_daemon.audio_conversor import convertir_audio
+from fts_web.models import Campana
+
+
+AUDIO_FILE = "test/wavs/8k16bitpcm.wav"
+
+OUTPUT_FILENAME = "/tmp/ftsdaemon_check_audio_conversor{0}"
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        logger = logging.getLogger()
+        [logger.removeHandler(x) for x in logger.handlers]
+
+        # NTP
+        self.stdout.write('Chequeando NTP...')
+        retcode = subprocess.call('ntpstat > /dev/null 2> /dev/null',
+            shell=True)
+        if retcode == 0:
+            self.stdout.write(' + OK')
+        else:
+            self.stdout.write(' + ERROR: ntpstat ha devuelto {0}'.format(
+                retcode))
+
+        # BD
+        self.stdout.write('Chequeando acceso a BD...')
+        Campana.objects.exists()
+        self.stdout.write(' + OK')
+
+        # Reload queues de Asterisk
+        self.stdout.write('Chequeando create_queue_config_file()...')
+        create_queue_config_file()
+        self.stdout.write(' + OK')
+
+        # Reload dialplan de Asterisk
+        self.stdout.write('Chequeando create_dialplan_config_file()...')
+        create_dialplan_config_file()
+        self.stdout.write(' + OK')
+
+        # Conversor de audio
+        audio = os.path.dirname(__file__)
+        audio = os.path.abspath(audio)
+        audio = os.path.join(audio, "../../..")
+        audio = os.path.join(audio, AUDIO_FILE)
+        assert os.path.exists(audio)
+        self.stdout.write('Chequeando convertir_audio()...')
+        output = OUTPUT_FILENAME.format(
+            settings.TMPL_FTS_AUDIO_CONVERSOR_EXTENSION)
+        if os.path.exists(output):
+            os.unlink(output)
+        convertir_audio(audio, output)
+        if os.path.exists(output):
+            self.stdout.write(' + OK')
+        else:
+            self.stdout.write(' + ERROR: no se encontro archivo de salida')
