635e120c5443db1511b43b741261265e65c0b945
Horacio G. de Oro <hgdeoro@gmail.com>
2014-05-25 01:38:45 -0300

    FTS-166 Elimina comentario de codigo no usado

--- fts_daemon/models.py
+++ fts_daemon/models.py
@@ -843,14 +843,6 @@ class GestionDeLlamadasManager(models.Manager):
 
         return values
 
-    # TODO: eliminar! ya no se usa
-    #def obtener_ts_ultimo_evento_de_campana(self, id_campana):
-    #    """Devuelve el timestamp del ultimo evento registrado
-    #    para la campaña
-    #    """
-    #    value = self.filter(campana_id=id_campana).aggregate(Max('timestamp'))
-    #    return value['timestamp__max']
-
 
 class EventoDeContacto(models.Model):
     """
