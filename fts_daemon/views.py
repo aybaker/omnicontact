698ae14b4c21c65179e2e6e26e40fee953258f7d
Horacio G. de Oro <hgdeoro@gmail.com>
2014-05-26 15:46:10 -0300

    FTS-175 Migra vistas para que usen psycopg2 poll (paso intermedio para refactorizar AGI-PROXY)

--- fts_daemon/views.py
+++ fts_daemon/views.py
@@ -2,12 +2,53 @@
 
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.http.response import HttpResponse, HttpResponseServerError
 from fts_daemon.models import EventoDeContacto
 import logging as logging_
+from psycopg2 import pool
 
 
 logger = logging_.getLogger(__name__)
+
+CONN_POOL = None
+
+
+def _insert_evento_de_contacto(campana_id, contacto_id, evento, dato):
+    global CONN_POOL
+    cx_pool = CONN_POOL
+    # ESTO ESTA MAL! Pero como paso intermedio del refactor, sirve...
+    # Con un lock mejoraría...
+    if cx_pool is None:
+        assert settings.DATABASES['default']['ENGINE'] == \
+            'django.db.backends.postgresql_psycopg2'
+
+        dsn = "dbname={0} user={1} password={2}".format(
+            settings.DATABASES['default']['NAME'],
+            settings.DATABASES['default']['USER'],
+            settings.DATABASES['default']['PASSWORD']
+        )
+        cx_pool = pool.ThreadedConnectionPool(5, 20, dsn)
+
+    try:
+        conn = cx_pool.getconn()
+        if not conn.autocommit:
+            conn.autocommit = True
+        cur = conn.cursor()
+
+        cur.execute("""INSERT INTO fts_daemon_eventodecontacto
+            (campana_id, contacto_id, timestamp, evento, dato)
+            VALUES
+            (%s, %s, NOW(), %s, %s)
+        """, [campana_id, contacto_id, evento, dato])
+    except:
+        logger.exception("No se pudo insertar EDC")
+    finally:
+        if conn is not None:
+            cx_pool.putconn(conn)
+
+    if CONN_POOL is None:
+        CONN_POOL = cx_pool
 
 
 #==============================================================================
@@ -19,9 +60,11 @@ def local_channel_pre_dial(request, campana_id, contacto_id, intento):
     contacto_id = int(contacto_id)
     intento = int(intento)
 
-    evento_id = EventoDeContacto.objects.dialplan_local_channel_pre_dial(
-        campana_id, contacto_id, intento).id
-    return HttpResponse("OK,{0}".format(evento_id))
+    ev = EventoDeContacto.EVENTO_ASTERISK_DIALPLAN_LOCAL_CHANNEL_INICIADO
+    _insert_evento_de_contacto(campana_id, contacto_id, ev, intento)
+    #    evento_id = EventoDeContacto.objects.dialplan_local_channel_pre_dial(
+    #        campana_id, contacto_id, intento).id
+    return HttpResponse("OK,{0}".format(0))
 
 
 def inicio_campana(request, campana_id, contacto_id, intento):
@@ -29,9 +72,11 @@ def inicio_campana(request, campana_id, contacto_id, intento):
     contacto_id = int(contacto_id)
     intento = int(intento)
 
-    evento_id = EventoDeContacto.objects.dialplan_campana_iniciado(
-        campana_id, contacto_id, intento).id
-    return HttpResponse("OK,{0}".format(evento_id))
+    ev = EventoDeContacto.EVENTO_ASTERISK_DIALPLAN_CAMPANA_INICIADO
+    _insert_evento_de_contacto(campana_id, contacto_id, ev, intento)
+    #    evento_id = EventoDeContacto.objects.dialplan_campana_iniciado(
+    #        campana_id, contacto_id, intento).id
+    return HttpResponse("OK,{0}".format(0))
 
 
 def fin_campana(request, campana_id, contacto_id, intento):
@@ -39,9 +84,11 @@ def fin_campana(request, campana_id, contacto_id, intento):
     contacto_id = int(contacto_id)
     intento = int(intento)
 
-    evento_id = EventoDeContacto.objects.dialplan_campana_finalizado(
-        campana_id, contacto_id, intento).id
-    return HttpResponse("OK,{0}".format(evento_id))
+    ev = EventoDeContacto.EVENTO_ASTERISK_DIALPLAN_CAMPANA_FINALIZADO
+    _insert_evento_de_contacto(campana_id, contacto_id, ev, intento)
+    #    evento_id = EventoDeContacto.objects.dialplan_campana_finalizado(
+    #        campana_id, contacto_id, intento).id
+    return HttpResponse("OK,{0}".format(0))
 
 
 def fin_err_t(request, campana_id, contacto_id, intento):
@@ -49,9 +96,11 @@ def fin_err_t(request, campana_id, contacto_id, intento):
     contacto_id = int(contacto_id)
     intento = int(intento)
 
-    evento_id = EventoDeContacto.objects.fin_err_t(
-        campana_id, contacto_id, intento).id
-    return HttpResponse("OK,{0}".format(evento_id))
+    ev = EventoDeContacto.EVENTO_ASTERISK_DIALPLAN_CAMPANA_ERR_T
+    _insert_evento_de_contacto(campana_id, contacto_id, ev, intento)
+    #    evento_id = EventoDeContacto.objects.fin_err_t(
+    #        campana_id, contacto_id, intento).id
+    return HttpResponse("OK,{0}".format(0))
 
 
 def fin_err_i(request, campana_id, contacto_id, intento):
@@ -59,9 +108,11 @@ def fin_err_i(request, campana_id, contacto_id, intento):
     contacto_id = int(contacto_id)
     intento = int(intento)
 
-    evento_id = EventoDeContacto.objects.fin_err_i(
-        campana_id, contacto_id, intento).id
-    return HttpResponse("OK,{0}".format(evento_id))
+    ev = EventoDeContacto.EVENTO_ASTERISK_DIALPLAN_CAMPANA_ERR_I
+    _insert_evento_de_contacto(campana_id, contacto_id, ev, intento)
+    #    evento_id = EventoDeContacto.objects.fin_err_i(
+    #        campana_id, contacto_id, intento).id
+    return HttpResponse("OK,{0}".format(0))
 
 
 def opcion_seleccionada(request, campana_id, contacto_id, intento,
@@ -87,9 +138,10 @@ def opcion_seleccionada(request, campana_id, contacto_id, intento,
             dtmf_number))
         return HttpResponseServerError("ERROR,opcion_dtmf_invalido")
 
-    evento_id = EventoDeContacto.objects.opcion_seleccionada(
-        campana_id, contacto_id, intento, evento).id
-    return HttpResponse("OK,{0}".format(evento_id))
+    _insert_evento_de_contacto(campana_id, contacto_id, evento, intento)
+    #    evento_id = EventoDeContacto.objects.opcion_seleccionada(
+    #        campana_id, contacto_id, intento, evento).id
+    return HttpResponse("OK,{0}".format(0))
 
 
 def local_channel_post_dial(request, campana_id, contacto_id, intento,
@@ -106,12 +158,13 @@ def local_channel_post_dial(request, campana_id, contacto_id, intento,
         mapped_ev = EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_UNKNOWN
         response_status = "WARN"
         logger.warn("local_channel_post_dial(): valor de DIALSTATUS "
-            "desconocido: '%s' (se guardara evento como"
+            "desconocido: '%s' (se guardara evento como "
             "EVENTO_ASTERISK_DIALSTATUS_UNKNOWN", dial_status)
 
-    evento_id = EventoDeContacto.objects.dialplan_local_channel_post_dial(
-        campana_id, contacto_id, intento, mapped_ev).id
-    return HttpResponse("{0},{1}".format(response_status, evento_id))
+    _insert_evento_de_contacto(campana_id, contacto_id, mapped_ev, intento)
+    #    evento_id = EventoDeContacto.objects.dialplan_local_channel_post_dial(
+    #        campana_id, contacto_id, intento, mapped_ev).id
+    return HttpResponse("{0},{1}".format(response_status, 0))
 
 
 def handle_agi_proxy_request(request, agi_network_script):
