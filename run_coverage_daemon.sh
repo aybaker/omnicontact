e78f38658f82d9be79d077fe400c614fe2335ccc
Horacio G. de Oro <hgdeoro@gmail.com>
2014-05-31 22:11:12 -0300

    FTS-143 Mas tests y script q' ejecuta coverage

--- run_coverage_daemon.sh
+++ run_coverage_daemon.sh
@@ -12,5 +12,7 @@ export FTS_DEBUG=1
 export USE_PG=1
 
 coverage run --omit='fts_daemon/migrations/*,fts_daemon/management/*,fts_web/migrations/*,fts_web/tests/*,fts_web/sample_settings_local.py,fts_web/wsgi.py' --source='fts_daemon' manage.py test fts_tests fts_web
+
 coverage html -d /tmp/fts-coverity --title="Coverage para daemon de FTS"
-which gnome-open > /dev/null 2> /dev/null && gnome-open /tmp/fts-coverity/index.html
+
+which gnome-open > /dev/null 2> /dev/null && gnome-open /tmp/fts-coverity/index.html > /dev/null 2> /dev/null
