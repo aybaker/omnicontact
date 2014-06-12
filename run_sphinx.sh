a8947531e19c913956175f569295fde2ab4f9036
Horacio G. de Oro <hgdeoro@gmail.com>
2014-06-12 17:45:07 -0300

    FTS-233 Update script de sphinx

--- run_sphinx.sh
+++ run_sphinx.sh
@@ -18,6 +18,6 @@ which gnome-open > /dev/null 2> /dev/null && gnome-open _build/html/index.html >
 echo ""
 echo "Para rsync:"
 echo ""
-echo "    rsync -a docs/_build/html deployer@192.168.99.224:/var/www/html/ftsender"
+echo "    rsync -av --delete docs/_build/html deployer@192.168.99.224:/var/www/html/ftsender/docs/"
 echo ""
 
