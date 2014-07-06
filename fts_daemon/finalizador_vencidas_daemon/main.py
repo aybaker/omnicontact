7bf55b78cc8e229b3679e36819c93abe50f8eb4c
Horacio G. de Oro <hgdeoro@gmail.com>
2014-07-06 14:29:39 -0300

    FTS-231 Refactoriza finalizar a clase y agrega unittest

--- fts_daemon/finalizador_vencidas_daemon/main.py
+++ fts_daemon/finalizador_vencidas_daemon/main.py
@@ -8,7 +8,7 @@ from __future__ import unicode_literals
 import time
 
 from django.conf import settings
-from fts_daemon.poll_daemon.call_status import AsteriskCallStatus,\
+from fts_daemon.poll_daemon.call_status import AsteriskCallStatus, \
     CampanaCallStatus
 from fts_web.models import Campana
 import logging as _logging
@@ -19,69 +19,110 @@ import logging as _logging
 logger = _logging.getLogger('fts_daemon.finalizador_vencidas_daemon.main')
 
 
-# TODO: transformar en objeto, para facilitar testing
-def main(max_loop=0, initial_wait=0.0, campana_call_status=None,
-    asterisk_call_status=None, obtener_vencidas_func=None):
-    """Funcion main() para daemon finalizador. Recibe por parametro
-    las dependencias para facilitar el testing.
+class FinalizadorDeCampanas(object):
 
-    :param campana_call_status: instancia de `CampanaCallStatus`
-    :param asterisk_call_status: instancia de `AsteriskCallStatus`
-    :param obtener_vencidas_func: funcion a llamar que devuelve campanas
-                                  vencidas. Por default usamos
-                                  `Campana.objects.
-                                  obtener_vencidas_para_finalizar()`.
-    """
-    logger.info("Iniciando daemon finalizador de campanas vencidas")
-    max_loop = max_loop or settings.FTS_FDCD_MAX_LOOP_COUNT
-    initial_wait = initial_wait or settings.FTS_FDCD_INITIAL_WAIT
+    def __init__(self, max_loop=0, initial_wait=None, campana_call_status=None,
+        asterisk_call_status=None):
+        """Constructor del finalizador de campañas
+        :param campana_call_status: instancia de `CampanaCallStatus`
+        :param asterisk_call_status: instancia de `AsteriskCallStatus`
+        """
 
-    if initial_wait > 0.0:
-        logger.info("Realizando espera inicial de %s segs...", initial_wait)
-        time.sleep(initial_wait)
-        logger.info("Espera inicial finalizada")
+        self.max_loop = max_loop or settings.FTS_FDCD_MAX_LOOP_COUNT
 
-    if campana_call_status is None:
-        campana_call_status = CampanaCallStatus()
+        self.initial_wait = initial_wait
+        if self.initial_wait is None:
+            self.initial_wait = settings.FTS_FDCD_INITIAL_WAIT
 
-    if asterisk_call_status is None:
-        asterisk_call_status = AsteriskCallStatus(campana_call_status)
+        if self.initial_wait > 0.0:
+            logger.info("Realizando espera inicial de %s segs...",
+                        self.initial_wait)
+            time.sleep(self.initial_wait)
+            logger.info("Espera inicial finalizada")
 
-    if obtener_vencidas_func is None:
-        obtener_vencidas_func = Campana.objects.\
-            obtener_vencidas_para_finalizar
+        self.campana_call_status = campana_call_status or CampanaCallStatus()
 
-    current_loop = 1
-    while True:
-        logger.info("Obteniendo status de llamadas en curso")
-        updt_ok = asterisk_call_status.refrescar_channel_status_si_es_posible()
+        self.asterisk_call_status = asterisk_call_status or \
+            AsteriskCallStatus(self.campana_call_status)
 
-        if updt_ok:
-            # El status se actualizo! Avanzamos...
-            for campana in obtener_vencidas_func():
-                count_llamadas_en_curso = campana_call_status.\
-                    get_count_llamadas_de_campana(campana)
+    # METODOS PROXY (para faclitar unittest)
 
-                if count_llamadas_en_curso > 0:
-                    logger.info("No se finalizara la campana %s porque hay "
-                        "%s llamadas en curso de dicha campana",
-                        campana.id, count_llamadas_en_curso)
-                else:
-                    logger.info("Finalizando campana %s", campana.id)
-                    campana.finalizar()
-            else:
-                logger.info("No hay campanas por finalizar...")
-        else:
-            logger.warn("No se pudo refrescar status de llamadas en curso... "
-                "Por lo tanto, no se finalizara ninguna campana")
+    def _refrescar_status(self):
+        """Llama a refrescar_channel_status_si_es_posible().
+        Es funcion 'proxy', para facilitar unittest.
+        """
+        return self.asterisk_call_status.\
+            refrescar_channel_status_si_es_posible()
 
-        if max_loop > 0:
-            current_loop += 1
-            if current_loop > max_loop:
-                logger.info("max_loop alcanzado. Exit!")
-                return
+    def _get_count_llamadas(self, campana):
+        """Llama a get_count_llamadas_de_campana().
+        Es funcion 'proxy', para facilitar unittest.
+        """
+        return self.campana_call_status.get_count_llamadas_de_campana(campana)
 
+    def _finalizar(self, campana):
+        """Finaliza la campaña
+        Es funcion 'proxy', para facilitar unittest.
+        """
+        campana.finalizar()
+
+    def _sleep(self):
+        """Realiza espera luego de finalizar el loop
+        Es funcion 'proxy', para facilitar unittest.
+        """
         time.sleep(settings.FTS_FDCD_LOOP_SLEEP)
 
+    # Metodos reales
+
+    def _obtener_vencidas(self):
+        """Devuelve campañas vencidas que hay que finalizar"""
+        return Campana.objects.obtener_vencidas_para_finalizar()
+
+    def _correr_proceso_de_finalizacion(self):
+        """Ejecuta proceso de finalizacion (busca campanas, las finaliza).
+        Esto debe ejecutarse solo cuando el udpate del status fue exitoso.
+        """
+        campanas = self._obtener_vencidas()
+
+        if not campanas:
+            logger.info("No hay campanas por finalizar...")
+            return
+
+        for campana in campanas:
+            count_llamadas_en_curso = self._get_count_llamadas(campana)
+
+            if count_llamadas_en_curso > 0:
+                logger.info("No se finalizara la campana %s porque "
+                    "hay %s llamadas en curso de dicha campana",
+                    campana.id, count_llamadas_en_curso)
+            else:
+                logger.info("Finalizando campana %s", campana.id)
+                self._finalizar(campana)
+
+    def run(self):
+        """Inicia el proceso."""
+
+        logger.info("Iniciando daemon finalizador de campanas vencidas")
+
+        current_loop = 1
+        while True:
+            logger.info("Obteniendo status de llamadas en curso")
+            updt_ok = self._refrescar_status()
+
+            if updt_ok:
+                self._correr_proceso_de_finalizacion()
+            else:
+                logger.warn("No se pudo refrescar status de llamadas en "
+                    "curso... Por lo tanto, no se finalizara ninguna campana")
+
+            if self.max_loop > 0:
+                current_loop += 1
+                if current_loop > self.max_loop:
+                    logger.info("max_loop alcanzado. Exit!")
+                    return
+
+            self._sleep()
+
+
 if __name__ == '__main__':
-    main()
+    FinalizadorDeCampanas().run()
---
+++ fts_daemon/tests/ut/tests_finalizador_vencidas_daemon.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+"""Tests del modulo fts_daemon.finalizador_vencidas_daemon"""
+
+from __future__ import unicode_literals
+
+from fts_daemon.finalizador_vencidas_daemon.main import FinalizadorDeCampanas
+from fts_web.models import Campana
+from fts_web.tests.utiles import FTSenderBaseTest
+import logging as _logging
+from mock import Mock
+
+
+logger = _logging.getLogger(__name__)
+
+
+class FinalizadorDeCampanasTests(FTSenderBaseTest):
+    """Unit tests de FinalizadorDeCampanas"""
+
+    def test_finaliza_vencida(self):
+        """Testea que finaliza campanas vencidas"""
+        campana = Campana(id=1)
+        finalizador = FinalizadorDeCampanas(max_loop=1, initial_wait=0)
+        finalizador._obtener_vencidas = Mock(return_value=[campana])
+        finalizador._refrescar_status = Mock(return_value=True)
+        finalizador._get_count_llamadas = Mock(return_value=0)
+        finalizador._finalizar = Mock(return_value=None)
+
+        finalizador.run()
+
+        finalizador._obtener_vencidas.assert_called_once_with()
+        finalizador._refrescar_status.assert_called_once_with()
+        finalizador._get_count_llamadas.assert_called_once_with(campana)
+        finalizador._finalizar.assert_called_once_with(campana)
+
+    def test_no_hace_nada_sin_campanas(self):
+        """Testea que no hace nada si no hay vencidas"""
+        finalizador = FinalizadorDeCampanas(max_loop=5, initial_wait=0)
+        finalizador._obtener_vencidas = Mock(return_value=[])
+        finalizador._refrescar_status = Mock(return_value=True)
+        finalizador._get_count_llamadas = Mock(return_value=0)
+        finalizador._finalizar = Mock(return_value=None)
+        finalizador._sleep = Mock(return_value=None)
+
+        finalizador.run()
+
+        self.assertEqual(finalizador._obtener_vencidas.call_count, 5)
+        self.assertEqual(finalizador._refrescar_status.call_count, 5)
+        self.assertEqual(finalizador._get_count_llamadas.call_count, 0)
+        self.assertEqual(finalizador._finalizar.call_count, 0)
