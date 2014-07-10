b244bb0c708f95b017a2ccea00f2119251425fed
Horacio G. de Oro <hgdeoro@gmail.com>
2014-07-10 19:08:47 -0300

    Ignoramos ut en reporte de coverage

--- run_coverage_daemon.sh
+++ run_coverage_daemon.sh
@@ -11,7 +11,7 @@ export SKIP_SELENIUM=1
 export FTS_DEBUG=1
 export USE_PG=1
 
-coverage run --omit='fts_daemon/migrations/*,fts_daemon/management/*,fts_web/migrations/*,fts_web/tests/*,fts_web/sample_settings_local.py,fts_web/wsgi.py' --source='fts_daemon' manage.py test fts_tests fts_web fts_daemon
+coverage run --omit='fts_daemon/migrations/*,fts_daemon/management/*,fts_web/migrations/*,fts_web/tests/*,fts_daemon/tests/*,fts_web/sample_settings_local.py,fts_web/wsgi.py' --source='fts_daemon' manage.py test fts_tests fts_web fts_daemon
 
 coverage html -d /tmp/fts-coverity --title="Coverage para daemon de FTS"
 
