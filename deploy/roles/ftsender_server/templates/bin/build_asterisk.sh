bd5137daea2e19b355b2e87cbf21a9ca529957d8
Horacio G. de Oro <hgdeoro@gmail.com>
2014-07-13 21:09:41 -0300

    Docker: crea variable de Ansible 'BUILD_ASTERISK_CFLAGS'

--- deploy/roles/ftsender_server/templates/bin/build_asterisk.sh
+++ deploy/roles/ftsender_server/templates/bin/build_asterisk.sh
@@ -8,7 +8,7 @@ tar xzf {{ DOWNLOAD_ASTERISK_FILENAME }}
 
 cd {{ DOWNLOAD_ASTERISK_EXTRACT_DIRNAME }}
 
-./configure --prefix=/opt/asterisk-11 CFLAGS="-march=i586"
+./configure --prefix=/opt/asterisk-11 CFLAGS="{{ BUILD_ASTERISK_CFLAGS }}"
 
 make -j 10
 make install
