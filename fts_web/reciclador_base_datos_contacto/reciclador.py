0b7bc0ca3f29c0986d1ba23966629a66bea16025
Carlos Ilcobich <cilcobich@gmail.com>
2014-07-17 18:33:54 -0300

    FTS-246 refactoriza el componente reciclador. Elimina método _obtener_contactos_reciclados.

--- fts_web/reciclador_base_datos_contacto/reciclador.py
+++ fts_web/reciclador_base_datos_contacto/reciclador.py
@@ -16,42 +16,19 @@ class RecicladorBaseDatosContacto(object):
     def _obtener_campana(self, campana_id):
         return Campana.objects.get(pk=campana_id)
 
-    def _obtener_contactos_reciclados(self, campana, tipo_reciclado):
-
-        if int(tipo_reciclado) == Campana.TIPO_RECICLADO_PENDIENTES:
-            contactos_reciclados =\
-                EventoDeContacto.objects_reciclador_contactos.\
-                obtener_contactos_pendientes(campana.pk)
-        elif int(tipo_reciclado) == Campana.TIPO_RECICLADO_OCUPADOS:
-            contactos_reciclados =\
-                EventoDeContacto.objects_reciclador_contactos.\
-                obtener_contactos_ocupados(campana.pk)
-        elif int(tipo_reciclado) == Campana.TIPO_RECICLADO_NO_CONTESTADOS:
-            contactos_reciclados = \
-                EventoDeContacto.objects_reciclador_contactos.\
-                obtener_contactos_no_contestados(campana.pk)
-        elif int(tipo_reciclado) == Campana.TIPO_RECICLADO_NUMERO_ERRONEO:
-            contactos_reciclados = \
-                EventoDeContacto.objects_reciclador_contactos.\
-                obtener_contactos_numero_erroneo(campana.pk)
-        elif int(tipo_reciclado) == Campana.TIPO_RECICLADO_LLAMADA_ERRONEA:
-            contactos_reciclados = \
-                EventoDeContacto.objects_reciclador_contactos.\
-                obtener_contactos_llamada_erronea(campana.pk)
-        else:
-            return None
-        return contactos_reciclados
-
-    def reciclar(self, campana_id, tipo_reciclado):
+    def reciclar(self, campana_id, tipos_reciclado):
         logger.info("Se iniciara el proceso de reciclado de base de datos"
                     " para la campana %s", campana_id)
+
         campana = self._obtener_campana(campana_id)
 
-        if int(tipo_reciclado) == Campana.TIPO_RECICLADO_TOTAL:
+        if int(tipos_reciclado[0]) == Campana.TIPO_RECICLADO_TOTAL:
             return campana.bd_contacto
         else:
-            contactos_reciclados = self._obtener_contactos_reciclados(campana,
-                tipo_reciclado)
+            contactos_reciclados =\
+                EventoDeContacto.objects_reciclador_contactos.\
+                obtener_contactos_reciclados(campana, tipos_reciclado)
+
             if not contactos_reciclados:
                 logger.warn("El reciclado de base datos no arrojo contactos.")
                 raise FtsRecicladoBaseDatosContactoError("No se registraron "
