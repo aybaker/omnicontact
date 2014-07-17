c9d824a9b671374f83008a29e6f61c93b6258ad8
Horacio G. de Oro <hgdeoro@gmail.com>
2014-07-17 09:48:13 -0300

    FTS-248 FIX: usa directorios temporales para dumps

--- fts_daemon/tests/it/tests_finalizadores.py
+++ fts_daemon/tests/it/tests_finalizadores.py
@@ -6,20 +6,29 @@ from __future__ import unicode_literals
 
 import unittest
 
+from django.test.utils import override_settings
 from django.utils.unittest.case import skipIf
 from fts_daemon.services.depurador_de_campana import DepuradorDeCampanaWorkflow
 from fts_web.models import Campana
 from fts_web.tests.utiles import FTSenderBaseTest
 import logging as _logging
 from mock import Mock
+import tempfile
 
 
 logger = _logging.getLogger(__name__)
 
 
+def _tmpdir():
+    """Crea directorio temporal"""
+    return tempfile.mkdtemp(prefix=".fts-tests-", dir="/dev/shm")
+
+
 class DepuradorDeCampanaWorkflowIntegTests(FTSenderBaseTest):
     """Integration tests de DepuradorDeCampanaWorkflow"""
 
+    @override_settings(FTS_BASE_DATO_CONTACTO_DUMP_PATH=_tmpdir(),
+                       MEDIA_ROOT=_tmpdir())
     def test_depura_campana_finalizada(self):
         campana = self.crear_campana_finalizada()
         depurador = DepuradorDeCampanaWorkflow()
