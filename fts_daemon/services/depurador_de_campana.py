4134f965a8e26fd90cef3d2ad534bf9993733eec
Horacio G. de Oro <hgdeoro@gmail.com>
2014-07-16 21:23:56 -0300

    FTS-248 Ajusta depurador

--- fts_daemon/services/depurador_de_campana.py
+++ fts_daemon/services/depurador_de_campana.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Componentes encargados del finalizado de Campañas.
+Servicio que realiza todos los pasos necesarios para la depuración
+de una Campaña finalizada, incluyendo:
+- Recalculo de Agregación de EDC
+- Creación de reporte
+- Depuración de EDC
 """
 
 from __future__ import unicode_literals
@@ -14,39 +18,56 @@ import logging as _logging
 logger = _logging.getLogger(__name__)
 
 
+class CampanaNoPuedeDepurarseError(Exception):
+    pass
+
+
 class DepuradorDeCampanaWorkflow(object):
-    """Realiza los chequeos y pasos necesarios para finalizar una campaña.
-    El metodo `finalizar()` debe ser llamddo, y pasar el ID de la campaña
-    a finalizar por parametro."""
+    """Implementa los pasos necesarios para depurar la campaña."""
 
     def _obtener_campana(self, campana_id):
         return Campana.objects.get(pk=campana_id)
 
-    def _finalizar(self, campana):
-        # assert isinstance(campana, Campana)
-        assert campana.puede_finalizarse()
+    def _depurar(self, campana):
+        if not campana.puede_depurarse():
+            logger.error("La campaña %s NO esta en condiciones de "
+                         "ser depurada", campana.id)
+            raise(CampanaNoPuedeDepurarseError("La campana no esta en "
+                                               "condiciones de ser depurada"))
 
+        # Recalcula agregacion
         campana.recalcular_aedc_completamente()
-        campana.finalizar()
 
-        # campana.procesar_finalizada()
+        # Genera reporte CSV
         campana.crea_reporte_csv()
+
+        # Depura EDC
         EventoDeContacto.objects.depurar_eventos_de_contacto(campana.id)
 
-    def finalizar(self, campana_id):
-        logger.info("Se iniciara el proceso de finalizacion para la camana %s",
+        # Setea la campaña como depurada
+        campana.depurar()
+
+        return True
+
+    def depurar(self, campana_id):
+        """Realiza depuración. Devuelve True si la campaña se depuro.
+        False si no hizo falta depurarla porque la campaña ya estaba depurada.
+
+        :raises CampanaNoPuedeDepurarseError: si el estado de la campaña no
+                                              permite su depuracion.
+        """
+        logger.info("Se iniciara el proceso de depuracion para la camana %s",
                     campana_id)
 
         with transaction.atomic():
-
             campana = self._obtener_campana(campana_id)
-            if campana.estado == Campana.ESTADO_FINALIZADA:
-                logger.info("Ignorando campana ya finalizada: %s", campana_id)
-                return
-
-            self._finalizar(campana)
+            if campana.estado == Campana.ESTADO_DEPURADA:
+                logger.info("Ignorando campana ya depurada: %s", campana.id)
+                return False
+            self._depurar(campana)
 
         # Fuera de la TX, logueamos OK (por si se produce un error
         # al realizar commit)
         logger.info("La campana %s fue finalizada correctamente",
                     campana_id)
+        return True
