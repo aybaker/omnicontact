1074048b358d5c147b582a502fa5b7d1f8e4599e
Carlos Ilcobich <cilcobich@gmail.com>
2014-07-29 12:01:50 -0300

    FTS-269 Agrega DerivacionExterna al admin de django.

--- fts_web/admin.py
+++ fts_web/admin.py
@@ -11,6 +11,7 @@ from fts_web import models
 from fts_daemon import models as daemon_models
 
 
+admin.site.register(models.DerivacionExterna)
 admin.site.register(models.GrupoAtencion)
 admin.site.register(models.AgenteGrupoAtencion)
 
