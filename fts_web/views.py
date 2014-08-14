847c7dbb836d90c1fae61ee637af759775f92fd1
Carlos Ilcobich <cilcobich@gmail.com>
2014-08-13 23:03:14 -0300

    FTS-292 Agrega las vistas para ABML de ArchivoDeAudio.

--- fts_web/views.py
+++ fts_web/views.py
@@ -18,7 +18,8 @@ from django.views.generic import (
     RedirectView, TemplateView)
 from fts_daemon.asterisk_config import create_dialplan_config_file, \
     reload_config, create_queue_config_file
-from fts_daemon.audio_conversor import convertir_audio_de_campana
+from fts_daemon.audio_conversor import (convertir_audio_de_campana,
+    convertir_audio_de_archivo_de_audio_globales)
 from fts_daemon.poll_daemon.statistics import StatisticsService
 from fts_daemon.tasks import esperar_y_depurar_campana_async
 from fts_web.errors import (FtsAudioConversionError,
@@ -28,10 +29,11 @@ from fts_web.errors import (FtsAudioConversionError,
 from fts_web.forms import (
     ActuacionForm, AgentesGrupoAtencionFormSet, AudioForm, CampanaForm,
     CalificacionForm, ConfirmaForm, GrupoAtencionForm, TipoRecicladoForm,
-    BaseDatosContactoForm, OpcionForm, DerivacionExternaForm, TemplateForm)
+    BaseDatosContactoForm, OpcionForm, DerivacionExternaForm, TemplateForm,
+    ArchivoAudioForm)
 from fts_web.models import (
     Actuacion, Calificacion, Campana, GrupoAtencion, DerivacionExterna,
-    BaseDatosContacto, Opcion)
+    BaseDatosContacto, Opcion, ArchivoDeAudio)
 from fts_web.parser import autodetectar_parser
 import logging as logging_
 from fts_web.reciclador_base_datos_contacto.reciclador import (
@@ -1919,6 +1921,150 @@ class DaemonStatusView(TemplateView):
         _update_context_with_statistics(context)
         return context
 
+
+# =============================================================================
+# Archivo Audio
+# =============================================================================
+
+class ArchivoAudioListView(ListView):
+    """
+    Esta vista lista los archivos de audios.
+    """
+
+    template_name = 'archivo_audio/lista_archivo_audio.html'
+    context_object_name = 'audios'
+    model = ArchivoDeAudio
+    queryset = ArchivoDeAudio.objects.all()
+
+
+class ArchivoAudioCreateView(CreateView):
+    """
+    Esta vista crea un objeto ArchivoDeAudio.
+    """
+
+    template_name = 'archivo_audio/nuevo_edita_archivo_audio.html'
+    model = ArchivoDeAudio
+    form_class = ArchivoAudioForm
+
+    def form_valid(self, form):
+        self.object = form.save()
+
+        try:
+            convertir_audio_de_archivo_de_audio_globales(self.object)
+            return redirect(self.get_success_url())
+        except FtsAudioConversionError:
+            self.object.audio_original = None
+            self.object.save()
+
+            message = '<strong>Operación Errónea!</strong> \
+                Hubo un inconveniente en la conversión del audio. Por favor \
+                verifique que el archivo subido sea el indicado.'
+            messages.add_message(
+                self.request,
+                messages.ERROR,
+                message,
+            )
+            return self.form_invalid(form)
+        except Exception, e:
+            self.object.audio_original = None
+            self.object.save()
+
+            logger.warn("convertir_audio_de_archivo_de_audio_globales(): "
+                        "produjo un error inesperado. Detalle: %s", e)
+
+            message = '<strong>Operación Errónea!</strong> \
+                Se produjo un error inesperado en la conversión del audio.'
+            messages.add_message(
+                self.request,
+                messages.ERROR,
+                message,
+            )
+            return self.form_invalid(form)
+
+    def get_success_url(self):
+        return reverse('lista_archivo_audio')
+
+
+class ArchivoAudioUpdateView(UpdateView):
+    """
+    Esta vista edita un objeto ArchivoDeAudio.
+    """
+
+    template_name = 'archivo_audio/nuevo_edita_archivo_audio.html'
+    model = ArchivoDeAudio
+    form_class = ArchivoAudioForm
+
+    def form_valid(self, form):
+        self.object = form.save()
+
+        if self.request.FILES.get('audio_original'):
+            try:
+                convertir_audio_de_archivo_de_audio_globales(self.object)
+                return redirect(self.get_success_url())
+            except FtsAudioConversionError:
+                self.object.audio_original = None
+                self.object.save()
+
+                message = '<strong>Operación Errónea!</strong> \
+                    Hubo un inconveniente en la conversión del audio. Por favor \
+                    verifique que el archivo subido sea el indicado.'
+                messages.add_message(
+                    self.request,
+                    messages.ERROR,
+                    message,
+                )
+                return self.form_invalid(form)
+            except Exception, e:
+                self.object.audio_original = None
+                self.object.save()
+
+                logger.warn("convertir_audio_de_archivo_de_audio_globales(): "
+                    "produjo un error inesperado. Detalle: %s", e)
+
+                message = '<strong>Operación Errónea!</strong> \
+                    Se produjo un error inesperado en la conversión del audio.'
+                messages.add_message(
+                    self.request,
+                    messages.ERROR,
+                    message,
+                )
+                return self.form_invalid(form)
+
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        return reverse('lista_archivo_audio')
+
+
+class ArchivoAudioDeleteView(DeleteView):
+    """
+    Esta vista se encarga de la eliminación del
+    objeto ArchivoDeAudio seleccionado.
+    """
+
+    model = ArchivoDeAudio
+    template_name = 'archivo_audio/elimina_archivo_audio.html'
+    queryset = ArchivoDeAudio.objects.all()
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        success_url = self.get_success_url()
+
+        # Marcamos el grupo de atención como borrado.
+        self.object.borrar()
+
+        message = '<strong>Operación Exitosa!</strong>\
+        Se llevó a cabo con éxito la eliminación del Archivo de Audio.'
+
+        messages.add_message(
+            self.request,
+            messages.SUCCESS,
+            message,
+        )
+        return HttpResponseRedirect(success_url)
+
+    def get_success_url(self):
+        return reverse('lista_archivo_audio')
 
 # =============================================================================
 # Test
