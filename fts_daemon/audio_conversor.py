717239dcf2a88ddd45ed3769f39624001a73fd0b
Horacio G. de Oro <hgdeoro@gmail.com>
2014-08-17 20:47:57 -0300

    FTS-294 Implementa conversion de audio y refactoriza a servcio

--- fts_daemon/audio_conversor.py
+++ fts_daemon/audio_conversor.py
@@ -21,6 +21,170 @@ import uuid
 logger = _logging.getLogger(__name__)
 
 
+class ConversorDeAudioService(object):
+    """Servicio que realiza la conversion de archivos de audio
+    para poder ser usados por Asterisk
+    """
+
+    def _crear_directorios(self, directorio, mode=0755):
+        """Crea directorio (recursivamente) si no existen. Es el equivalente
+        de `mkdir -p`.
+
+        :param directorio: path absoluto al directorio
+        :param mode: modo del archivo (OCTAL)
+        :returns: True si fue creado, False si no fue creado (ya
+                  existia). Puede devolver FALSOS POSITIVOS (o sea,
+                  devuelve True pero en realidad otro proceso lo ha
+                  creado).
+        """
+        assert os.path.isabs(directorio), \
+            "El directorio especificado no es un path absoluto: {0}".format(
+                directorio)
+        if not os.path.exists(directorio):
+            logger.info("Se crearan directorios: %s", directorio)
+            os.makedirs(directorio, mode)
+            return True
+
+        return False
+
+    def _crear_archivo(self, archivo, mode=0644):
+        """Crea archivo vacio si no existe.
+
+        :param archivo: path absoluto al archivo
+        :param mode: modo del archivo (OCTAL)
+        :returns: True si fue creado, False si no fue creado (ya
+                  existia). Puede devolver FALSOS POSITIVOS (o sea,
+                  devuelve True pero en realidad otro proceso lo ha
+                  creado).
+        """
+        assert os.path.isabs(archivo), \
+            "El archivo especificado no es un path absoluto: {0}".format(
+                archivo)
+        if not os.path.exists(archivo):
+            with open(archivo, "a") as _:
+                pass
+            os.chmod(archivo, mode)
+            return True
+
+        return False
+
+    def _convertir_audio(self, input_file_abs, output_filename_abs):
+        """Convierte archivo de audio.
+
+        :param input_file_abs: path absoluto a archivo de entrada (.wav)
+        :type input_file_abs: str
+        :param output_filename_abs: path absoluto a archivo de salida
+        :type output_filename_abs: str
+
+        :raises: FtsAudioConversionError: si se produjo algun tipo de error
+        """
+
+        # chequeos...
+        if not os.path.exists(input_file_abs):
+            logger.error("El archivo de entrada no existe: %s", input_file_abs)
+            raise FtsAudioConversionError("El archivo de entrada no existe")
+
+        if not os.path.abspath(input_file_abs):
+            logger.error("El archivo de entrada no es un path absoluto: %s",
+                input_file_abs)
+            raise FtsAudioConversionError("El archivo de entrada no es "
+                "un path absoluto")
+
+        if not os.path.abspath(output_filename_abs):
+            logger.error("El archivo de salida no es un path absoluto: %s",
+                output_filename_abs)
+            raise FtsAudioConversionError("El archivo de salida no es "
+                "un path absoluto")
+
+        stdout_file = tempfile.TemporaryFile()
+        stderr_file = tempfile.TemporaryFile()
+
+        FTS_AUDIO_CONVERSOR = []
+        for item in settings.TMPL_FTS_AUDIO_CONVERSOR:
+            if item == "<INPUT_FILE>":
+                FTS_AUDIO_CONVERSOR.append(input_file_abs)
+            elif item == "<OUTPUT_FILE>":
+                FTS_AUDIO_CONVERSOR.append(output_filename_abs)
+            else:
+                FTS_AUDIO_CONVERSOR.append(item)
+
+        assert input_file_abs in FTS_AUDIO_CONVERSOR
+        assert output_filename_abs in FTS_AUDIO_CONVERSOR
+
+        # ejecutamos comando...
+        try:
+            logger.info("Iniciando conversion de audio de %s -> %s",
+                input_file_abs, output_filename_abs)
+            subprocess.check_call(FTS_AUDIO_CONVERSOR,
+                stdout=stdout_file, stderr=stderr_file)
+            logger.info("Conversion de audio finalizada exitosamente")
+
+        except subprocess.CalledProcessError as e:
+            logger.warn("Exit status erroneo: %s", e.returncode)
+            logger.warn(" - Comando ejecutado: %s", e.cmd)
+            try:
+                stdout_file.seek(0)
+                stderr_file.seek(0)
+                stdout = stdout_file.read().splitlines()
+                for line in stdout:
+                    if line:
+                        logger.warn(" STDOUT> %s", line)
+                stderr = stderr_file.read().splitlines()
+                for line in stderr:
+                    if line:
+                        logger.warn(" STDERR> %s", line)
+            except:
+                logger.exception("Error al intentar reporter STDERR y STDOUT "
+                    "(lo ignoramos)")
+
+            raise FtsAudioConversionError("Error detectado al ejecutar "
+                                          "conversor", cause=e)
+
+        finally:
+            stdout_file.close()
+            stderr_file.close()
+
+    def convertir_audio_de_archivo_de_audio_globales(self, archivo_de_audio):
+        """Realiza la conversión y actualiza la instancia de ArchivoDeAudio.
+
+        Esta funcion debe usarse en el Alta y Modificacioin de ArchivoDeAudio.
+
+        :param archivo_de_audio: ArchivoDeAudio para la cual hay que convertir
+                                 el audio
+        :type archivo_de_audio: fts_web.models.ArchivoDeAudio
+        :raises: FtsAudioConversionError
+        """
+        from fts_web.models import ArchivoDeAudio
+        assert isinstance(archivo_de_audio, ArchivoDeAudio)
+
+        # chequea archivo original (a convertir)
+        wav_full_path = default_storage.path(
+            archivo_de_audio.audio_original.name)
+        assert os.path.exists(wav_full_path)
+
+        # genera nombre del archivo de salida
+        dirname = "audio_asterisk_predefinido"
+        filename = "audio-predefinido-{0}{1}".format(archivo_de_audio.id,
+            settings.TMPL_FTS_AUDIO_CONVERSOR_EXTENSION)
+
+        # Creamos directorios si no existen
+        abs_output_dir = os.path.join(settings.MEDIA_ROOT, dirname)
+        self._crear_directorios(abs_output_dir)
+
+        # Creamos archivo si no existe
+        abs_output_filename = os.path.join(abs_output_dir, filename)
+        self._crear_archivo(abs_output_filename)
+
+        assert os.path.exists(abs_output_filename)
+
+        # convierte archivo
+        self._convertir_audio(wav_full_path, abs_output_filename)
+
+        # guarda ref. a archivo convertido
+        archivo_de_audio.audio_asterisk = os.path.join(dirname, filename)
+        archivo_de_audio.save()
+
+
 def convertir_audio_de_campana(campana):
     """Convierte archivo de audio de campaña,
     y actualiza la instancia de campaña.
@@ -62,8 +226,14 @@ def convertir_audio_de_archivo_de_audio_globales(archivo_de_audio):
     :type archivo_de_audio: fts_web.models.ArchivoDeAudio
     :raises: FtsAudioConversionError
     """
-    from fts_web.models import ArchivoDeAudio
-    assert isinstance(archivo_de_audio, ArchivoDeAudio)
+    #
+    # Este metodo fue movido a ConversorDeAudioService()
+    # TODO: eliminar este metodo y refactorizar quienes lo usan
+    #       para que usen el servicio
+    #
+    service = ConversorDeAudioService()
+    return service.convertir_audio_de_archivo_de_audio_globales(
+        archivo_de_audio)
 
 
 def obtener_id_archivo_de_audio_desde_path(file_path):
@@ -89,68 +259,10 @@ def convertir_audio(input_file_abs, output_filename_abs):
 
     :raises: FtsAudioConversionError: si se produjo algun tipo de error
     """
-
-    # chequeos...
-    if not os.path.exists(input_file_abs):
-        logger.error("El archivo de entrada no existe: %s", input_file_abs)
-        raise FtsAudioConversionError("El archivo de entrada no existe")
-
-    if not os.path.abspath(input_file_abs):
-        logger.error("El archivo de entrada no es un path absoluto: %s",
-            input_file_abs)
-        raise FtsAudioConversionError("El archivo de entrada no es "
-            "un path absoluto")
-
-    if not os.path.abspath(output_filename_abs):
-        logger.error("El archivo de salida no es un path absoluto: %s",
-            output_filename_abs)
-        raise FtsAudioConversionError("El archivo de salida no es "
-            "un path absoluto")
-
-    stdout_file = tempfile.TemporaryFile()
-    stderr_file = tempfile.TemporaryFile()
-
-    FTS_AUDIO_CONVERSOR = []
-    for item in settings.TMPL_FTS_AUDIO_CONVERSOR:
-        if item == "<INPUT_FILE>":
-            FTS_AUDIO_CONVERSOR.append(input_file_abs)
-        elif item == "<OUTPUT_FILE>":
-            FTS_AUDIO_CONVERSOR.append(output_filename_abs)
-        else:
-            FTS_AUDIO_CONVERSOR.append(item)
-
-    assert input_file_abs in FTS_AUDIO_CONVERSOR
-    assert output_filename_abs in FTS_AUDIO_CONVERSOR
-
-    # ejecutamos comando...
-    try:
-        logger.info("Iniciando conversion de audio de %s -> %s",
-            input_file_abs, output_filename_abs)
-        subprocess.check_call(FTS_AUDIO_CONVERSOR,
-            stdout=stdout_file, stderr=stderr_file)
-        logger.info("Conversion de audio finalizada exitosamente")
-
-    except subprocess.CalledProcessError as e:
-        logger.warn("Exit status erroneo: %s", e.returncode)
-        logger.warn(" - Comando ejecutado: %s", e.cmd)
-        try:
-            stdout_file.seek(0)
-            stderr_file.seek(0)
-            stdout = stdout_file.read().splitlines()
-            for line in stdout:
-                if line:
-                    logger.warn(" STDOUT> %s", line)
-            stderr = stderr_file.read().splitlines()
-            for line in stderr:
-                if line:
-                    logger.warn(" STDERR> %s", line)
-        except:
-            logger.exception("Error al intentar reporter STDERR y STDOUT "
-                "(lo ignoramos)")
-
-        raise FtsAudioConversionError("Error detectado al ejecutar conversor",
-            cause=e)
-
-    finally:
-        stdout_file.close()
-        stderr_file.close()
+    #
+    # Este metodo fue movido a ConversorDeAudioService()
+    # TODO: eliminar este metodo y refactorizar quienes lo usan
+    #       para que usen el servicio
+    #
+    service = ConversorDeAudioService()
+    return service._convertir_audio(input_file_abs, output_filename_abs)
