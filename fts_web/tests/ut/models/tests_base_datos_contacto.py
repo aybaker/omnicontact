ec3bc6f81303dc9b0dc54ac9e9687826f1a27d96
Horacio G. de Oro <hgdeoro@gmail.com>
2014-09-05 17:46:37 -0300

    FTS-318: agrega validaciones y tests a MetadataBaseDatosContacto

--- fts_web/tests/ut/models/tests_base_datos_contacto.py
+++ fts_web/tests/ut/models/tests_base_datos_contacto.py
@@ -13,21 +13,43 @@ from fts_web.tests.utiles import FTSenderBaseTest
 class TestMetadataBaseDatosContacto(FTSenderBaseTest):
     """Clase para testear MetadataBaseDatosContacto"""
 
-    def test_parsea_datos_correctos(self):
-        bd = BaseDatosContacto()
-        bd.metadata = json.dumps({'col_telefono': 6})
-        metadata = bd.get_metadata()
+    def setUp(self):
+        self.metadata = {'col_telefono': 6,
+                         'cant_col': 9}
+        self.metadata_codificada = json.dumps(self.metadata)
 
-        self.assertEqual(metadata.columna_con_telefono, 6)
+    def test_parsea_datos_correctos(self):
+        bd = BaseDatosContacto(pk=1, metadata=self.metadata_codificada)
+
+        self.assertEqual(bd.get_metadata().columna_con_telefono, 6)
+        self.assertEqual(bd.get_metadata().cantidad_de_columnas, 9)
 
     def test_guarda_datos_correctos(self):
-        bd = BaseDatosContacto()
-        bd.metadata = json.dumps({'col_telefono': 6})
+        bd = BaseDatosContacto(pk=1)
         metadata = bd.get_metadata()
+        metadata.cantidad_de_columnas = 4
+        metadata.columna_con_telefono = 3
 
-        metadata.columna_con_telefono = 123
+        self.assertDictEqual(json.loads(bd.metadata), {'col_telefono': 3,
+                                                       'cant_col': 4})
 
-        self.assertDictEqual(json.loads(bd.metadata), {'col_telefono': 123})
+    def test_valida_columna_con_telefono(self):
+        bd = BaseDatosContacto(pk=1)
+        metadata = bd.get_metadata()
+        metadata.cantidad_de_columnas = 4
+        with self.assertRaises(AssertionError):
+            # columna_con_telefono NO puede ser 4
+            metadata.columna_con_telefono = 4
+
+    def test_valida_nombres_de_columnas(self):
+        bd = BaseDatosContacto(pk=1)
+        metadata = bd.get_metadata()
+        metadata.cantidad_de_columnas = 4
+        metadata.nombres_de_columnas = ["a", "b", "c", "d"]
+        with self.assertRaises(AssertionError):
+            metadata.nombres_de_columnas = ["a", "b", "c"]
+        with self.assertRaises(AssertionError):
+            metadata.nombres_de_columnas = ["a", "b", "c", "d", "e"]
 
     def test_genera_valueerror_sin_datos(self):
         with self.assertRaises(ValueError):
