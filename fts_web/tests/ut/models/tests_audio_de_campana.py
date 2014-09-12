ec3bca40a4d3484b396217b825d1573b16b11080
Carlos Ilcobich <cilcobich@gmail.com>
2014-09-12 17:37:51 -0300

    FTS-309 Agrega test para unique orden.

--- fts_web/tests/ut/models/tests_audio_de_campana.py
+++ fts_web/tests/ut/models/tests_audio_de_campana.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 
 import datetime
 
+from django.db import IntegrityError
 from django.utils.unittest.case import skipIf
 from fts_web.models import (AudioDeCampana)
 
@@ -12,14 +13,19 @@ from fts_web.tests.utiles import FTSenderBaseTest
 from mock import Mock, patch
 
 
+class OrdenUniqueTest(FTSenderBaseTest):
+    def test_falla_con_ordenes_iguales(self):
+        campana = self.crear_campana_activa()
+
+
 class ObtieneSiguienteOrdenTest(FTSenderBaseTest):
     def test_devuelve_1(self):
         campana = self.crear_campana_activa()
 
         # -----
-
-        self.assertEqual(AudioDeCampana.objects.obtener_siguiente_orden(
-                         campana.pk), 1)
+        AudioDeCampana.objects.create(campana=campana, orden=1)
+        with self.assertRaises(IntegrityError):
+            AudioDeCampana.objects.create(campana=campana, orden=1)
 
     def test_devuelve_1_a_9(self):
         campana = self.crear_campana_activa()
