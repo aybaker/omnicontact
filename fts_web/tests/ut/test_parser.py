a3182d1cd8402c382f04fc49264d83df1afb5c60
Horacio G. de Oro <hgdeoro@gmail.com>
2014-09-15 13:42:17 -0300

    FIX tests ParserCsv

--- fts_web/tests/ut/test_parser.py
+++ fts_web/tests/ut/test_parser.py
@@ -4,11 +4,12 @@ from __future__ import unicode_literals
 
 from django.test.utils import override_settings
 from fts_web.errors import (FtsParserMinRowError, FtsParserMaxRowError,
-                            FtsParserCsvDelimiterError)
+                            FtsParserCsvDelimiterError,
+    FtsParserCsvImportacionError)
 from fts_web.parser import ParserCsv, validate_telefono, sanitize_number, \
     validate_fechas, validate_horas
 from fts_web.tests.utiles import FTSenderBaseTest, get_test_resource_directory
-from fts_web.models import BaseDatosContacto, MetadataBaseDatosContactoDTO,\
+from fts_web.models import BaseDatosContacto, MetadataBaseDatosContactoDTO, \
     MetadataBaseDatosContacto
 
 
@@ -31,18 +32,35 @@ class GetDialectTest(FTSenderBaseTest):
 class ParserCsvReadFileTests(FTSenderBaseTest):
     """Unit tests de ParserCsv.read_file()"""
 
-    @override_settings(MEDIA_ROOT=get_test_resource_directory())
-    def test_generador_devuelve_datos(self):
+    def _get_bdc_para_planilla_ejemplo_0(self):
+        """Devuelve BDC que se crea con "planilla-ejemplo-0.csv"
         """
-        Se crea este test debido a que la función zip() en caso que el
-        generator de read_file no devuelva datos no falla, y, los test pasarán
-        cuando no deberían. Testeando que el generator de read_file devuelva
-        el primer dato correcto nos aseguramos que los demás test pasen
-        correctamente.
+        nombre_archivo = "planilla-ejemplo-0.csv"
+        BaseDatosContacto.objects.create(nombre="test",
+                                         archivo_importacion=nombre_archivo,
+                                         nombre_archivo_importacion="xx.csv",
+                                         metadata="")
+
+        # Solo hay 1, get() no puede fallar
+        bdc = BaseDatosContacto.objects.all().get()
+        metadata = bdc.get_metadata()
+        assert isinstance(metadata, MetadataBaseDatosContacto)
+        metadata.cantidad_de_columnas = 4
+        metadata.columna_con_telefono = 0
+        metadata.columnas_con_fecha = [2]
+        metadata.columnas_con_hora = [3]
+        metadata.nombres_de_columnas = ["telefono_fijo",
+                                        "nombre",
+                                        "fecha",
+                                        "hora"
+                                        ]
+        metadata.primer_fila_es_encabezado = True
+        bdc.save()
+        return bdc
+
+    def _get_bdc_para_planilla_ejemplo_1(self):
+        """Devuelve BDC que se crea con "planilla-ejemplo-1.csv"
         """
-
-        parser = ParserCsv()
-
         nombre_archivo = "planilla-ejemplo-1.csv"
         BaseDatosContacto.objects.create(nombre="test",
                                          archivo_importacion=nombre_archivo,
@@ -54,143 +72,207 @@ class ParserCsvReadFileTests(FTSenderBaseTest):
         metadata = bdc.get_metadata()
         assert isinstance(metadata, MetadataBaseDatosContacto)
         metadata.cantidad_de_columnas = 3
-        metadata.columna_con_telefono = 0  # o 2?
+        metadata.columna_con_telefono = 0
         metadata.nombres_de_columnas = ["telefono_fijo",
                                         "nombre",
                                         "celular"]
         metadata.primer_fila_es_encabezado = False
         bdc.save()
+        return bdc
 
-        generator = parser.read_file(bdc)
+    def _get_bdc_para_planilla_ejemplo_3(self):
+        """Devuelve BDC que se crea con "planilla-ejemplo-3.csv".
+        Usa columna '2' (3ra columna) para telefonos.
+        OJO: la 5ta fila tenga un telefono invalido.
+        """
+        nombre_archivo = "planilla-ejemplo-3.csv"
+        BaseDatosContacto.objects.create(nombre="test",
+                                         archivo_importacion=nombre_archivo,
+                                         nombre_archivo_importacion="xx.csv",
+                                         metadata="")
 
-        self.assertEqual(next(generator),
-                         ['3543009865', 'lkasdjlfkaf', '0351156219387'])
+        # Solo hay 1, get() no puede fallar
+        bdc = BaseDatosContacto.objects.all().get()
+        metadata = bdc.get_metadata()
+        assert isinstance(metadata, MetadataBaseDatosContacto)
+        metadata.cantidad_de_columnas = 3
+        # Usamos columna '2', que es la que tiene numeros de telefonos
+        #  invalidos en la 5ta fila
+        metadata.columna_con_telefono = 2
+        metadata.nombres_de_columnas = ["telefono_fijo",
+                                        "nombre",
+                                        "celular"]
+        metadata.primer_fila_es_encabezado = False
+        bdc.save()
+        return bdc
 
+    def _get_bdc_para_planilla_ejemplo_4(self):
+        """Devuelve BDC que se crea con "planilla-ejemplo-4.csv"
+        """
+        nombre_archivo = "planilla-ejemplo-4.csv"
+        BaseDatosContacto.objects.create(nombre="test",
+                                         archivo_importacion=nombre_archivo,
+                                         nombre_archivo_importacion="xx.csv",
+                                         metadata="")
+
+        # Solo hay 1, get() no puede fallar
+        bdc = BaseDatosContacto.objects.all().get()
+        metadata = bdc.get_metadata()
+        assert isinstance(metadata, MetadataBaseDatosContacto)
+        metadata.cantidad_de_columnas = 1
+        metadata.columna_con_telefono = 0  # o 2?
+        metadata.nombres_de_columnas = ["telefono_fijo"]
+        metadata.primer_fila_es_encabezado = False
+        bdc.save()
+        return bdc
+
+    @override_settings(MEDIA_ROOT=get_test_resource_directory())
     def test_devuelve_datos_correctos(self):
 
-        lista_datos = [['3543009865', 'lkasdjlfkaf', '0351156219387'],
-                       ['111534509230', 'dkasjflkja', '0351156982639'],
-                       ['2830173491', 'alsdkjfieasdf', '3516983419'],
-                       ['3560127341', 'kahvuahdsfasdfa', '2954638961']]
+        datos_correctos = [['3543009865', 'lkasdjlfkaf', '0351156219387'],
+                           ['111534509230', 'dkasjflkja', '0351156982639'],
+                           ['2830173491', 'alsdkjfieasdf', '3516983419'],
+                           ['3560127341', 'kahvuahdsfasdfa', '2954638961']]
+
+        bdc = self._get_bdc_para_planilla_ejemplo_1()
+
+        parser = ParserCsv()
 
         # -----
 
-        parser = ParserCsv()
-        planilla = self.get_test_resource("planilla-ejemplo-1.csv")
-        datos_parseados = parser.read_file(0, [], [], open(planilla, 'r'))
+        self.assertListEqual(datos_correctos,
+                             [_ for _ in parser.read_file(bdc)])
 
-        for parseados, dato in zip(datos_parseados, lista_datos):
-            self.assertEqual(parseados, dato)
-
+    @override_settings(MEDIA_ROOT=get_test_resource_directory())
     def test_datos_en_una_columna_sin_delimitador(self):
 
-        lista_datos = [['35430098657'],
-                       ['11153450923'],
-                       ['28301734914'],
-                       ['35601273413']]
+        datos_correctos = [['35430098657'],
+                           ['11153450923'],
+                           ['28301734914'],
+                           ['35601273413']]
+
+        bdc = self._get_bdc_para_planilla_ejemplo_4()
+
+        parser = ParserCsv()
 
         # -----
 
+        self.assertListEqual(datos_correctos,
+                             [_ for _ in parser.read_file(bdc)])
+
+    @override_settings(MEDIA_ROOT=get_test_resource_directory())
+    def test_detecta_datos_invalidos(self):
+
+        # El archivo tiene 4 lineas validas, y la 5ta es INVALIDA
+        datos_correctos = [['3543009865', 'lkasdjlfkaf', '0351156219387'],
+                           ['111534509230', 'dkasjflkja', '0351156982639'],
+                           ['2830173491', 'alsdkjfieasdf', '3516983419'],
+                           ['3560127341', 'kahvuahdsfasdfa', '2954638961'],
+                           ]
+
+        bdc = self._get_bdc_para_planilla_ejemplo_3()
+
         parser = ParserCsv()
-        planilla = self.get_test_resource("planilla-ejemplo-4.csv")
-        datos_parseados = parser.read_file(0, [], [], open(planilla, 'r'))
-
-        for parseados, dato in zip(datos_parseados, lista_datos):
-            self.assertEqual(parseados, dato)
-
-    def test_datos_sin_fila_de_titulos(self):
-
-        lista_datos = [['3543009865', 'lkasdjlfkaf', '10/10/2014', '12:00'],
-                       ['111534509230', 'dkasjflkja', '10/10/2014', '12:00'],
-                       ['2830173491', 'alsdkjfieasdf', '10/10/2014', '12:00'],
-                       ['3560127341', 'kahvuahdsfasdfa', 'fecha', 'hora']]
 
         # -----
 
-        parser = ParserCsv()
-        planilla = self.get_test_resource("planilla-ejemplo-0.csv")
-        datos_parseados = parser.read_file(0, [], [], open(planilla, 'r'))
+        datos_parseados = []
+        with self.assertRaises(FtsParserCsvImportacionError):
+            for datos_contacto in parser.read_file(bdc):
+                datos_parseados.append(datos_contacto)
 
-        for parseados, dato in zip(datos_parseados, lista_datos):
-            self.assertEqual(parseados, dato)
+        # Debio devolver los primeros 4 ANTES de generar la excepcion
+        self.assertEquals(len(datos_parseados), 4)
 
-    def test_datos_validos_y_datos_invalidos(self):
-        """Test de csv con datos válidos y datos no válidos.
-        Testea que no importe datos inválidos.
-        """
+        self.assertListEqual(datos_correctos, datos_parseados)
 
-        lista_datos = [['3543009865', 'lkasdjlfkaf', '0351156219387'],
-                       ['111534509230', 'dkasjflkja', '0351156982639'],
-                       ['2830173491', 'alsdkjfieasdf', '3516983419'],
-                       ['3560127341', 'kahvuahdsfasdfa', '2954638961'],
-                       ['283091', 'alsdkjfieasdf', '351619'],
-                       ['127341', 'kahvuahdsfasdfa', '295463']]
-
-        cantidad_datos_validos = 4
-
-        # -----
-
-        parser = ParserCsv()
-        planilla = self.get_test_resource("planilla-ejemplo-3.csv")
-        datos_parseados = parser.read_file(0, [], [], open(planilla, 'r'))
-
-        cantidad_parseados = 0
-        for parseados, dato in zip(datos_parseados, lista_datos):
-            cantidad_parseados += 1
-            self.assertEqual(parseados, dato)
-
-        self.assertEqual(cantidad_parseados, cantidad_datos_validos)
-
-    @override_settings(FTS_MAX_CANTIDAD_CONTACTOS=2)
+    @override_settings(FTS_MAX_CANTIDAD_CONTACTOS=2,
+                       MEDIA_ROOT=get_test_resource_directory())
     def test_limite_max_importacion(self):
-        lista_datos = [['3543009865', 'lkasdjlfkaf', '0351156219387'],
-                       ['111534509230', 'dkasjflkja', '0351156982639'],
-                       ['2830173491', 'alsdkjfieasdf', '3516983419'],
-                       ['3560127341', 'kahvuahdsfasdfa', '2954638961']]
+
+        datos_correctos = [['3543009865', 'lkasdjlfkaf', '0351156219387'],
+                           ['111534509230', 'dkasjflkja', '0351156982639'],
+                           ]
+
+        bdc = self._get_bdc_para_planilla_ejemplo_1()
+
+        parser = ParserCsv()
 
         # -----
 
-        parser = ParserCsv()
-        planilla = self.get_test_resource("planilla-ejemplo-1.csv")
-        datos_parseados = parser.read_file(0, [], [], open(planilla, 'r'))
-
+        datos_parseados = []
         with self.assertRaises(FtsParserMaxRowError):
-            for parseados, dato in zip(datos_parseados, lista_datos):
-                self.assertEqual(parseados, dato)
+            for datos_contacto in parser.read_file(bdc):
+                datos_parseados.append(datos_contacto)
 
+        # Debio devolver los primeros 2 ANTES de generar la excepcion
+        self.assertEquals(len(datos_parseados), 2)
+
+        self.assertListEqual(datos_correctos, datos_parseados)
+
+    @override_settings(MEDIA_ROOT=get_test_resource_directory())
     def test_datos_con_fecha_excluye_invalido(self):
 
-        lista_datos = [['3543009865', 'lkasdjlfkaf', '10/10/2014', '12:00'],
-                       ['111534509230', 'dkasjflkja', '10/10/2014', '12:00'],
-                       ['2830173491', 'alsdkjfieasdf', '10/10/2014', '12:00']]
-        # Excluye:             ]
-        # ['3560127341', 'kahvuahdsfasdfa', 'fecha', 'hora']]
+        datos_correctos = [
+            ['3543009865', 'lkasdjlfkaf', '10/10/2014', '12:00'],
+            ['111534509230', 'dkasjflkja', '10/10/2014', '12:00'],
+            ['2830173491', 'alsdkjfieasdf', '10/10/2014', '12:00']
+        ]
+
+        bdc = self._get_bdc_para_planilla_ejemplo_0()
+        metadata = bdc.get_metadata()
+        # Cambiamos metadata, para q' ignore 'hora' y solo detecte
+        #  error en 'fecha'
+        metadata.columnas_con_hora = []
+        bdc.save()
+
+        parser = ParserCsv()
 
         # -----
 
-        parser = ParserCsv()
-        planilla = self.get_test_resource("planilla-ejemplo-0.csv")
-        datos_parseados = parser.read_file(0, [2], [], open(planilla, 'r'))
+        datos_parseados = []
+        with self.assertRaises(FtsParserCsvImportacionError) as cm:
+            for datos_contacto in parser.read_file(bdc):
+                datos_parseados.append(datos_contacto)
 
-        for parseados, dato in zip(datos_parseados, lista_datos):
-            self.assertEqual(parseados, dato)
+        self.assertEquals(cm.exception.valor_celda, "fecha")
 
+        # Debio devolver los primeros 3 ANTES de generar la excepcion
+        self.assertEquals(len(datos_parseados), 3)
+
+        self.assertListEqual(datos_correctos, datos_parseados)
+
+    @override_settings(MEDIA_ROOT=get_test_resource_directory())
     def test_datos_con_hora_excluye_invalido(self):
 
-        lista_datos = [['3543009865', 'lkasdjlfkaf', '10/10/2014', '12:00'],
-                       ['111534509230', 'dkasjflkja', '10/10/2014', '12:00'],
-                       ['2830173491', 'alsdkjfieasdf', '10/10/2014', '12:00']]
-        # Excluye:             ]
-        # ['3560127341', 'kahvuahdsfasdfa', 'fecha', 'hora']]
+        datos_correctos = [
+            ['3543009865', 'lkasdjlfkaf', '10/10/2014', '12:00'],
+            ['111534509230', 'dkasjflkja', '10/10/2014', '12:00'],
+            ['2830173491', 'alsdkjfieasdf', '10/10/2014', '12:00']
+        ]
+
+        bdc = self._get_bdc_para_planilla_ejemplo_0()
+        metadata = bdc.get_metadata()
+        # Cambiamos metadata, para q' ignore 'fecha' y solo detecte
+        #  error en 'hora'
+        metadata.columnas_con_fecha = []
+        bdc.save()
+
+        parser = ParserCsv()
 
         # -----
 
-        parser = ParserCsv()
-        planilla = self.get_test_resource("planilla-ejemplo-0.csv")
-        datos_parseados = parser.read_file(0, [], [3], open(planilla, 'r'))
+        datos_parseados = []
+        with self.assertRaises(FtsParserCsvImportacionError) as cm:
+            for datos_contacto in parser.read_file(bdc):
+                datos_parseados.append(datos_contacto)
 
-        for parseados, dato in zip(datos_parseados, lista_datos):
-            self.assertEqual(parseados, dato)
+        self.assertEquals(cm.exception.valor_celda, "hora")
+
+        # Debio devolver los primeros 3 ANTES de generar la excepcion
+        self.assertEquals(len(datos_parseados), 3)
+
+        self.assertListEqual(datos_correctos, datos_parseados)
 
 
 class ValidateTelefonoTest(FTSenderBaseTest):
