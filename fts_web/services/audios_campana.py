1ad968863129c79440c0482e42f6dd4f8b7c8496
Carlos Ilcobich <cilcobich@gmail.com>
2014-09-17 12:59:54 -0300

    FTS-310 Implementa los métodos OrdenAudiosCampanaService.sube_audio_una_posisicion y OrdenAudiosCampanaService.baja_audio_una_posisicion.

--- fts_web/services/audios_campana.py
+++ fts_web/services/audios_campana.py
@@ -18,5 +18,40 @@ logger = logging.getLogger(__name__)
 
 class OrdenAudiosCampanaService(object):
 
-    def ordenar_audios_campana(self, audio_de_campana, direccion):
-        pass
+    def sube_audio_una_posisicion(self, audio_de_campana):
+        """
+        Este método intercambia el orden del objeto audio_de_campana pasado
+        por parámetro con el objeto audio_de_campana con el siguiente orden.
+        """
+
+        audio_superior = audio_de_campana.obtener_audio_siguiente()
+
+        orden_audio_de_campana = audio_de_campana.orden
+        orden_audio_de_campana_superior = audio_superior.orden
+
+        audio_de_campana.orden = 0
+        audio_de_campana.save()
+        audio_superior.orden = orden_audio_de_campana
+        audio_superior.save()
+
+        audio_de_campana.orden = orden_audio_de_campana_superior
+        audio_de_campana.save()
+
+    def baja_audio_una_posisicion(self, audio_de_campana):
+        """
+        Este método intercambia el orden del objeto audio_de_campana pasado
+        por parámetro con el objeto audio_de_campana con el anterior orden.
+        """
+
+        audio_inferior = audio_de_campana.obtener_audio_anterior()
+
+        orden_audio_de_campana = audio_de_campana.orden
+        orden_audio_de_campana_inferior = audio_inferior.orden
+
+        audio_de_campana.orden = 0
+        audio_de_campana.save()
+        audio_inferior.orden = orden_audio_de_campana
+        audio_inferior.save()
+
+        audio_de_campana.orden = orden_audio_de_campana_inferior
+        audio_de_campana.save()
