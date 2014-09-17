7f5e7e235c20c037308efb516291b68a8c5b1fb2
Carlos Ilcobich <cilcobich@gmail.com>
2014-09-17 16:39:51 -0300

    FTS-310 Agrega excepción NoSePuedeModificarOrdenError al servicio, y se aplica validación antes de procesar el reordenamiento.

--- fts_web/services/audios_campana.py
+++ fts_web/services/audios_campana.py
@@ -10,10 +10,17 @@ import logging
 import json
 
 from django.conf import settings
+
+from fts_web.errors import FtsError
 from fts_web.models import Campana
 
 
 logger = logging.getLogger(__name__)
+
+
+class NoSePuedeModificarOrdenError(FtsError):
+    """Indica que no se puede inferir los metadatos"""
+    pass
 
 
 class OrdenAudiosCampanaService(object):
@@ -25,6 +32,9 @@ class OrdenAudiosCampanaService(object):
         """
 
         audio_superior = audio_de_campana.obtener_audio_siguiente()
+        if not audio_superior:
+            raise(NoSePuedeModificarOrdenError("No se encontro un siguiente "
+                                               "audio para cambiar el orden."))
 
         orden_audio_de_campana = audio_de_campana.orden
         orden_audio_de_campana_superior = audio_superior.orden
@@ -44,6 +54,10 @@ class OrdenAudiosCampanaService(object):
         """
 
         audio_inferior = audio_de_campana.obtener_audio_anterior()
+        if not audio_inferior:
+            raise(NoSePuedeModificarOrdenError("No se encontro un audio "
+                                               "anterior para cambiar el "
+                                               "orden."))
 
         orden_audio_de_campana = audio_de_campana.orden
         orden_audio_de_campana_inferior = audio_inferior.orden
