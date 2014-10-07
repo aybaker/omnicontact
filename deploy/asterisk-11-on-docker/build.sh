6d29d69815a8416a9a15906d957c55d01bac8677
Horacio G. de Oro <hgdeoro@gmail.com>
2014-10-07 16:12:48 -0300

    Agrega soporte para generacion de CDR a asteriks@docker

--- deploy/asterisk-11-on-docker/build.sh
+++ deploy/asterisk-11-on-docker/build.sh
@@ -2,5 +2,16 @@
 
 cd $(dirname $0)
 
-docker build -t hgdeoro/fts-asterisk-dev .
+if [ ! -e cdr_pgsql.conf ] ; then
+	echo "ERROR: no existe '$(pwd)/cdr_pgsql.conf'"
+	echo "Use 'cdr_pgsql.conf.sample' de referencia para crear 'cdr_pgsql.conf'"
+	exit 1
+fi
 
+if [ ! -e yum.conf.extra ] ; then
+	echo "ERROR: no existe '$(pwd)/yum.conf.extra'"
+	echo "El archivo debe existir, al menos vacio"
+	exit 1
+fi
+
+docker build -t hgdeoro/fts-asterisk-dev .
