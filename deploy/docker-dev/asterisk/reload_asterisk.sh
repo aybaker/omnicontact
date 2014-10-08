ae8adb80157fcc431b2499f1c1182587d92d2288
Horacio G. de Oro <hgdeoro@gmail.com>
2014-10-08 10:45:27 -0300

    FIX scripts de asterisk@docker / fig

--- deploy/docker-dev/asterisk/reload_asterisk.sh
+++ deploy/docker-dev/asterisk/reload_asterisk.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-docker run \
-	-v /opt/data-tsunami/fts/asterisk/conf:/opt/data-tsunami/fts/asterisk/conf \
-	-v /opt/data-tsunami/fts/asterisk/run:/opt/asterisk-11/var/run/asterisk \
-	-ti hgdeoro/fts-asterisk-dev \
-	/opt/asterisk-11/sbin/asterisk -x reload
+BASEDIR=$(cd $(dirname $0); pwd)
+
+. $BASEDIR/../../virtualenv-fig/bin/activate
+
+cd $BASEDIR/..
+
+fig run asterisk /opt/asterisk-11/sbin/asterisk -x reload
+
--- deploy/docker-dev/asterisk/run_asterisk.sh
+++ deploy/docker-dev/asterisk/run_asterisk.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-if [ ! -e /opt/data-tsunami/fts/asterisk/conf/.placeholder ] ; then
-	sudo mkdir -p /opt/data-tsunami/fts/asterisk/conf
-	sudo chown $UID /opt/data-tsunami/fts/asterisk/conf
-	sudo chmod 0755 /opt/data-tsunami/fts/asterisk/conf
-        touch /opt/data-tsunami/fts/asterisk/conf/.placeholder
-fi
+BASEDIR=$(cd $(dirname $0); pwd)
 
-docker run \
-	-p 5060:5060/udp -p 7088:7088 \
-	-v /opt/data-tsunami/fts/asterisk/conf:/opt/data-tsunami/fts/asterisk/conf \
-	-v /opt/data-tsunami/fts/asterisk/run:/opt/asterisk-11/var/run/asterisk \
-	-ti hgdeoro/fts-asterisk-dev
+. $BASEDIR/../../virtualenv-fig/bin/activate
+
+cd $BASEDIR/..
+
+fig run asterisk
 
