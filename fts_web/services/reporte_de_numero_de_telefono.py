d3ca2bc7d4f6d19bb1399dadb348e736e96acebc
Carlos Ilcobich <cilcobich@gmail.com>
2014-10-08 13:29:02 -0300

    FTS-337 Agrega módulo de servicio para el reporte de teléfono.

---
+++ fts_web/services/reporte_de_numero_de_telefono.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+from __future__ import unicode_literals
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class ReporteDeTelefonoService(object):
+    def obtener_reporte(self, numero_telefono):
+        pass
