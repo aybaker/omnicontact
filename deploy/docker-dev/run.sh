b88097bcb6d1c8f4fa58a50501b994d056efdf46
Horacio G. de Oro <hgdeoro@gmail.com>
2014-10-09 00:24:50 -0300

    Ajustes de Asterisk@docker

--- deploy/docker-dev/run.sh
+++ deploy/docker-dev/run.sh
@@ -6,5 +6,14 @@ BASEDIR=$(cd $(dirname $0); pwd)
 
 cd $BASEDIR
 
+if [ ! -e asterisk/conf-runtime/cdr_pgsql.conf ] ; then
+	echo ""
+	echo "*** ERROR: no existe asterisk/conf-runtime/cdr_pgsql.conf"
+	echo ""
+	echo " Creelo, usando 'asterisk/conf-runtime/cdr_pgsql.conf.sample' de referencia"
+	echo ""
+	exit 1
+fi
+
 fig up
 
