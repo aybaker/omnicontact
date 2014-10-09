837e9e5d02cdb06d43d86e09c093c14da0a09f07
Carlos Ilcobich <cilcobich@gmail.com>
2014-10-09 16:33:04 -0300

    FTS-337 Agrega métodos para generar las duraciones de llamadas y remueve lo que ya no se usan.

--- fts_web/services/generador_de_duracion_de_llamadas.py
+++ fts_web/services/generador_de_duracion_de_llamadas.py
@@ -18,7 +18,9 @@ class GeneradorDeDuracionDeLlamandasService(object):
     la campana.
     """
 
-    def _validar_campana_estado_correcto(self, campana):
+    NOMBRE_TABLA_TEMPORAL = "CDR_TEMPORAL"
+
+    def _validar_estado_correcto_de_campana(self, campana):
         """
         Valida que la campana se encuentre en el estado correcto. De lo
         contrario podrían no existir los registros de EventoDeContacto de esta
@@ -29,74 +31,65 @@ class GeneradorDeDuracionDeLlamandasService(object):
 
         return True
 
-    def _obtener_eventos_de_contacto_de_campana(self, campana):
-        """
-        Devuelve los EventoDeContacto de la campana que tienen el evento:
-        EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_ANSWER
+    def _drop_tabla_temporal_si_existe(self):
+        cursor = connection.cursor()
+        sql = """DROP TABLE IF EXISTS %s"""
+        params = [self.NOMBRE_TABLA_TEMPORAL]
 
-        Supone que un contacto de una campana atiende una sola vez, por lo que
-        habrá un registro por contacto que haya atendido.
-        """
+        with log_timing(logger, "_drop_tabla_temporal_si_existe tardo %s seg"):
+            cursor.execute(sql, params)
 
-        eventos_de_contacto =  \
-            EventoDeContacto.objects.\
-            obtener_eventos_de_contacto_para_duracion_llamada(campana.id)
-
-    def _obtener_duracion_de_llamada(self, campana, contacto,
-                                     numero_telefonico):
-        """
-        Devuelve la duración de la llamada obtenida de CDR que genera Asterisk.
-        Se considera que esta configurado con la opción *unanswered en NO.
-        Por lo que *supongo que debería haber un solo registro del contacto de
-        la campana, simempre y cuádo la llamda haya sido atendida.
-
-        *unanswered: Log unanswered calls. Normally, only answered calls result
-        in a CDR. Logging all call attempts can result in a large number of
-        extra call records that most people do not care about. The default
-        value is no.
-        """
+    def _crear_tabla_temporal_desde_cdr(self, campana):
 
         cursor = connection.cursor()
-        sql = """SELECT duration FROM cdr
-                  WHERE dcontext LIKE '%s-%s-%'
-                  AND channel LIKE 'FTS_local_campana_%s'
-                  AND disposition in ('ANSWERED')"""
-        params = [contacto.id, numero_telefonico, campana.id]
+        sql = """CREATE TABLE {0} AS SELECT
+        CAST(substring(channel from '_(\d+)$') as integer) as campana_id,
+        CAST(substring(dcontext from '(\d+)') as integer) as contacto_id,
+        substring(dcontext from '-(\d+)-') as numero_telefono,
+        duration as duracion_en_segundos,
+        FROM cdr
+        WHERE channel = 'FTS_local_campana_%s'
+        """
+        params = [campana.id]
 
-        with log_timing(logger, "_obtener_duracion_de_llamada() tardo %s seg"):
+        with log_timing(logger,
+                        "_crear_tabla_temporal_desde_cdr() tardo %s seg"):
             cursor.execute(sql, params)
-            value = cursor.fetchone()
-        return value[0]
+
+    def _insertar_duracion_de_llamdas(self, campana):
+
+        cursor = connection.cursor()
+        sql = """INSERT INTO fts_web_duraciondellamada
+        (fecha_hora_llamada, duracion_en_segundos, campana_id, numero_telefono)
+        SELECT
+        fts_daemon_eventodecontacto.fecha_hora_llamada,
+        %('tabla_temporal')s.duracion_en_segundos,
+        %('tabla_temporal')s.campana_id,
+        %('tabla_temporal')s.numero_telefono
+        FROM  fts_daemon_eventodecontacto
+        INNER JOIN %('tabla_temporal')s ON
+        fts_daemon_eventodecontacto.contacto_id =
+        %('tabla_temporal')s.contacto_id
+        WHERE fts_daemon_eventodecontacto.campana_id = %('campana_id')s
+        AND fts_daemon_eventodecontacto.evento IN (%('evento_answer')s)
+        """
+        params = [{'tabla_temporal': self.NOMBRE_TABLA_TEMPORAL,
+                   'campana_id': campana.id, 'evento_answer':
+                   EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_ANSWER}]
 
     def generar_duracion_de_llamdas_para_campana(self, campana):
         """
         Genera las DuracionDeLlamada para la campana. Es llamado desde el
         proceso de depuración de la campana.
-        Valida que la campana este en el estado correcto.
-        Obtiene los eventos de contacto para las llamadas contestadas de la
-        campana y por cada evento busca en cdr la duración de esa llamada que
-        genero el evento de contacto.
 
         Supone que el cdr esta completo para la campana y existen todos los
         registros de las llamdas contestadas.
         """
 
-        # Validamos la campana.
-        self._validar_campana_estado_correcto(campana)
+        self._validar_estado_correcto_de_campana(campana)
 
-        # Obtenemos los eventos de contacto de la campana.
-        eventos_de_contacto = self._obtener_eventos_de_contacto_de_campana(
-            campana)
+        self._drop_tabla_temporal_si_existe()
 
-        for evento_de_contacto in eventos_de_contacto:
-            contacto = Contacto.object.get(pk=evento_de_contacto.contacto_id)
-            numero_telefonico, _ = contacto.obtener_telefono_y_datos_extras()
+        self._crear_tabla_temporal_desde_cdr(campana)
 
-            duracion_en_segundos = self._obtener_duracion_de_llamada(
-                campana.id, contacto.id, numero_telefonico)
-
-            DuracionDeLlamada.objects.create(
-                campana_id=evento_de_contacto.campana_id,
-                numero_telefonico_contacto=numero_telefonico,
-                fecha_hora_llamada=evento_de_contacto.timestamp,
-                duracion_en_segundos=duracion_en_segundos)
+        self._insertar_duracion_de_llamdas(campana)
