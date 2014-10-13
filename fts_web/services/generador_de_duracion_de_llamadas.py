e98258df003eb305fb7779cd1996b923782180f6
Carlos Ilcobich <cilcobich@gmail.com>
2014-10-13 17:22:27 -0300

    FTS-337 Cambia el nombre de la tabla temporal para cdr de una costante a estar directamente en la sentencia sql.

--- fts_web/services/generador_de_duracion_de_llamadas.py
+++ fts_web/services/generador_de_duracion_de_llamadas.py
@@ -2,10 +2,12 @@
 
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.db import connection
 from django.db import transaction
 from fts_daemon.models import EventoDeContacto
 from fts_web.models import Campana, Contacto, DuracionDeLlamada
+from fts_web.utiles import log_timing
 import logging as _logging
 
 
@@ -17,8 +19,6 @@ class GeneradorDeDuracionDeLlamandasService(object):
     Genera los registros de DuracionDeLlamada para las llamdas realizadas en
     la campana.
     """
-
-    NOMBRE_TABLA_TEMPORAL = "CDR_TEMPORAL"
 
     def _validar_estado_correcto_de_campana(self, campana):
         """
@@ -32,25 +32,26 @@ class GeneradorDeDuracionDeLlamandasService(object):
         return True
 
     def _drop_tabla_temporal_si_existe(self):
+
         cursor = connection.cursor()
-        sql = """DROP TABLE IF EXISTS %s"""
-        params = [self.NOMBRE_TABLA_TEMPORAL]
+        sql = """DROP TABLE IF EXISTS  fts_custom_cdr_temporal"""
 
         with log_timing(logger, "_drop_tabla_temporal_si_existe tardo %s seg"):
-            cursor.execute(sql, params)
+            cursor.execute(sql)
 
     def _crear_tabla_temporal_desde_cdr(self, campana):
 
         cursor = connection.cursor()
-        sql = """CREATE TABLE {0} AS SELECT
+        sql = """CREATE TABLE fts_custom_cdr_temporal AS SELECT
         CAST(substring(channel from '_(\d+)$') as integer) as campana_id,
         CAST(substring(dcontext from '(\d+)') as integer) as contacto_id,
         substring(dcontext from '-(\d+)-') as numero_telefono,
-        duration as duracion_en_segundos,
-        FROM cdr
-        WHERE channel = 'FTS_local_campana_%s'
-        """
-        params = [campana.id]
+        duration as duracion_en_segundos
+        FROM {0}
+        WHERE channel = 'FTS_local_campana_%(campana_id)s'
+        AND disposition IN ('ANSWERED')
+        """.format(settings.FTS_NOMBRE_TABLA_CDR)
+        params = {'campana_id': campana.id}
 
         with log_timing(logger,
                         "_crear_tabla_temporal_desde_cdr() tardo %s seg"):
@@ -62,20 +63,23 @@ class GeneradorDeDuracionDeLlamandasService(object):
         sql = """INSERT INTO fts_web_duraciondellamada
         (fecha_hora_llamada, duracion_en_segundos, campana_id, numero_telefono)
         SELECT
-        fts_daemon_eventodecontacto.fecha_hora_llamada,
-        %('tabla_temporal')s.duracion_en_segundos,
-        %('tabla_temporal')s.campana_id,
-        %('tabla_temporal')s.numero_telefono
+        fts_daemon_eventodecontacto.timestamp,
+        fts_custom_cdr_temporal.duracion_en_segundos,
+        fts_custom_cdr_temporal.campana_id,
+        fts_custom_cdr_temporal.numero_telefono
         FROM  fts_daemon_eventodecontacto
-        INNER JOIN %('tabla_temporal')s ON
+        INNER JOIN fts_custom_cdr_temporal ON
         fts_daemon_eventodecontacto.contacto_id =
-        %('tabla_temporal')s.contacto_id
-        WHERE fts_daemon_eventodecontacto.campana_id = %('campana_id')s
-        AND fts_daemon_eventodecontacto.evento IN (%('evento_answer')s)
+        fts_custom_cdr_temporal.contacto_id
+        WHERE fts_daemon_eventodecontacto.campana_id = %(campana_id)s
+        AND fts_daemon_eventodecontacto.evento IN (%(evento_answer)s)
         """
-        params = [{'tabla_temporal': self.NOMBRE_TABLA_TEMPORAL,
-                   'campana_id': campana.id, 'evento_answer':
-                   EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_ANSWER}]
+        params = {'campana_id': campana.id, 'evento_answer':
+                  EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_ANSWER}
+
+        with log_timing(logger,
+                        "_insertar_duracion_de_llamdas() tardo %s seg"):
+            cursor.execute(sql, params)
 
     def generar_duracion_de_llamdas_para_campana(self, campana):
         """
