b3d302dc1de9375bb19ef2f5bb237ce3e8b9f36c
Horacio G. de Oro <hgdeoro@gmail.com>
2014-10-13 22:20:38 -0300

    Agrega test de referencia explicando como testear servicios

--- fts_web/tests/boundary/tests_mock_boundaries.py
+++ fts_web/tests/boundary/tests_mock_boundaries.py
@@ -29,7 +29,10 @@ class ServicioB(object):
         raise(UnaExcepcion())
 
 
-class TestMockeoDeServicios(TestCase):
+class TestAntipatternsMockeoDeServicios(TestCase):
+    """Demuestra formas peligrosas de utilizar mocks, con ejemplos
+    de tests que pasan, pero NO debrian pasar
+    """
 
     def test_problemas_con_mock(self):
 
@@ -56,6 +59,14 @@ class TestMockeoDeServicios(TestCase):
         # 3. no reporta problemas de atributos inexistentes
         servicio_b.servicio_que_no_existe = Mock()
         servicio_b.servicio_que_no_existe.some_method_que_no_existe()
+
+
+class TestMockConAutospec(TestCase):
+    """Tests de comportamiento de 'create_autospec()'.
+
+    Basicamente crea una instancia con todos sus metodos mockeados.
+    Por lo tanto, no se podra ejecutar ninguno de los metodos reales.
+    """
 
     def test_create_autospec_mockea_metodos(self):
 
@@ -110,5 +121,55 @@ class TestMockeoDeServicios(TestCase):
         # seteamos `return_value`
         servicio_b.servicio_a.some_method_a.return_value = 'd-e-f'
 
-        self.assertEquals(servicio_b.servicio_a.some_method_a("VAL"),
-                          'd-e-f')
+        self.assertEquals(servicio_b.servicio_a.some_method_a("VAL"), 'd-e-f')
+
+
+class TestMockeoDeServicio(TestCase):
+    """Testea todos los casos que debe soportar una buena implementacion
+    de Mock para mockear servicios.
+    """
+
+    def setUp(self):
+        self.servicio_b = ServicioB()
+        self.servicio_b = Mock(spec_set=self.servicio_b, wraps=self.servicio_b)
+
+    def test_funcionalidad_original_puede_ser_llamada(self):
+        with self.assertRaises(UnaExcepcion):
+            self.servicio_b.some_method_b("")
+
+    def test_respeta_method_signatures(self):
+        with self.assertRaises(TypeError):
+            self.servicio_b.some_method_b()
+
+        with self.assertRaises(TypeError):
+            self.servicio_b.some_method_b("1", "2")
+
+    def test_falla_si_intenta_setear_atributo_inexistente(self):
+        with self.assertRaises(AttributeError):
+            self.servicio_b.some_method_que_no_existe = Mock()
+
+    def test_permite_usar_returns_value(self):
+        self.servicio_b.some_method_b.return_value = 'abc'
+        value = self.servicio_b.some_method_b()
+        self.assertEquals(value, 'abc')
+
+    def test_permite_usar_side_effect(self):
+        self.servicio_b.some_method_b.side_effect = KeyError()
+        with self.assertRaises(KeyError):
+            self.servicio_b.some_method_b("")
+
+    def test_permite_mockear_metodo_de_servicio_de_servicio(self):
+        with self.assertRaises(UnaExcepcion):
+            self.servicio_b.servicio_a.some_method_a("")
+
+        #
+        # ATENCION! Al llamar a `servicio_a.some_method_a()` SIN PARAMETROS,
+        #   ESTA LLAMADA DEBERIA FALLAR! ¿Por que no se estan controlando
+        #   la validez de los parametros?
+        #
+        # Lo mas raro es que, ANTES de cambiar 'rteurn_value', si no se
+        #   especifican los parametros requeridos, falla!
+        #
+        self.servicio_b.servicio_a.some_method_a.return_value = 'abc'
+        value = self.servicio_b.servicio_a.some_method_a()
+        self.assertEquals(value, 'abc')
