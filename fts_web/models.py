0ade7540bd5f968eba92bb0d0a0c274099eb0303
Carlos Ilcobich <cilcobich@gmail.com>
2014-10-14 14:17:08 -0300

    FTS-335 Agrega manager y método a DuracionDeLlamada.

--- fts_web/models.py
+++ fts_web/models.py
@@ -2260,9 +2260,23 @@ class ArchivoDeAudio(models.Model):
 # DuracionDeLlamada
 #==============================================================================
 
+class DuracionDeLlamadaManager(models.Manager):
+    """Manager para DuracionDeLlamada"""
+
+    def obtener_duracion_de_llamdas(self, numero_telefono):
+        return self.filter(numero_telefono=numero_telefono)
+
+
 class DuracionDeLlamada(models.Model):
     """Representa la duración de las llamdas de las campanas, con el fin
         de contar con los datos para búsquedas y estadísticas"""
+
+    objects_default = models.Manager()
+    # Por defecto django utiliza el primer manager instanciado. Se aplica al
+    # admin de django, y no aplica las customizaciones del resto de los
+    # managers que se creen.
+
+    objects = DuracionDeLlamadaManager()
 
     campana = models.ForeignKey('Campana')
 
