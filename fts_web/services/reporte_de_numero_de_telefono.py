e473f543837bc245f39d78730c5a9d1493dab770
Carlos Ilcobich <cilcobich@gmail.com>
2014-10-16 19:14:22 -0300

    FTS-337 Agrega dto para la transferencia de las llamadas encontradas.

--- fts_web/services/reporte_de_numero_de_telefono.py
+++ fts_web/services/reporte_de_numero_de_telefono.py
@@ -5,11 +5,14 @@ from __future__ import unicode_literals
 
 import logging
 import re
+import json
 
 logger = logging.getLogger(__name__)
 
 from fts_web.errors import FtsError
 from fts_web.models import DuracionDeLlamada
+
+from fts_daemon.models import EventoDeContacto
 
 
 class NumeroDeTelefonoInvalidoError(FtsError):
@@ -42,8 +45,36 @@ class ReporteDeTelefonoService(object):
     def obtener_reporte(self, numero_telefono):
         self._valida_numero_telefono(numero_telefono)
 
-        reporte_telefono = \
+        duracion_de_llamadas = \
             self._obtener_duracion_de_llamadas_de_numero_telefono(
                 numero_telefono)
 
-        return reporte_telefono
+        listado_de_llamadas = []
+        for duracion_de_llamada in duracion_de_llamadas:
+            listado_de_llamadas.append(ReporteDeTelefonoDTO(
+                                       duracion_de_llamada))
+        return listado_de_llamadas
+
+
+class ReporteDeTelefonoDTO(object):
+    def __init__(self, duracion_de_llamada):
+        self.duracion_de_llamada = duracion_de_llamada
+        self.opciones_seleccionadas = self._obtener_opciones_seleccionadas()
+
+    def _obtener_opciones_seleccionadas(self):
+        eventos_del_contacto = json.loads(
+            self.duracion_de_llamada.eventos_del_contacto)
+
+        opciones_seleccionas = []
+        for evento in eventos_del_contacto:
+            digito_seleccionado = \
+                EventoDeContacto.EVENTO_A_NUMERO_OPCION_MAP[evento]
+
+            opcion_selectionada =\
+                self.duracion_de_llamada.campana.opciones.get(
+                    digito=digito_seleccionado)
+
+            opciones_seleccionas.append(
+                opcion_selectionada.get_descripcion_de_opcion())
+
+        return opciones_seleccionas
