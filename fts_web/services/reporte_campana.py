61b389ecdfa5488abc8a6a9dcdbf9167a62feb54
Carlos Ilcobich <cilcobich@gmail.com>
2014-11-04 14:39:59 -0300

    FTS-347 Refactor reporte de campana.

--- fts_web/services/reporte_campana.py
+++ fts_web/services/reporte_campana.py
@@ -14,48 +14,60 @@ import json
 from django.conf import settings
 from fts_web.models import Campana
 from fts_web.utiles import crear_archivo_en_media_root
+
+from fts_daemon.models import EventoDeContacto
+
 from django.utils.encoding import force_text
 
 
 logger = logging.getLogger(__name__)
 
 
-class ReporteCampanaService(object):
+class ArchivoDeReporteCsv(object):
+    def __init__(self, campana):
+        self._campana = campana
+        self.nombre_del_directorio = 'reporte_campana'
+        self.prefijo_nombre_de_archivo = "{0}-reporte".format(self._campana.id)
+        self.sufijo_nombre_de_archivo = ".csv"
+        self.nombre_de_archivo = "{0}{1}".format(
+            self.prefijo_nombre_de_archivo, self.sufijo_nombre_de_archivo)
+        self.url_descarga = os.path.join(settings.MEDIA_URL,
+                                         self.nombre_del_directorio,
+                                         self.nombre_de_archivo)
+        self.ruta = os.path.join(settings.MEDIA_ROOT,
+                                 self.nombre_del_directorio,
+                                 self.nombre_de_archivo)
 
-    def crea_reporte_csv(self, campana):
-        from fts_daemon.models import EventoDeContacto
+    def ya_existe(self):
+        if os.path.exists(self.ruta):
+            return True
+        return False
 
-        assert campana.estado == Campana.ESTADO_FINALIZADA
-
-        dirname = 'reporte_campana'
-        filename = "{0}-reporte.csv".format(campana.id)
-        file_path = os.path.join(settings.MEDIA_ROOT, dirname, filename)
-
-        file_url = "{0}{1}/{2}".format(settings.MEDIA_URL, dirname, filename)
-
-        if os.path.exists(file_path):
+    def crear_archivo_en_directorio(self):
+        if self.ya_existe():
             # Esto puede suceder si en un intento previo de depuracion, el
             # proceso es abortado, y por lo tanto, el archivo puede existir.
-            logger.warn("crea_reporte_csv(): Ya existe archivo CSV de "
+            logger.warn("ArchivoDeReporteCsv: Ya existe archivo CSV de "
                         "reporte para la campana %s. Archivo: %s. "
-                        "El archivo sera sobreescrito", campana.pk, file_path)
+                        "El archivo sera sobreescrito", self._campana.pk,
+                        self.ruta)
 
-        dirname, filename = crear_archivo_en_media_root(
-            dirname, "{0}-reporte".format(campana.id), ".csv")
+        crear_archivo_en_media_root(
+            self.nombre_del_directorio,
+            self.prefijo_nombre_de_archivo,
+            self.sufijo_nombre_de_archivo)
 
-        contacto_opciones = EventoDeContacto.objects_estadisticas\
-            .obtener_opciones_por_contacto(campana.pk)
-
-        with open(file_path, 'wb') as csvfile:
+    def escribir_archivo_csv(self, opciones_por_contacto):
+        with open(self.ruta, 'wb') as csvfile:
             # Creamos encabezado
             encabezado = []
 
-            cantidad_datos = len(json.loads(contacto_opciones[0][0]))
+            cantidad_datos = len(json.loads(opciones_por_contacto[0][0]))
             for c in range(cantidad_datos):
                 encabezado.append("Extra{0}".format(c+1))
 
             opciones_dict = dict([(op.digito, op.get_descripcion_de_opcion())
-                                 for op in campana.opciones.all()])
+                                 for op in self._campana.opciones.all()])
 
             for opcion in range(10):
                 try:
@@ -72,7 +84,7 @@ class ReporteCampanaService(object):
             csvwiter.writerow(lista_encabezados_utf8)
 
             # guardamos datos
-            for contacto, lista_eventos in contacto_opciones:
+            for contacto, lista_eventos in opciones_por_contacto:
                 lista_opciones = []
                 for dato in json.loads(contacto):
                     lista_opciones.append(dato)
@@ -88,21 +100,33 @@ class ReporteCampanaService(object):
                                        for item in lista_opciones]
                 csvwiter.writerow(lista_opciones_utf8)
 
-        return file_url
+
+class ReporteCampanaService(object):
+
+    def _obtener_opciones_por_contacto(self, campana):
+        opciones_por_contacto = EventoDeContacto.objects_estadisticas\
+            .obtener_opciones_por_contacto(campana.pk)
+        return opciones_por_contacto
+
+    def crea_reporte_csv(self, campana):
+        assert campana.estado == Campana.ESTADO_FINALIZADA
+
+        archivo_de_reporte = ArchivoDeReporteCsv(campana)
+
+        archivo_de_reporte.crear_archivo_en_directorio()
+
+        opciones_por_contacto = self._obtener_opciones_por_contacto(campana)
+
+        archivo_de_reporte.escribir_archivo_csv(opciones_por_contacto)
 
     def obtener_url_reporte_csv_descargar(self, campana):
         assert campana.estado == Campana.ESTADO_DEPURADA
 
-        dirname = 'reporte_campana'
-        filename = "{0}-reporte.csv".format(campana.id)
-        file_path = "{0}/{1}/{2}".format(settings.MEDIA_ROOT, dirname,
-                                         filename)
-        file_url = "{0}{1}/{2}".format(settings.MEDIA_URL, dirname,
-                                        filename)
-        if os.path.exists(file_path):
-            return file_url
+        archivo_de_reporte = ArchivoDeReporteCsv(campana)
+        if archivo_de_reporte.ya_existe():
+            return archivo_de_reporte.url_descarga
 
         # Esto no debería suceder.
         logger.error("obtener_url_reporte_csv_descargar(): NO existe archivo"
                      " CSV de descarga para la campana %s", campana.pk)
-        assert os.path.exists(file_path)
+        assert os.path.exists(archivo_de_reporte.url_descarga)
