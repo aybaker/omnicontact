1fc6797b8e13915c7c4891d75203c5a15eafd61f
Horacio G. de Oro <hgdeoro@gmail.com>
2015-04-27 12:56:35 -0300

    Implementa integration tests de proceso de creacin de DB de contactos

---
+++ fts_daemon/tests/it/tests_crear_bd_contacto_desde_csv.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+"""
+Test de integracion de la creación de BD de contactos desde archivos CSV.
+"""
+
+from __future__ import unicode_literals
+
+import logging as _logging
+
+from django.test.utils import override_settings
+
+from fts_web.models import BaseDatosContacto
+from fts_web.parser import ParserCsv
+from fts_web.services.base_de_datos_contactos import PredictorMetadataService, CreacionBaseDatosService
+from fts_web.tests.utiles import FTSenderBaseTest, get_test_resource_directory
+
+
+logger = _logging.getLogger(__name__)
+
+
+class TestWorkflowCreacionBdContactoDesdeCsv(FTSenderBaseTest):
+
+    @override_settings(MEDIA_ROOT=get_test_resource_directory())
+    def test_planilla_ejemplo(self):
+        bd_contacto = BaseDatosContacto.objects.create(
+            nombre="base-datos-contactos",
+            archivo_importacion=self.get_test_resource("planilla-ejemplo-6.csv"),
+            nombre_archivo_importacion='planilla-ejemplo-6.csv')
+
+        parser = ParserCsv()
+        estructura_archivo = parser.previsualiza_archivo(bd_contacto)
+        predictor_metadata = PredictorMetadataService()
+        metadata_inferida = predictor_metadata.inferir_metadata_desde_lineas(
+            estructura_archivo)
+
+        metadata = bd_contacto.get_metadata()
+        metadata._metadata = metadata_inferida._metadata
+        metadata.nombres_de_columnas = ["TELEFONO", "NOMBRE", "FECHA", "HORA"]
+        metadata.save()
+
+        creacion_base_datos_service = CreacionBaseDatosService()
+        creacion_base_datos_service.importa_contactos(bd_contacto)
+        creacion_base_datos_service.define_base_dato_contacto(bd_contacto)
+
+        self.assertEquals(BaseDatosContacto.objects.get(pk=bd_contacto.id).estado,
+                          BaseDatosContacto.ESTADO_DEFINIDA,
+                          "La BD no ha quedado en estado ESTADO_DEFINIDA")
