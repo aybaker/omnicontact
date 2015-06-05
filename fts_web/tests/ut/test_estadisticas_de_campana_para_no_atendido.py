2c92189af31bbc089bc2461393ce2d9d2a88d5d9
federicopeker <federicopeker@gmail.com>
2015-06-05 16:02:58 -0300

    Agrega test de 2 intentos situaciones de busy, no answer, failed

--- fts_web/tests/ut/test_estadisticas_de_campana_para_no_atendido.py
+++ fts_web/tests/ut/test_estadisticas_de_campana_para_no_atendido.py
@@ -81,7 +81,7 @@ class EstadisticasParaReporteNoAtendido(FTSenderBaseTest):
                 EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_CONGESTION: 1,
                 EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_CHANUNAVAIL: 0})
 
-    def test_obtener_total_no_atendido_por_evento_dos_intento(self):
+    def test_obtener_total_no_atendido_por_evento_dos_intento_busy(self):
         """
         Este test chequea que se generen correctamente los datos de no 
         atendidos por los 4 diferentes estados:Ocupado, No atendido, Canal no 
@@ -147,6 +147,184 @@ class EstadisticasParaReporteNoAtendido(FTSenderBaseTest):
         # no atendieron
         self._insertar_evento(campana, 2, EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_BUSY, 2)
 
+        lista_eventos = [EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_BUSY,
+                         EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_NOANSWER,
+            EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_CONGESTION,
+            EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_CHANUNAVAIL]
+
+        # obtenemos el listado de los eventos
+        listado = EventoDeContacto.objects_estadisticas.\
+            obtener_eventos_por_contacto(campana)
+
+        estadisticas_para_reporte_no_atendido = EstadisticasCampanaService()
+
+        counter_por_evento = estadisticas_para_reporte_no_atendido.\
+            _obtener_total_no_atendidos_por_evento(listado)
+
+        self.assertEqual(lista_eventos, counter_por_evento.keys())
+        self.assertDictEqual(counter_por_evento,
+            {EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_BUSY: 1,
+                EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_NOANSWER: 0,
+                EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_CONGESTION: 0,
+                EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_CHANUNAVAIL: 0})
+
+    def test_obtener_total_no_atendido_por_evento_dos_intento_no_answer(self):
+        """
+        Este test chequea que se generen correctamente los datos de no 
+        atendidos por los 4 diferentes estados:Ocupado, No atendido, Canal no 
+        disponible y congestion
+        """
+        campana = self.crear_campana_finalizada()
+
+        self._crear_tabla_evento_depurado(campana)
+
+        # crear evento programado
+        self._insertar_evento(campana, 1, EventoDeContacto.EVENTO_CONTACTO_PROGRAMADO)
+        self._insertar_evento(campana, 2, EventoDeContacto.EVENTO_CONTACTO_PROGRAMADO)
+        self._insertar_evento(campana, 3, EventoDeContacto.EVENTO_CONTACTO_PROGRAMADO)
+        self._insertar_evento(campana, 4, EventoDeContacto.EVENTO_CONTACTO_PROGRAMADO)
+
+        # inicia intento 1
+        self._insertar_evento(campana, 1, EventoDeContacto.EVENTO_DAEMON_INICIA_INTENTO, 1)
+        self._insertar_evento(campana, 2, EventoDeContacto.EVENTO_DAEMON_INICIA_INTENTO, 1)
+        self._insertar_evento(campana, 3, EventoDeContacto.EVENTO_DAEMON_INICIA_INTENTO, 1)
+        self._insertar_evento(campana, 4, EventoDeContacto.EVENTO_DAEMON_INICIA_INTENTO, 1)
+
+        # originate exitoso
+        self._insertar_evento(campana, 1, EventoDeContacto.EVENTO_DAEMON_ORIGINATE_SUCCESSFUL, 1)
+        self._insertar_evento(campana, 2, EventoDeContacto.EVENTO_DAEMON_ORIGINATE_SUCCESSFUL, 1)
+        self._insertar_evento(campana, 3, EventoDeContacto.EVENTO_DAEMON_ORIGINATE_SUCCESSFUL, 1)
+        self._insertar_evento(campana, 4, EventoDeContacto.EVENTO_DAEMON_ORIGINATE_SUCCESSFUL, 1)
+
+        # canal local iniciado
+        self._insertar_evento(campana, 1, EventoDeContacto.EVENTO_ASTERISK_DIALPLAN_LOCAL_CHANNEL_INICIADO, 1)
+        self._insertar_evento(campana, 2, EventoDeContacto.EVENTO_ASTERISK_DIALPLAN_LOCAL_CHANNEL_INICIADO, 1)
+        self._insertar_evento(campana, 3, EventoDeContacto.EVENTO_ASTERISK_DIALPLAN_LOCAL_CHANNEL_INICIADO, 1)
+        self._insertar_evento(campana, 4, EventoDeContacto.EVENTO_ASTERISK_DIALPLAN_LOCAL_CHANNEL_INICIADO, 1)
+
+        # atendieron
+        self._insertar_evento(campana, 4, EventoDeContacto.EVENTO_ASTERISK_DIALPLAN_CAMPANA_INICIADO, 1)
+        self._insertar_evento(campana, 4, EventoDeContacto.EVENTO_ASTERISK_AMD_HUMAN_DETECTED, 1)
+        self._insertar_evento(campana, 4, EventoDeContacto.EVENTO_ASTERISK_OPCION_1, 1)
+
+        # no atendieron
+        self._insertar_evento(campana, 1, EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_NOANSWER, 1)
+        self._insertar_evento(campana, 2, EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_NOANSWER, 1)
+        self._insertar_evento(campana, 3, EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_NOANSWER, 1)
+
+        # inicia intento 2
+        self._insertar_evento(campana, 1, EventoDeContacto.EVENTO_DAEMON_INICIA_INTENTO, 2)
+        self._insertar_evento(campana, 2, EventoDeContacto.EVENTO_DAEMON_INICIA_INTENTO, 2)
+        self._insertar_evento(campana, 3, EventoDeContacto.EVENTO_DAEMON_INICIA_INTENTO, 2)
+
+        # originate exitoso
+        self._insertar_evento(campana, 1, EventoDeContacto.EVENTO_DAEMON_ORIGINATE_SUCCESSFUL, 2)
+        self._insertar_evento(campana, 2, EventoDeContacto.EVENTO_DAEMON_ORIGINATE_SUCCESSFUL, 2)
+        self._insertar_evento(campana, 3, EventoDeContacto.EVENTO_DAEMON_ORIGINATE_SUCCESSFUL, 2)
+
+        # canal local iniciado        
+        self._insertar_evento(campana, 1, EventoDeContacto.EVENTO_ASTERISK_DIALPLAN_LOCAL_CHANNEL_INICIADO, 2)
+        self._insertar_evento(campana, 2, EventoDeContacto.EVENTO_ASTERISK_DIALPLAN_LOCAL_CHANNEL_INICIADO, 2)
+        self._insertar_evento(campana, 3, EventoDeContacto.EVENTO_ASTERISK_DIALPLAN_LOCAL_CHANNEL_INICIADO, 2)
+
+        # atendieron
+        self._insertar_evento(campana, 3, EventoDeContacto.EVENTO_ASTERISK_DIALPLAN_CAMPANA_INICIADO, 2)
+        self._insertar_evento(campana, 3, EventoDeContacto.EVENTO_ASTERISK_AMD_HUMAN_DETECTED, 2)
+        self._insertar_evento(campana, 3, EventoDeContacto.EVENTO_ASTERISK_OPCION_2, 2)
+
+        # no atendieron
+        self._insertar_evento(campana, 2, EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_NOANSWER, 2)
+        self._insertar_evento(campana, 2, EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_CONGESTION, 2)
+
+        lista_eventos = [EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_BUSY,
+                         EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_NOANSWER,
+            EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_CONGESTION,
+            EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_CHANUNAVAIL]
+
+        # obtenemos el listado de los eventos
+        listado = EventoDeContacto.objects_estadisticas.\
+            obtener_eventos_por_contacto(campana)
+
+        estadisticas_para_reporte_no_atendido = EstadisticasCampanaService()
+
+        counter_por_evento = estadisticas_para_reporte_no_atendido.\
+            _obtener_total_no_atendidos_por_evento(listado)
+
+        self.assertEqual(lista_eventos, counter_por_evento.keys())
+        self.assertDictEqual(counter_por_evento,
+            {EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_BUSY: 0,
+                EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_NOANSWER: 2,
+                EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_CONGESTION: 0,
+                EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_CHANUNAVAIL: 0})
+
+    def test_obtener_total_no_atendido_por_evento_dos_intento_failed(self):
+        """
+        Este test chequea que se generen correctamente los datos de no 
+        atendidos por los 4 diferentes estados:Ocupado, No atendido, Canal no 
+        disponible y congestion
+        """
+        campana = self.crear_campana_finalizada()
+
+        self._crear_tabla_evento_depurado(campana)
+
+        # crear evento programado
+        self._insertar_evento(campana, 1, EventoDeContacto.EVENTO_CONTACTO_PROGRAMADO)
+        self._insertar_evento(campana, 2, EventoDeContacto.EVENTO_CONTACTO_PROGRAMADO)
+        self._insertar_evento(campana, 3, EventoDeContacto.EVENTO_CONTACTO_PROGRAMADO)
+        self._insertar_evento(campana, 4, EventoDeContacto.EVENTO_CONTACTO_PROGRAMADO)
+
+        # inicia intento 1
+        self._insertar_evento(campana, 1, EventoDeContacto.EVENTO_DAEMON_INICIA_INTENTO, 1)
+        self._insertar_evento(campana, 2, EventoDeContacto.EVENTO_DAEMON_INICIA_INTENTO, 1)
+        self._insertar_evento(campana, 3, EventoDeContacto.EVENTO_DAEMON_INICIA_INTENTO, 1)
+        self._insertar_evento(campana, 4, EventoDeContacto.EVENTO_DAEMON_INICIA_INTENTO, 1)
+
+        # originate exitoso
+        self._insertar_evento(campana, 1, EventoDeContacto.EVENTO_DAEMON_ORIGINATE_SUCCESSFUL, 1)
+        self._insertar_evento(campana, 2, EventoDeContacto.EVENTO_DAEMON_ORIGINATE_SUCCESSFUL, 1)
+        self._insertar_evento(campana, 3, EventoDeContacto.EVENTO_DAEMON_ORIGINATE_SUCCESSFUL, 1)
+        self._insertar_evento(campana, 4, EventoDeContacto.EVENTO_DAEMON_ORIGINATE_SUCCESSFUL, 1)
+
+        # canal local iniciado
+        self._insertar_evento(campana, 1, EventoDeContacto.EVENTO_ASTERISK_DIALPLAN_LOCAL_CHANNEL_INICIADO, 1)
+        self._insertar_evento(campana, 2, EventoDeContacto.EVENTO_ASTERISK_DIALPLAN_LOCAL_CHANNEL_INICIADO, 1)
+        self._insertar_evento(campana, 3, EventoDeContacto.EVENTO_ASTERISK_DIALPLAN_LOCAL_CHANNEL_INICIADO, 1)
+        self._insertar_evento(campana, 4, EventoDeContacto.EVENTO_ASTERISK_DIALPLAN_LOCAL_CHANNEL_INICIADO, 1)
+
+        # atendieron
+        self._insertar_evento(campana, 4, EventoDeContacto.EVENTO_ASTERISK_DIALPLAN_CAMPANA_INICIADO, 1)
+        self._insertar_evento(campana, 4, EventoDeContacto.EVENTO_ASTERISK_AMD_HUMAN_DETECTED, 1)
+        self._insertar_evento(campana, 4, EventoDeContacto.EVENTO_ASTERISK_OPCION_1, 1)
+
+        # no atendieron
+        self._insertar_evento(campana, 1, EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_CHANUNAVAIL, 1)
+        self._insertar_evento(campana, 2, EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_CONGESTION, 1)
+        self._insertar_evento(campana, 3, EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_CHANUNAVAIL, 1)
+
+        # inicia intento 2
+        self._insertar_evento(campana, 1, EventoDeContacto.EVENTO_DAEMON_INICIA_INTENTO, 2)
+        self._insertar_evento(campana, 2, EventoDeContacto.EVENTO_DAEMON_INICIA_INTENTO, 2)
+        self._insertar_evento(campana, 3, EventoDeContacto.EVENTO_DAEMON_INICIA_INTENTO, 2)
+
+        # originate exitoso
+        self._insertar_evento(campana, 1, EventoDeContacto.EVENTO_DAEMON_ORIGINATE_SUCCESSFUL, 2)
+        self._insertar_evento(campana, 2, EventoDeContacto.EVENTO_DAEMON_ORIGINATE_SUCCESSFUL, 2)
+        self._insertar_evento(campana, 3, EventoDeContacto.EVENTO_DAEMON_ORIGINATE_SUCCESSFUL, 2)
+
+        # canal local iniciado        
+        self._insertar_evento(campana, 1, EventoDeContacto.EVENTO_ASTERISK_DIALPLAN_LOCAL_CHANNEL_INICIADO, 2)
+        self._insertar_evento(campana, 2, EventoDeContacto.EVENTO_ASTERISK_DIALPLAN_LOCAL_CHANNEL_INICIADO, 2)
+        self._insertar_evento(campana, 3, EventoDeContacto.EVENTO_ASTERISK_DIALPLAN_LOCAL_CHANNEL_INICIADO, 2)
+
+        # atendieron
+        self._insertar_evento(campana, 3, EventoDeContacto.EVENTO_ASTERISK_DIALPLAN_CAMPANA_INICIADO, 2)
+        self._insertar_evento(campana, 3, EventoDeContacto.EVENTO_ASTERISK_AMD_HUMAN_DETECTED, 2)
+        self._insertar_evento(campana, 3, EventoDeContacto.EVENTO_ASTERISK_OPCION_2, 2)
+
+        # no atendieron
+        self._insertar_evento(campana, 2, EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_CHANUNAVAIL, 2)
+        self._insertar_evento(campana, 2, EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_CONGESTION, 2)
+
 
 
 
@@ -159,19 +337,17 @@ class EstadisticasParaReporteNoAtendido(FTSenderBaseTest):
         listado = EventoDeContacto.objects_estadisticas.\
             obtener_eventos_por_contacto(campana)
 
-        print listado
-
         estadisticas_para_reporte_no_atendido = EstadisticasCampanaService()
 
         counter_por_evento = estadisticas_para_reporte_no_atendido.\
             _obtener_total_no_atendidos_por_evento(listado)
-        print counter_por_evento
+
         self.assertEqual(lista_eventos, counter_por_evento.keys())
         self.assertDictEqual(counter_por_evento,
-            {EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_BUSY: 1,
+            {EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_BUSY: 0,
                 EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_NOANSWER: 0,
-                EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_CONGESTION: 0,
-                EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_CHANUNAVAIL: 0})
+                EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_CONGESTION: 1,
+                EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_CHANUNAVAIL: 1})
 
     def _crear_tabla_evento_depurado(self, campana):
         nombre_tabla = "EDC_depurados_{0}".format(campana.pk)
