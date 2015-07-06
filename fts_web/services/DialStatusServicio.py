ff9b0ef77b87e6d3e533adb6089faa17d961a103
federicopeker <federicopeker@gmail.com>
2015-07-06 17:49:47 -0300

    Refactoriza servicio prioridad evento a DialStatusServicio

---
+++ fts_web/services/DialStatusServicio.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+"""
+Servicio define la prioridad de los eventos de las llamadas
+"""
+
+from __future__ import unicode_literals
+from fts_daemon.models import EventoDeContacto
+
+
+class DialStatusService(object):
+    """
+    Servicio de dialstatus
+    http://www.voip-info.org/wiki/view/Asterisk+variable+DIALSTATUS 
+    """
+
+    def definir_prioridad_dialstatus(self, lista_eventos, lista_tiempo):
+        """
+        Define prioridad de dialstatus de los siguientes eventos en ese orden 
+        Busy, No answer, Failed(chanunavail, congestion)
+        :param lista_eventos: Lista con todos los eventos del contacto
+        :param lista_tiempo: Lista con todos los tiempos de los eventos 
+        """
+
+        dialstatus_evento, dialstatus_timestamp =  None, None
+        for un_dialstatus, un_timestamp in zip(lista_eventos, lista_tiempo):
+            if un_dialstatus is EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_BUSY:
+                dialstatus_evento = un_dialstatus
+                dialstatus_timestamp = un_timestamp
+                break
+            elif un_dialstatus is EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_NOANSWER:
+                dialstatus_evento = un_dialstatus
+                dialstatus_timestamp = un_timestamp
+            elif un_dialstatus is EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_CHANUNAVAIL or\
+            un_dialstatus is EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_CONGESTION:
+                if not dialstatus_evento is EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_NOANSWER:
+                    dialstatus_evento = un_dialstatus
+                    dialstatus_timestamp = un_timestamp
+
+        if dialstatus_evento is None:
+            return None
+        else:
+            return DatosDialStatus(dialstatus_evento, dialstatus_timestamp,
+                                   self._mapear_dialstatu_nombre(dialstatus_evento))
+
+    def _mapear_dialstatu_nombre(self, dialstatus_evento):
+        if dialstatus_evento is EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_BUSY:
+            return "Ocupado"
+        elif dialstatus_evento is EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_NOANSWER:
+            return "No contesto"
+        elif dialstatus_evento is EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_CHANUNAVAIL:
+            return "Canal no disponible"
+        elif dialstatus_evento is EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_CONGESTION:
+            return "Congestion"
+        else:
+            return None
+
+class DatosDialStatus(object):
+    """Encapsula los datos del dialstatus.
+    """
+
+    def __init__(self, evento, timestamp, nombre_dialstatus):
+
+        self._evento = evento
+        self._timestamp = timestamp
+        self._nombre_dialstatus = nombre_dialstatus
+
+    @property
+    def evento(self):
+        return self._evento
+
+    @property
+    def timestamp(self):
+        return self._timestamp
+
+    @property
+    def dialstatus(self):
+        return self._nombre_dialstatus
--- fts_web/services/prioridad_evento.py
+++ fts_web/services/prioridad_evento.py
@@ -14,26 +14,27 @@ class PrioridadEventoNoAtendidosService(object):
     chanunavail, congestion )
     """
 
-    def definir_prioridad_evento(self, lista_eventos):
+    def definir_prioridad_evento(self, lista_eventos, lista_tiempo):
         """
         Prioridad de eventos no atendidos en este orden Busy, No answer,
         Failed(chanunavail, congestion)
-        :param lista_eventos: Lista con todos los eventos del contacto 
+        :param lista_eventos: Lista con todos los eventos del contacto
+        :param lista_tiempo: Lista con todos los tiempos de los eventos 
         """
-        evento_prioridad = None
-        indice_evento = None
-        for ev in lista_eventos:
-            if ev is EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_BUSY:
-                evento_prioridad = ev
-                indice_evento = lista_eventos.index(ev)
+        dialstatus_evento = None
+        dialstatus_timestamp = None
+        for un_dialstatus, un_timestamp in zip(lista_eventos, lista_tiempo):
+            if un_dialstatus is EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_BUSY:
+                dialstatus_evento = un_dialstatus
+                dialstatus_timestamp = un_timestamp
                 break
-            elif ev is EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_NOANSWER:
-                evento_prioridad = ev
-                indice_evento = lista_eventos.index(ev)
-            elif ev is EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_CHANUNAVAIL or\
-            ev is EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_CONGESTION:
-                if not evento_prioridad is EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_NOANSWER:
-                    evento_prioridad = ev
-                    indice_evento = lista_eventos.index(ev)
+            elif un_dialstatus is EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_NOANSWER:
+                dialstatus_evento = un_dialstatus
+                dialstatus_timestamp = un_timestamp
+            elif un_dialstatus is EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_CHANUNAVAIL or\
+            un_dialstatus is EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_CONGESTION:
+                if not dialstatus_evento is EventoDeContacto.EVENTO_ASTERISK_DIALSTATUS_NOANSWER:
+                    dialstatus_evento = un_dialstatus
+                    dialstatus_timestamp = un_timestamp
 
-        return evento_prioridad, indice_evento
+        return dialstatus_evento, dialstatus_timestamp
