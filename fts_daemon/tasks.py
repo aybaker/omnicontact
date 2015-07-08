d8108713bfa3aa62f3d52641809e446c9cd2ac7a
Horacio G. de Oro <hgdeoro@gmail.com>
2015-07-08 12:28:51 -0300

    Implementa management command y hook para poder testear workers de Celery

--- fts_daemon/tasks.py
+++ fts_daemon/tasks.py
@@ -9,6 +9,7 @@ quedar acoplado a la herramienta usada actualmente (ej: Celery).
 from __future__ import unicode_literals
 
 import logging
+import time
 
 from fts_daemon import fts_celery_daemon
 from fts_daemon.services.depurador_de_campana import (
@@ -22,6 +23,29 @@ from fts_daemon.services.esperador_para_depuracion_segura import (
 logger = logging.getLogger(__name__)
 
 
+def _internal_command(campana_id):
+    """
+    :param campana_id:
+    :return: boolean, indica si se ha detectado y ejecutado un comando valido
+    """
+    logging.info("_internal_command(): '%s'", campana_id)
+
+    if campana_id.startswith('SLEEP_'):
+        sleep_time = float(campana_id.split('_')[1])
+        logger.info("Iniciando espera de %s segundos", sleep_time)
+        time_fin = time.time() + sleep_time
+        while time.time() < time_fin:
+            logger.info("Esperando...")
+            time.sleep(1)
+        logger.info("Espera de %s segundos finalizada", sleep_time)
+        return True
+    else:
+        logging.info("_internal_command() desconocido: '%s'", campana_id)
+        return False
+
+
+
+
 # -----------------------------------------------------------------------------
 # DepuradorDeCampanaWorkflow
 # -----------------------------------------------------------------------------
@@ -29,6 +53,11 @@ logger = logging.getLogger(__name__)
 @fts_celery_daemon.app.task(ignore_result=True)
 def depurar_campana(campana_id):
     """Depura la campaña"""
+
+    if isinstance(campana_id, (str, unicode)):
+        if _internal_command(campana_id):
+            return
+
     DepuradorDeCampanaWorkflow().depurar(campana_id)
 
 
@@ -48,6 +77,11 @@ def depurar_campana_async(campana_id):
 @fts_celery_daemon.app.task(ignore_result=True)
 def esperar_y_depurar_campana(campana_id):
     """Espera a que no haya llamadas en curso, y depura la campaña"""
+
+    if isinstance(campana_id, (str, unicode)):
+        if _internal_command(campana_id):
+            return
+
     EsperadorParaDepuracionSegura().esperar_y_depurar(campana_id)
 
 
