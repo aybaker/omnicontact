29ddbe9c198910aa90aa129a1581466622ee9738
Horacio G. de Oro <hgdeoro@gmail.com>
2015-07-11 14:07:58 -0300

    Implementa mecanismo de locks usando domain unix sockets

---
+++ fts_daemon/locks.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Implementacion de locks para sincronizar procesos de Celery.
+"""
+
+from __future__ import unicode_literals
+
+import logging
+import re
+import socket
+
+
+from fts_web import errors
+
+
+logger = logging.getLogger(__name__)
+
+REGEX_VALID_LOCK_NAME = re.compile(r'^[a-zA-Z0-9/\._-]+$')
+"""Valid lock names can contain leters, numbers and
+the characters / . - _
+"""
+
+LOCKS = {}
+"""Dict with the created locks.
+Key: lock name, Value: the socket.
+"""
+
+class LockingError(errors.FtsError):
+    pass
+
+
+def lock(lock_name):
+    """
+    Create a lock (using Unix Domain Socket).
+
+    :param lock_name: name of the lock
+    :raises: LockingError
+    """
+
+    assert isinstance(lock_name, (str, unicode))
+
+    if not REGEX_VALID_LOCK_NAME.match(lock_name):
+        raise LockingError(message="Nombre de lock invalido: '{0}'".format(lock_name))
+
+    lock_socket = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+    try:
+        lock_socket.bind('\x00' + lock_name)
+        LOCKS[lock_name] = lock_socket
+    except socket.error:
+        raise LockingError(message="No se pudo crear el lock '{0}'".format(lock_name))
