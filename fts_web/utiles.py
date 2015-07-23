75e7cc18bc061cb6e7127ab85139d39c9e8fa353
federicopeker <federicopeker@gmail.com>
2015-07-23 14:30:47 -0300

    Crea metodo elimina_espacios_parentesis_guiones

--- fts_web/utiles.py
+++ fts_web/utiles.py
@@ -51,6 +51,10 @@ def elimina_tildes(s):
     return ''.join(unicodedata.normalize('NFD', s).encode('ASCII', 'ignore'))
 
 
+def elimina_espacios_parentesis_guiones(cadena):
+    return re.sub("\(?\)?\s?\-?", "", str(cadena))
+
+
 def crear_archivo_en_media_root(dirname_template, prefix, suffix=""):
     """Crea un archivo en el directorio MEDIA_ROOT. Si los directorios
     no existen, los crea tambien.
