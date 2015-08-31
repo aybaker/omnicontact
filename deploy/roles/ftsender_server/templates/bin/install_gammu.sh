bf21ee209d4bb9cc6ee2401364b5818a07f790bc
federicopeker <federicopeker@gmail.com>
2015-08-31 16:49:32 -0300

    modifica faltó informacion en los script de test e installacion gammu

--- deploy/roles/ftsender_server/templates/bin/install_gammu.sh
+++ deploy/roles/ftsender_server/templates/bin/install_gammu.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+cd /opt/gammu/{{ DOWNLOAD_GAMMU_EXTRACT_DIRNAME }}/build
 make install >> gammu.log
--- deploy/roles/ftsender_server/templates/bin/test_gammu.sh
+++ deploy/roles/ftsender_server/templates/bin/test_gammu.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+cd /opt/gammu/{{ DOWNLOAD_GAMMU_EXTRACT_DIRNAME }}/build
 make -i test >> gammu.log
