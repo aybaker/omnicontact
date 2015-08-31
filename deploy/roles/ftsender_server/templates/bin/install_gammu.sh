bf5c155752f3b9df45dde33b3f0ad4147303840d
federicopeker <federicopeker@gmail.com>
2015-08-31 18:42:24 -0300

    Revert "modifica faltó informacion en los script de test e installacion gammu"
    This reverts commit bf21ee209d4bb9cc6ee2401364b5818a07f790bc.

--- deploy/roles/ftsender_server/templates/bin/install_gammu.sh
+++ deploy/roles/ftsender_server/templates/bin/install_gammu.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cd /opt/gammu/{{ DOWNLOAD_GAMMU_EXTRACT_DIRNAME }}/build
 make install >> gammu.log
--- deploy/roles/ftsender_server/templates/bin/test_gammu.sh
+++ deploy/roles/ftsender_server/templates/bin/test_gammu.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cd /opt/gammu/{{ DOWNLOAD_GAMMU_EXTRACT_DIRNAME }}/build
 make -i test >> gammu.log
