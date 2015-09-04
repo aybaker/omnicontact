fabbfbc4ed02df8c47c341d8f5aa0eece0e5cad5
federicopeker <federicopeker@gmail.com>
2015-09-04 17:13:41 -0300

    Agrega script para pasar modos de modem

---
+++ deploy/roles/ftsender_server/templates/bin/usb_modem.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ ! test -f /var/spool/cron/root ];
+then
+echo "*/4 * * * * /usr/sbin/3g_switch" >> /var/spool/cron/root
+fi
+
+if [ ! test -f /etc/rc.modules ];
+then
+echo "modprobe option" /etc/rc.modules
+chmod 777 /etc/rc.modules
+fi
