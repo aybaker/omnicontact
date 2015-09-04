26a1db7fbf67527a979232f8e8aa0ab131474954
federicopeker <federicopeker@gmail.com>
2015-09-04 17:39:43 -0300

    agrega comillas invertidad en script usb_modem

--- deploy/roles/ftsender_server/templates/bin/usb_modem.sh
+++ deploy/roles/ftsender_server/templates/bin/usb_modem.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ ! test -f /var/spool/cron/root ];
+if [ ! `test -f /var/spool/cron/root` ];
 then
-touch /var/spool/cron/root
+#touch /var/spool/cron/root
 echo "*/4 * * * * /usr/sbin/3g_switch" >> /var/spool/cron/root
-chmod 777 /var/spool/cron/root
+#chmod 777 /var/spool/cron/root
 fi
 
-if [ ! test -f /etc/rc.modules ];
+if [ ! `test -f /etc/rc.modules` ];
 then
 echo "modprobe option" /etc/rc.modules
 chmod 777 /etc/rc.modules
