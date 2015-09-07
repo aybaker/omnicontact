1269858d374a2db2761fec4844cd9dd4289d4258
federicopeker <federicopeker@gmail.com>
2015-09-07 15:12:53 -0300

    elimina test del script usb_modem.sh

--- deploy/roles/ftsender_server/templates/bin/usb_modem.sh
+++ deploy/roles/ftsender_server/templates/bin/usb_modem.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-`test -f /var/spool/cron/root`
-
-if [ echo $? -eq 1  ];
+if [ ! -f /var/spool/cron/root ];
 then
 echo "*/4 * * * * /usr/sbin/3g_switch" >> /var/spool/cron/root
 fi
 
-if [ ! `test -f /etc/rc.modules` ];
+if [ ! -f /etc/rc.modules ];
 then
 echo "modprobe option" /etc/rc.modules
 chmod 777 /etc/rc.modules
