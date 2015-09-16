a884dc66851a73012954420d5782926eade4672f
federicopeker <federicopeker@gmail.com>
2015-09-16 14:41:29 -0300

    Agrega creacion de carpeta temporal reportesms

--- build.sh
+++ build.sh
@@ -52,6 +52,12 @@ echo "Usando directorio temporal: $TMP/appsms..."
 echo "Descargando demonio sms en directorio temporal"
 tar -xzf /home/deployer/aplicacionsms/DEMONIO-SMS.tar.gz -C $TMP/appsms
 
+mkdir -p $TMP/reportesms
+echo "Usando directorio temporal: $TMP/reportesms..."
+
+echo "Descargando modulo reporte sms en directorio temporal"
+tar -xzf /home/deployer/aplicacionsms/reporte_sms.tar.gz -C $TMP/reportesms
+
 # ----------
 
 echo "Obteniendo datos de version..."
@@ -88,5 +94,5 @@ python $TMP/app/fts_web/version.py
 export DO_CHECKS="${DO_CHECKS:-no}"
 
 echo "Ejecutando Ansible"
-ansible-playbook deploy/playbook.yml --extra-vars "BUILD_DIR=$TMP/app  BUILD_DIR_SMS=$TMP/appsms" $*
+ansible-playbook deploy/playbook.yml --extra-vars "BUILD_DIR=$TMP/app  BUILD_DIR_SMS=$TMP/appsms BUILD_DIR_REPORTE_SMS=$TMP/reportesms" $*
 
