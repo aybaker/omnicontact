69a5174e6313765f50d0fcfb4720b4d0a1cab25d
federicopeker <federicopeker@gmail.com>
2015-10-06 16:06:04 -0300

    Elimina migracion de base de datos de smsd a ftsender

--- fts_web/sql/plpgsql/update_timestamp.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE FUNCTION update_timestamp() RETURNS trigger AS $update_timestamp$
-  BEGIN
-    NEW."UpdatedInDB" := LOCALTIMESTAMP(0);
-    RETURN NEW;
-  END;
-$update_timestamp$ LANGUAGE plpgsql;
