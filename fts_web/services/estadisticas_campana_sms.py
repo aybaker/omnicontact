1466aa3253dcb91b04f282602b7afe073a932e00
federicopeker <federicopeker@gmail.com>
2015-11-02 18:38:42 -0300

    agrega filtro para mostrar bien los numeros de mensajes recibidos

--- fts_web/services/estadisticas_campana_sms.py
+++ fts_web/services/estadisticas_campana_sms.py
@@ -148,8 +148,8 @@ class EstadisticasCampanaSmsService():
             'total_enviado': total_estado_enviado['total_enviado'],
             'total_error_envio': total_estado_enviado['total_error_envio'],
             'total_no_procesados': total_estado_enviado['total_no_procesados'],
-            'total_recibidos_respuesta': total_recibidos_respuesta,
-            'total_recibidos_respuesta_invalida': total_recibidos_respuesta_invalida,
+            'total_recibidos_respuesta': total_recibidos_respuesta[0][0],
+            'total_recibidos_respuesta_invalida': total_recibidos_respuesta_invalida[0][0],
         }
 
         return total_supervision
