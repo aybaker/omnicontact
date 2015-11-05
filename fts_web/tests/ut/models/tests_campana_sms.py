4733645d0b35ee5e48698a0f2b39eca900b82a34
federicopeker <federicopeker@gmail.com>
2015-11-05 14:54:59 -0300

    crea test para replicar_campana_sms

--- fts_web/tests/ut/models/tests_campana_sms.py
+++ fts_web/tests/ut/models/tests_campana_sms.py
@@ -7,7 +7,7 @@ from __future__ import unicode_literals
 import datetime
 
 
-from fts_web.models import (CampanaSms, Actuacion)
+from fts_web.models import (CampanaSms, ActuacionSms, OpcionSms)
 
 from fts_web.tests.utiles import FTSenderBaseTest
 from mock import Mock
@@ -17,16 +17,16 @@ class MetodosValidadoresDeCampanaSmsTest(FTSenderBaseTest):
 
     def test_campana_valida_mensaje_falla_sin_template_mensaje(self):
         campana_sms = CampanaSms.objects.create(cantidad_chips=3,
-                                            tiene_respuesta=False,
-                                            identificador_campana_sms=1)
+                                                tiene_respuesta=False,
+                                                identificador_campana_sms=1)
 
         self.assertEqual(campana_sms.valida_mensaje(), False)
 
     def test_campana_valida_mensaje(self):
         campana_sms = CampanaSms.objects.create(cantidad_chips=3,
                                                 template_mensaje="test mensaje",
-                                            tiene_respuesta=False,
-                                            identificador_campana_sms=1)
+                                                tiene_respuesta=False,
+                                                identificador_campana_sms=1)
 
         self.assertEqual(campana_sms.valida_mensaje(), True)
 
@@ -78,3 +78,47 @@ class EliminarCampanaSmsTest(FTSenderBaseTest):
 
         with self.assertRaises(CampanaSms.DoesNotExist):
             CampanaSms.objects.get(id=1)
+
+
+class CampanaReplicarCampana(FTSenderBaseTest):
+
+    def test_replicar_campana_falla_parametro_incorrecto(self):
+        campana_sms = OpcionSms(pk=1)
+
+        # -----
+
+        with self.assertRaises(AssertionError):
+
+            CampanaSms.objects.replicar_campana_sms(campana_sms)
+
+    def test_replicar_campana_sms_no_falla(self):
+        hora_desde = datetime.time(00, 00)
+        hora_hasta = datetime.time(23, 59)
+
+        campana_sms = self.crear_campana_sms()
+        repuesta = OpcionSms(respuesta="SI", campana_sms=campana_sms)
+        repuesta.save()
+
+        [self.crea_campana_sms_actuacion_sms(dia_semanal, hora_desde,
+            hora_hasta, campana_sms) for dia_semanal in range(0, 4)]
+
+        # -----
+
+        campana_replicada = CampanaSms.objects.replicar_campana_sms(campana_sms)
+
+        self.assertEqual(campana_replicada.estado,
+                         CampanaSms.ESTADO_EN_DEFINICION)
+        self.assertEqual(OpcionSms.objects.filter(
+                         campana_sms=campana_replicada).count(), 1)
+        self.assertEqual(ActuacionSms.objects.filter(
+                         campana_sms=campana_replicada).count(), 4)
+        self.assertEqual(campana_sms.cantidad_chips,
+                         campana_replicada.cantidad_chips)
+        self.assertEqual(campana_sms.tiene_respuesta,
+                         campana_replicada.tiene_respuesta)
+        self.assertEqual(campana_sms.template_mensaje,
+                         campana_replicada.template_mensaje)
+        self.assertEqual(campana_sms.template_mensaje_alternativo,
+                         campana_replicada.template_mensaje_alternativo)
+        self.assertEqual(campana_sms.template_mensaje_opcional,
+                         campana_replicada.template_mensaje_opcional)
