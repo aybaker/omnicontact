8e527be69fe1a7ce7794050f6e1ab9de0be38200
federicopeker <federicopeker@gmail.com>
2015-11-09 17:34:56 -0300

    agrega comilla dobles en la creacion de gammu

--- fts_web/migrations/0050_crea_tablas_gammu.py
+++ fts_web/migrations/0050_crea_tablas_gammu.py
@@ -20,8 +20,8 @@ class Migration(SchemaMigration):
         # Crea tabla daemons
         sql = """
         CREATE TABLE daemons (
-            Start text NOT NULL,
-            Info text NOT NULL
+            "Start" text NOT NULL,
+            "Info" text NOT NULL
         )
         """
         cursor.execute(sql)
@@ -29,33 +29,33 @@ class Migration(SchemaMigration):
         # Crea tabla gammu
         sql = """
         CREATE TABLE gammu (
-            Version smallint NOT NULL DEFAULT '0'
+            "Version" smallint NOT NULL DEFAULT '0'
         )
         """
         cursor.execute(sql)
 
         # Insert tabla gammu
         sql = """
-        INSERT INTO gammu (Version) VALUES (14)
+        INSERT INTO gammu ("Version") VALUES (14)
         """
         cursor.execute(sql)
 
         # Crea tabla inbox
         sql = """
         CREATE TABLE inbox (
-            UpdatedInDB timestamp(0) WITHOUT time zone NOT NULL DEFAULT LOCALTIMESTAMP(0),
-            ReceivingDateTime timestamp(0) WITHOUT time zone NOT NULL DEFAULT LOCALTIMESTAMP(0),
-            Text text NOT NULL,
-            SenderNumber varchar(20) NOT NULL DEFAULT '',
-            Coding varchar(255) NOT NULL DEFAULT 'Default_No_Compression',
-            UDH text NOT NULL,
-            SMSCNumber varchar(20) NOT NULL DEFAULT '',
-            Class integer NOT NULL DEFAULT '-1',
-            TextDecoded text NOT NULL DEFAULT '',
-            ID serial PRIMARY KEY,
-            RecipientID text NOT NULL,
-            Processed boolean NOT NULL DEFAULT 'false',
-            CHECK (Coding IN 
+            "UpdatedInDB" timestamp(0) WITHOUT time zone NOT NULL DEFAULT LOCALTIMESTAMP(0),
+            "ReceivingDateTime" timestamp(0) WITHOUT time zone NOT NULL DEFAULT LOCALTIMESTAMP(0),
+            "Text" text NOT NULL,
+            "SenderNumber" varchar(20) NOT NULL DEFAULT '',
+            "Coding" varchar(255) NOT NULL DEFAULT 'Default_No_Compression',
+            "UDH" text NOT NULL,
+            "SMSCNumber" varchar(20) NOT NULL DEFAULT '',
+            "Class" integer NOT NULL DEFAULT '-1',
+            "TextDecoded" text NOT NULL DEFAULT '',
+            "ID" serial PRIMARY KEY,
+            "RecipientID" text NOT NULL,
+            "Processed" boolean NOT NULL DEFAULT 'false',
+            CHECK ("Coding" IN
             ('Default_No_Compression','Unicode_No_Compression','8bit','Default_Compression','Unicode_Compression')) 
         )
         """
@@ -70,39 +70,39 @@ class Migration(SchemaMigration):
         # Crea tabla outbox
         sql = """
         CREATE TABLE outbox (
-            UpdatedInDB timestamp(0) WITHOUT time zone NOT NULL DEFAULT LOCALTIMESTAMP(0),
-            InsertIntoDB timestamp(0) WITHOUT time zone NOT NULL DEFAULT LOCALTIMESTAMP(0),
-            SendingDateTime timestamp NOT NULL DEFAULT LOCALTIMESTAMP(0),
-            SendBefore time NOT NULL DEFAULT '23:59:59',
-            SendAfter time NOT NULL DEFAULT '00:00:00',
-            Text text,
-            DestinationNumber varchar(20) NOT NULL DEFAULT '',
-            Coding varchar(255) NOT NULL DEFAULT 'Default_No_Compression',
-            UDH text,
-            Class integer DEFAULT '-1',
-            TextDecoded text NOT NULL DEFAULT '',
-            ID serial PRIMARY KEY,
-            MultiPart boolean NOT NULL DEFAULT 'false',
-            RelativeValidity integer DEFAULT '-1',
-            SenderID varchar(255),
-            SendingTimeOut timestamp(0) WITHOUT time zone NOT NULL DEFAULT LOCALTIMESTAMP(0),
-            DeliveryReport varchar(10) DEFAULT 'default',
-            CreatorID text NOT NULL,
-            CHECK (Coding IN 
+            "UpdatedInDB" timestamp(0) WITHOUT time zone NOT NULL DEFAULT LOCALTIMESTAMP(0),
+            "InsertIntoDB" timestamp(0) WITHOUT time zone NOT NULL DEFAULT LOCALTIMESTAMP(0),
+            "SendingDateTime" timestamp NOT NULL DEFAULT LOCALTIMESTAMP(0),
+            "SendBefore" time NOT NULL DEFAULT '23:59:59',
+            "SendAfter" time NOT NULL DEFAULT '00:00:00',
+            "Text" text,
+            "DestinationNumber" varchar(20) NOT NULL DEFAULT '',
+            "Coding" varchar(255) NOT NULL DEFAULT 'Default_No_Compression',
+            "UDH" text,
+            "Class" integer DEFAULT '-1',
+            "TextDecoded" text NOT NULL DEFAULT '',
+            "ID" serial PRIMARY KEY,
+            "MultiPart" boolean NOT NULL DEFAULT 'false',
+            "RelativeValidity" integer DEFAULT '-1',
+            "SenderID" varchar(255),
+            "SendingTimeOut" timestamp(0) WITHOUT time zone NOT NULL DEFAULT LOCALTIMESTAMP(0),
+            "DeliveryReport" varchar(10) DEFAULT 'default',
+            "CreatorID" text NOT NULL,
+            CHECK ("Coding" IN
             ('Default_No_Compression','Unicode_No_Compression','8bit','Default_Compression','Unicode_Compression')),
-            CHECK (DeliveryReport IN ('default','yes','no'))
+            CHECK ("DeliveryReport" IN ('default','yes','no'))
         )
         """
         cursor.execute(sql)
 
         # Crea indices para la tabla outbox
         sql = """
-        CREATE INDEX outbox_date ON outbox(SendingDateTime, SendingTimeOut)
+        CREATE INDEX outbox_date ON outbox("SendingDateTime", "SendingTimeOut")
         """
         cursor.execute(sql)
 
         sql = """
-        CREATE INDEX outbox_sender ON outbox(SenderID)
+        CREATE INDEX outbox_sender ON outbox("SenderID")
         """
         cursor.execute(sql)
 
@@ -115,27 +115,27 @@ class Migration(SchemaMigration):
         # Crea tabla outbox_multipart
         sql = """
         CREATE TABLE outbox_multipart (
-            Text text,
-            Coding varchar(255) NOT NULL DEFAULT 'Default_No_Compression',
-            UDH text,
-            Class integer DEFAULT '-1',
-            TextDecoded text DEFAULT NULL,
-            ID serial,
-            SequencePosition integer NOT NULL DEFAULT '1',
-            PRIMARY KEY (ID, SequencePosition),
-            CHECK (Coding IN 
+            "Text" text,
+            "Coding" varchar(255) NOT NULL DEFAULT 'Default_No_Compression',
+            "UDH" text,
+            "Class" integer DEFAULT '-1',
+            "TextDecoded" text DEFAULT NULL,
+            "ID" serial,
+            "SequencePosition" integer NOT NULL DEFAULT '1',
+            PRIMARY KEY ("ID", "SequencePosition"),
+            CHECK ("Coding" IN
             ('Default_No_Compression','Unicode_No_Compression','8bit','Default_Compression','Unicode_Compression'))
         )
         """
-        cursor.execute(sql)
+        cursor.execute(sql)anti
 
         # Crea tabla pbk
         sql = """
         CREATE TABLE pbk (
-            ID serial PRIMARY KEY,
-            GroupID integer NOT NULL DEFAULT '-1',
-            Name text NOT NULL,
-            Number text NOT NULL
+            "ID" serial PRIMARY KEY,
+            "GroupID" integer NOT NULL DEFAULT '-1',
+            "Name" text NOT NULL,
+            "Number" text NOT NULL
         )
         """
         cursor.execute(sql)
@@ -143,8 +143,8 @@ class Migration(SchemaMigration):
         # Crea tabla pbk_groups
         sql = """
         CREATE TABLE pbk_groups (
-            Name text NOT NULL,
-            ID serial PRIMARY KEY
+            "Name" text NOT NULL,
+            "ID" serial PRIMARY KEY
         )
         """
         cursor.execute(sql)
@@ -152,20 +152,20 @@ class Migration(SchemaMigration):
         # Crea tabla phones
         sql = """
         CREATE TABLE phones (
-            ID text NOT NULL,
-            UpdatedInDB timestamp(0) WITHOUT time zone NOT NULL DEFAULT LOCALTIMESTAMP(0),
-            InsertIntoDB timestamp(0) WITHOUT time zone NOT NULL DEFAULT LOCALTIMESTAMP(0),
-            TimeOut timestamp(0) WITHOUT time zone NOT NULL DEFAULT LOCALTIMESTAMP(0),
-            Send boolean NOT NULL DEFAULT 'no',
-            Receive boolean NOT NULL DEFAULT 'no',
-            IMEI varchar(35) PRIMARY KEY NOT NULL,
-            NetCode varchar(10) DEFAULT 'ERROR',
-            NetName varchar(35) DEFAULT 'ERROR',
-            Client text NOT NULL,
-            Battery integer NOT NULL DEFAULT -1,
-            Signal integer NOT NULL DEFAULT -1,
-            Sent integer NOT NULL DEFAULT 0,
-            Received integer NOT NULL DEFAULT 0
+            "ID" text NOT NULL,
+            "UpdatedInDB" timestamp(0) WITHOUT time zone NOT NULL DEFAULT LOCALTIMESTAMP(0),
+            "InsertIntoDB" timestamp(0) WITHOUT time zone NOT NULL DEFAULT LOCALTIMESTAMP(0),
+            "TimeOut" timestamp(0) WITHOUT time zone NOT NULL DEFAULT LOCALTIMESTAMP(0),
+            "Send" boolean NOT NULL DEFAULT 'no',
+            "Receive" boolean NOT NULL DEFAULT 'no',
+            "IMEI" varchar(35) PRIMARY KEY NOT NULL,
+            "NetCode" varchar(10) DEFAULT 'ERROR',
+            "NetName" varchar(35) DEFAULT 'ERROR',
+            "Client" text NOT NULL,
+            "Battery" integer NOT NULL DEFAULT -1,
+            "Signal" integer NOT NULL DEFAULT -1,
+            "Sent" integer NOT NULL DEFAULT 0,
+            "Received" integer NOT NULL DEFAULT 0
         )
         """
         cursor.execute(sql)
@@ -179,53 +179,53 @@ class Migration(SchemaMigration):
         # Crea tabla sentitems
         sql = """
         CREATE TABLE sentitems (
-            UpdatedInDB timestamp(0) WITHOUT time zone NOT NULL DEFAULT LOCALTIMESTAMP(0),
-            InsertIntoDB timestamp(0) WITHOUT time zone NOT NULL DEFAULT LOCALTIMESTAMP(0),
-            SendingDateTime timestamp(0) WITHOUT time zone NOT NULL DEFAULT LOCALTIMESTAMP(0),
-            DeliveryDateTime timestamp(0) WITHOUT time zone NULL,
-            Text text NOT NULL,
-            DestinationNumber varchar(20) NOT NULL DEFAULT '',
-            Coding varchar(255) NOT NULL DEFAULT 'Default_No_Compression',
-            UDH text NOT NULL,
-            SMSCNumber varchar(20) NOT NULL DEFAULT '',
-            Class integer NOT NULL DEFAULT '-1',
-            TextDecoded text NOT NULL DEFAULT '',
-            ID serial,
-            SenderID varchar(255) NOT NULL,
-            SequencePosition integer NOT NULL DEFAULT '1',
-            Status varchar(255) NOT NULL DEFAULT 'SendingOK',
-            StatusError integer NOT NULL DEFAULT '-1',
-            TPMR integer NOT NULL DEFAULT '-1',
-            RelativeValidity integer NOT NULL DEFAULT '-1',
-            CreatorID text NOT NULL,
-            CHECK (Status IN 
+            "UpdatedInDB" timestamp(0) WITHOUT time zone NOT NULL DEFAULT LOCALTIMESTAMP(0),
+            "InsertIntoDB" timestamp(0) WITHOUT time zone NOT NULL DEFAULT LOCALTIMESTAMP(0),
+            "SendingDateTime" timestamp(0) WITHOUT time zone NOT NULL DEFAULT LOCALTIMESTAMP(0),
+            "DeliveryDateTime" timestamp(0) WITHOUT time zone NULL,
+            "Text" text NOT NULL,
+            "DestinationNumber" varchar(20) NOT NULL DEFAULT '',
+            "Coding" varchar(255) NOT NULL DEFAULT 'Default_No_Compression',
+            "UDH" text NOT NULL,
+            "SMSCNumber" varchar(20) NOT NULL DEFAULT '',
+            "Class" integer NOT NULL DEFAULT '-1',
+            "TextDecoded" text NOT NULL DEFAULT '',
+            "ID" serial,
+            "SenderID" varchar(255) NOT NULL,
+            "SequencePosition" integer NOT NULL DEFAULT '1',
+            "Status" varchar(255) NOT NULL DEFAULT 'SendingOK',
+            "StatusError" integer NOT NULL DEFAULT '-1',
+            "TPMR" integer NOT NULL DEFAULT '-1',
+            "RelativeValidity" integer NOT NULL DEFAULT '-1',
+            "CreatorID" text NOT NULL,
+            CHECK ("Status" IN
             ('SendingOK','SendingOKNoReport','SendingError','DeliveryOK','DeliveryFailed','DeliveryPending',
             'DeliveryUnknown','Error')),
-            CHECK (Coding IN 
+            CHECK ("Coding" IN
             ('Default_No_Compression','Unicode_No_Compression','8bit','Default_Compression','Unicode_Compression')),
-            PRIMARY KEY (ID, SequencePosition)
+            PRIMARY KEY ("ID", "SequencePosition")
         )
         """
         cursor.execute(sql)
 
         # Crea indices para la tabla sentitems
         sql = """
-        CREATE INDEX sentitems_date ON sentitems(DeliveryDateTime)
+        CREATE INDEX sentitems_date ON sentitems("DeliveryDateTime")
         """
         cursor.execute(sql)
 
         sql = """
-        CREATE INDEX sentitems_tpmr ON sentitems(TPMR)
+        CREATE INDEX sentitems_tpmr ON sentitems("TPMR")
         """
         cursor.execute(sql)
 
         sql = """
-        CREATE INDEX sentitems_dest ON sentitems(DestinationNumber)
+        CREATE INDEX sentitems_dest ON sentitems("DestinationNumber")
         """
         cursor.execute(sql)
 
         sql = """
-        CREATE INDEX sentitems_sender ON sentitems(SenderID)
+        CREATE INDEX sentitems_sender ON sentitems("SenderID")
         """
         cursor.execute(sql)
 
