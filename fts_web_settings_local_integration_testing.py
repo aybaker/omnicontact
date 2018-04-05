78be19e2bde32795d908786517a1e33dec1507c5
Horacio G. de Oro <hgdeoro@gmail.com>
2018-04-05 14:28:05 -0300

    Cambia uso de django.utils.log.NullHandler a logging.NullHandler

--- fts_web_settings_local_integration_testing.py
+++ fts_web_settings_local_integration_testing.py
@@ -95,7 +95,7 @@ LOGGING = {
     'handlers': {
         'null': {
             'level': 'DEBUG',
-            'class': 'django.utils.log.NullHandler',
+            'class': 'logging.NullHandler',
         },
         'console': {
             'level': 'DEBUG',
