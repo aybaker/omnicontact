89409a30bd9a20fdd74a26addc0b16b7a9963629
Horacio G. de Oro <hgdeoro@gmail.com>
2018-05-30 17:05:53 -0300

    Expone ICS en /ics/*

--- ominicontacto/urls.py
+++ ominicontacto/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('ominicontacto_app.urls')),
     url(r'^', include('reciclado_app.urls')),
-    url(r'^', include('fts_web.urls')),
+    url(r'^ics/', include('fts_web.urls')),
     url(r'^accounts/logout/$', auth_views.logout,
         {'next_page': '/accounts/login/'}, name="logout"),
 
