4e774204c711703c78e83bfab1020cefee87e64e
lionite <felipe.macias@freetechsolutions.com.ar>
2018-09-03 11:34:38 -0300

    primer cherry-pick

--- deploy/ansible/roles/deploy_supervision/templates/config.php
+++ deploy/ansible/roles/deploy_supervision/templates/config.php
@@ -2,15 +2,24 @@
 date_default_timezone_set("America/Argentina/Cordoba");
 
 define('AMI_USERNAME','omnisup');
+<<<<<<< ours
 define('AMI_PASSWORD','Sup3rst1c10n');
 define('AMI_HOST','127.0.0.1');
 define('PG_USER', 'kamailio');
 define('PG_PASSWORD', 'kamailiorw');
 define('PG_HOST', '127.0.0.1');
 define('OMNI_HOST', $_SERVER['SERVER_ADDR']);
+=======
+define('AMI_PASWORD','Sup3rst1c10n');
+define('AMI_HOST','172.18.0.2');
+define('PG_USER','kamailio');
+define('PG_PASSWORD','kamailiorw');
+define('PG_HOST','172.18.0.1');
+define('OMNI_HOST', ['SERVER_ADDR'].':443');
+>>>>>>> theirs
 define('OMNI_HOST_LOGOUT', OMNI_HOST .'/accounts/logout/');
-define('WD_API_USER', 'demoadmin');
-define('WD_API_PASS', 'demo');
+define('WD_API_USER','demoadmin');
+define('WD_API_PASS','demo123');
 define("entities", $_SERVER['DOCUMENT_ROOT'].'/Omnisup/entities');
 define("helpers", $_SERVER['DOCUMENT_ROOT'].'/Omnisup/helpers');
 define("models", $_SERVER['DOCUMENT_ROOT'].'/Omnisup/Model');
